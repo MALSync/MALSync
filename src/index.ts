c310523e9d6fb86bc03a80128fb521f6233841f7
lolamtisch <francisco.seipel@yahoo.de>
2018-08-12 15:57:14 +0200

    [TASK] Add typescript and webpack config

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+async function main() {
+  alert('Hello World!');
+}
+
+main();
---
+++ webpackConfig/userscript.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: path.join(__dirname, '..', 'src/index.ts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '..', 'dist')
+  }
+};
