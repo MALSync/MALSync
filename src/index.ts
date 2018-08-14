8578e9aef19d1e03477f07acd6c3572bb0c5acb9
lolamtisch <francisco.seipel@yahoo.de>
2018-08-14 06:19:36 +0200

    [TASK] Add temp console styling

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,19 @@ function main() {
 }
 
 main();
+
+//temp
+var log = function () {
+    return Function.prototype.bind.call(console.log, console, "%cMal-Sync", "background-color: #2e51a2; color: white; padding: 2px 10px; border-radius: 3px;");
+}();
+
+var error = function () {
+    return Function.prototype.bind.call(console.error, console, "%cMal-Sync", "background-color: #8f0000; color: white; padding: 2px 10px; border-radius: 3px;");
+}();
+
+var info = function () {
+    return Function.prototype.bind.call(console.info, console, "%cMal-Sync", "background-color: wheat; color: black; padding: 2px 10px; border-radius: 3px;");
+}();
+log('log');
+error('error');
+info('info');
