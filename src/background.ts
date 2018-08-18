7c6f9823fe752cc947fa0a2a7d37ac7cf1ecea24
lolamtisch <francisco.seipel@yahoo.de>
2018-08-18 11:22:00 +0200

    [TASK] Add interface in the background

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,18 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+import {xhrI, xhrResponseI, sendMessageI, responseMessageI} from "./api/messageInterface";
+
+chrome.runtime.onMessage.addListener((message: sendMessageI, sender, sendResponse)  => {
   switch (message.name) {
     case "xhr": {
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function(){
         if (xhr.readyState == 4) {
           console.log(xhr);
-          sendResponse(xhr.responseText);
+          var responseObj: xhrResponseI = {
+            finalUrl: xhr.responseURL,
+            responseText: xhr.responseText,
+            status: xhr.status
+          }
+          sendResponse(responseObj);
         }
       }
       xhr.open(message.method, message.url, true);
