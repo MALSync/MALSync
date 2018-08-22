e0c6f30bbe07c9a2d9140dde7a52b8bcaecce7dc
lolamtisch <francisco.seipel@yahoo.de>
2018-08-22 17:55:58 +0200

    [TASK] Add kissmanga config

--- src/pages/Kissmanga/main.ts
+++ src/pages/Kissmanga/main.ts
@@ -4,12 +4,52 @@ export const Kissmanga: pageInterface = {
     domain: 'http://kissmanga.com',
     database: 'Kissmanga',
     type: 'manga',
-    isSyncPage: function(url){return true;},
+    isSyncPage: function(url){
+      if(typeof utils.urlPart(url, 5) != 'undefined'){
+        return true;
+      }
+      return false;
+    },
     sync:{
-      getTitle: function(url){return $('.bigChar').first().text();},
-      getIdentifier: function(url){return $('.bigChar').first().text();},
-      getEpisode: function(url){return parseInt($('.bigChar').first().text());},
-      getVolume: function(url){return parseInt($('.bigChar').first().text());},
+      getTitle: function(url){return utils.urlPart(url, 4);},
+      getIdentifier: function(url){return utils.urlPart(url, 4);},
+      getEpisode: function(url){
+        var episodePart = utils.urlPart(url, 5);
+        con.log(episodePart);
+        //var temp = [];
+        /*try{
+          episodePart = episodePart.replace($('.bigChar').attr('href').split('/')[2],'');
+        }catch(e){
+          episodePart = episodePart.replace(kalUrl.split("/")[4],'');
+        }*/
+        var temp = episodePart.match(/[c,C][h,H][a,A]?[p,P]?[t,T]?[e,E]?[r,R]?\D?\d+/);
+        if(temp === null){
+          episodePart = episodePart.replace(/[V,v][o,O][l,L]\D?\d+/,'');
+          temp = episodePart.match(/\d{3}/);
+          if(temp === null){
+            temp = episodePart.match(/\d+/);
+            if(temp === null){
+              episodePart = 0;
+            }else{
+              episodePart = temp[0];
+            }
+          }else{
+            episodePart = temp[0];
+          }
+        }else{
+          episodePart = temp[0].match(/\d+/)[0];
+        }
+        return episodePart;
+      },
+      getVolume: function(url){
+        try{
+          url = url.match(/[V,v][o,O][l,L]\D?\d{3}/)[0];
+          url = url.match(/\d+/)[0].slice(-3);
+        }catch(e){
+         return;
+        }
+        return url;
+      },
     },
     init(page){
       $(document).ready(function(){page.handlePage()});
