167becf3e08ba929bdc3a2c9e3d0342a2b5d7778
lolamtisch <francisco.seipel@yahoo.de>
2018-08-29 16:12:55 +0200

    [TASK] Add popup.ts

---
+++ webpackConfig/webextension.popup.config.js
@@ -0,0 +1,41 @@
+const webpack = require("webpack");
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: path.join(__dirname, '..', 'src/popup.ts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'to-string-loader' }, {loader: 'css-loader'}, {loader: 'less-loader'}]
+      }
+    ]
+  },
+  devtool: "cheap-source-map",
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js', '.less' ]
+  },
+  mode: 'development',
+  output: {
+    filename: 'popup.js',
+    path: path.resolve(__dirname, '..', 'dist', 'webextension')
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      con: path.resolve(__dirname, './../src/utils/console'),
+      utils: path.resolve(__dirname, './../src/utils/general'),
+      api: path.resolve(__dirname, './../src/api/webextension'),
+    }),
+  ],
+  optimization: {
+    minimize: false
+  }
+};
