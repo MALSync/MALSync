16689ccbb680e02a9edf6a8586aab989c16798b2
lolamtisch <francisco.seipel@yahoo.de>
2018-09-07 16:32:56 +0200

    [TASK] Pack extension

---
+++ package/webextension.js
@@ -0,0 +1,13 @@
+const archiver = require('archiver');
+const fs = require('fs');
+const path = require('path');
+
+const dist = path.join(__dirname, '../dist');
+
+const output = fs.createWriteStream(path.join(dist, '/webextension.zip'));
+const archive = archiver('zip', {
+  zlib: { level: 9 }
+});
+archive.pipe(output);
+archive.directory(path.join(dist, 'webextension'), false);
+archive.finalize();
