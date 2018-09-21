3b3feb967eb3dd3f1b4d9d1549e3893efd7e1690
lolamtisch <francisco.seipel@yahoo.de>
2018-09-21 11:54:17 +0200

    [TASK] Fix headless tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,33 @@
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+const {expect} = require('chai');
+const puppeteer = require('puppeteer');
+
+describe('Headless Tests:', function () {
+  // Define global variables
+  let browser
+  let page
+
+  before(async function () {
+    browser = await puppeteer.launch()
+    page = await browser.newPage()
+  })
+
+  beforeEach(async function () {
+    page = await browser.newPage()
+    await page.goto('https://myanimelist.net/')
+  })
+
+  afterEach(async function () {
+    await page.close()
+  })
+
+  after(async function () {
+    await browser.close()
+  })
+
+  it('MyAnimeList logo text', async function () {
+    const expectedInput = 'MyAnimeList.net'
+
+    const text = await page.evaluate(() => $('.link-mal-logo').text().trim())
+    expect(text).to.equal(expectedInput);
+  })
+})
