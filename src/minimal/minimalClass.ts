cd9eaf3e4f579afb86adb779fe330a99f85a7ec1
lolamtisch <francisco.seipel@yahoo.de>
2018-10-10 15:39:49 +0200

    [TASK] Move options to myanimelist card

--- src/minimal/minimalClass.ts
+++ src/minimal/minimalClass.ts
@@ -391,8 +391,6 @@ export class minimal{
             <h2 class="mdl-card__title-text">General</h2>
           </div>
           ${materialCheckbox('autoTracking','Autotracking'+utils.getTooltip('Autotracking is the function where this script automatically updates the anime`s you watch with your MAL account.','','bottom'))}
-          ${materialCheckbox('malTags','Continue watching links'+utils.getTooltip('If enabled: On your MAL Anime List and the bookmark list in miniMAL, an icon-link will be added to the last used streaming site you were using to watch an anime.<br>Simply click the icon to continue watching the anime.','','bottom'))}
-          ${materialCheckbox('epPredictions','Estimate episode number')}
         </div>
 
         <div class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet mdl-shadow--4dp">
@@ -427,6 +425,8 @@ export class minimal{
               </select>
             </span>
           </li>
+          ${materialCheckbox('epPredictions','Estimate episode number')}
+          ${materialCheckbox('malTags','Continue watching links'+utils.getTooltip('If enabled: On your MAL Anime List and the bookmark list in miniMAL, an icon-link will be added to the last used streaming site you were using to watch an anime.<br>Simply click the icon to continue watching the anime.','','bottom'))}
         </div>
 
         <div class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet mdl-shadow--4dp">
