9776750c1dc5e3c591d7ade79cef1269228a9bfa
lolamtisch <francisco.seipel@yahoo.de>
2019-01-06 13:16:03 +0100

    [TASK] Add entryClass interface

--- src/provider/provider.ts
+++ src/provider/provider.ts
@@ -1,5 +1,42 @@
 import {mal} from "./MyAnimeList/entryClass";
 
-export function entryClass(url:string, miniMAL:boolean = false){
+interface entryClass {
+  readonly id: number,
+  readonly type: "anime"|"manga",
+
+  name: string,
+  totalEp: number,
+  totalVol?: number,
+  addAnime: boolean,
+  login: boolean,
+  wrong: boolean,
+
+  init(),
+  update(),
+  getEpisode(),
+  setEpisode(ep:number),
+  getVolume(),
+  setVolume(ep:number),
+  getStatus(),
+  setStatus(status:number),
+  getScore(),
+  setScore(score:number),
+  getRewatching(): 1|0,
+  setRewatching(rewatching:1|0),
+  setCompletionDateToNow(),
+  setStartingDateToNow(),
+  getStreamingUrl(),
+  setStreamingUrl(url:string),
+  getRating: () => Promise<any>,
+  setResumeWaching: (url:string, ep:number) => Promise<any>,
+  getResumeWaching: () => Promise<{url:string, ep:number}>,
+  setContinueWaching: (url:string, ep:number) => Promise<any>,
+  getContinueWaching:() => Promise<{url:string, ep:number}>,
+  getImage: () => Promise<string>,
+  clone(),
+  sync(),
+}
+
+export function entryClass(url:string, miniMAL:boolean = false): entryClass{
   return new mal(url, miniMAL);
 }
