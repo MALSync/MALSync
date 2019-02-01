652b47c629282bff472ba5b8e40cc87c133d8b23
lolamtisch <francisco.seipel@yahoo.de>
2019-02-01 19:36:04 +0100

    [TASK] Add testing array

--- test/test.js
+++ test/test.js
@@ -1,33 +1,80 @@
 const {expect} = require('chai');
 const puppeteer = require('puppeteer');
 
-describe('Headless Tests:', function () {
-  // Define global variables
-  let browser
-  let page
+var testsArray = [
+  {
+    title: 'MyAnimeList',
+    url: 'https://myanimelist.net/',
+    testCases: [
+      {
+        url: 'https://myanimelist.net/',
+        expectedInput: 'MyAnimeList.net'
+      },
+      {
+        url: 'https://myanimelist.net/2',
+        expectedInput: 'MyAnimeList.net'
+      },
+    ]
+  },
+  {
+    title: 'MyAnimeList2',
+    url: 'https://myanimelist.net/',
+    testCases: [
+      {
+        url: 'https://myanimelist.net/',
+        expectedInput: 'MyAnimeList.net'
+      },
+      {
+        url: 'https://myanimelist.net/2',
+        expectedInput: 'MyAnimeList.net'
+      },
+    ]
+  }
+];
 
-  before(async function () {
-    browser = await puppeteer.launch()
-    page = await browser.newPage()
-  })
+// Define global variables
+let browser
+let page
 
-  beforeEach(async function () {
-    page = await browser.newPage()
-    await page.goto('https://myanimelist.net/')
-  })
+before(async function () {
+  browser = await puppeteer.launch()
+  page = await browser.newPage()
+})
 
-  afterEach(async function () {
-    await page.close()
-  })
+beforeEach(async function () {
+  page = await browser.newPage()
+})
 
-  after(async function () {
-    await browser.close()
-  })
+afterEach(async function () {
+  await page.close()
+})
 
-  it('MyAnimeList logo text', async function () {
-    const expectedInput = 'MyAnimeList.net'
+after(async function () {
+  await browser.close()
+})
 
-    const text = await page.evaluate(() => $('.link-mal-logo').text().trim())
-    expect(text).to.equal(expectedInput);
-  })
+testsArray.forEach(function(testPage) {
+  describe(testPage.title, function () {
+
+    it('Online', async function () {
+      const [response] = await Promise.all([
+        page.goto(testPage.url, {timeout:0}),
+        page.waitForNavigation({timeout:0}),
+      ]);
+
+      expect(200).to.equal(parseInt(response.headers().status));
+    })
+
+    testPage.testCases.forEach(function(testCase) {
+      it(testCase.url, async function () {
+        const [response] = await Promise.all([
+          page.goto(testCase.url, {timeout:0}),
+          page.waitForNavigation({timeout:0}),
+        ]);
+
+        const text = await page.evaluate(() => $('.link-mal-logo').text().trim())
+        expect(text).to.equal(testCase.expectedInput);
+      })
+    });
+  });
 })
