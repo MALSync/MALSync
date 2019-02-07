2737d626159524db028e0c094996f8a446411e31
lolamtisch <francisco.seipel@yahoo.de>
2019-02-07 18:24:23 +0100

    [TASK] Add minimalApp.vue

---
+++ src/minimal/minimalApp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <span>{{message}}</span><br/>
+    <input v-model="message">
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    data: () => ({
+      message: 'test',
+    }),
+  }
+</script>
