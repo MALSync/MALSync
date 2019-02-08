fcf4bac6354ed7990be3f8fce60610232d1dcd5d
lolamtisch <francisco.seipel@yahoo.de>
2019-02-08 20:35:33 +0100

    [TASK] Add overview.vue

---
+++ src/minimal/minimalApp/overview.vue
@@ -0,0 +1,296 @@
+<template>
+  <div class="page-content">
+    <div v-show="xhr == ''" id="loadOverview" class="mdl-progress mdl-js-progress mdl-progress__indeterminate" style="width: 100%; position: absolute;"></div>
+    <div v-if="xhr != ''" v-html="html" />
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    data: function(){
+      return {
+        xhr: '',
+      }
+    },
+    props: {
+      url: {
+        type: String,
+        default: ''
+      },
+    },
+    watch: {
+      url: async function(url){
+        this.xhr = '';
+        return api.request.xhr('GET', this.url).then((response) => {
+          this.xhr = response.responseText;
+          return response.responseText;
+          //This.vars = response.responseText;
+          //resolve(response.responseText);
+        });
+      }
+    },
+    computed: {
+      html: function(){
+        var data = this.xhr;
+        if(data == '' || this.url == '') return '';
+        var html = '';
+
+        var image = '';
+        var title = '';
+        var description  = '';
+        var altTitle = '';
+        var stats = '';
+
+        try{
+            image = data.split('js-scrollfix-bottom')[1].split('<img src="')[1].split('"')[0];
+        }catch(e) {console.log('[iframeOverview] Error:',e);}
+
+        try{
+            title = data.split('itemprop="name">')[1].split('<')[0];
+        }catch(e) {console.log('[iframeOverview] Error:',e);}
+
+        try{
+            description = data.split('itemprop="description">')[1].split('</span')[0];
+        }catch(e) {console.log('[iframeOverview] Error:',e);}
+
+        try{
+            altTitle = data.split('<h2>Alternative Titles</h2>')[1].split('<h2>')[0];
+            altTitle = altTitle.replace(/spaceit_pad/g,'mdl-chip" style="margin-right: 5px;');
+            altTitle = altTitle.replace(/<\/span>/g,'</span><span class="mdl-chip__text">');
+            altTitle = altTitle.replace(/<\/div>/g,'</span></div>');
+        }catch(e) {console.log('[iframeOverview] Error:',e);}
+
+        try{
+            var statsBlock = data.split('<h2>Statistics</h2>')[1].split('<h2>')[0];
+            // @ts-ignore
+            var tempHtml = j.$.parseHTML( statsBlock );
+            var statsHtml = '<ul class="mdl-list mdl-grid mdl-grid--no-spacing mdl-cell mdl-cell--12-col" style="display: flex; justify-content: space-around;">';
+            j.$.each(j.$(tempHtml).filter('div').slice(0,5), function( index, value ) {
+                statsHtml += '<li class="mdl-list__item mdl-list__item--two-line" style="padding: 0; padding-left: 10px; padding-right: 3px; min-width: 18%;">';
+                    statsHtml += '<span class="mdl-list__item-primary-content">';
+                        statsHtml += '<span>';
+                            statsHtml += j.$(value).find('.dark_text').text();
+                        statsHtml += '</span>';
+                        statsHtml += '<span class="mdl-list__item-sub-title">';
+                            statsHtml += j.$(value).find('span[itemprop=ratingValue]').height() != null ? j.$(value).find('span[itemprop=ratingValue]').text() : j.$(value).clone().children().remove().end().text();
+                        statsHtml += '</span>';
+                    statsHtml += '</span>';
+                statsHtml += '</li>';
+            });
+            statsHtml += '</ul>';
+            stats = statsHtml;
+        }catch(e) {console.log('[iframeOverview] Error:',e);}
+
+        html += overviewElement(this.url, title, image, description, altTitle, stats);
+        try{
+          var localType = utils.urlPart(this.url, 3);
+          html +=
+          `<div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-shadow--4dp data-block mdl-grid mdl-grid--no-spacing malClear">
+            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+              <tbody>
+                <li class="mdl-list__item mdl-list__item--three-line" style="width: 100%;">
+                  <span class="mdl-list__item-primary-content">
+                    <span>Status:</span>
+                    <span class="mdl-list__item-text-body">
+                      <select name="myinfo_status" id="myinfo_status" class="inputtext js-anime-status-dropdown mdl-textfield__input" style="outline: none; visibility: hidden;">
+                        <option selected="selected" value="1">${utils.watching(localType)}</option>
+                        <option value="2">Completed</option>
+                        <option value="3">On-Hold</option>
+                        <option value="4">Dropped</option>
+                        <option value="6">${utils.planTo(localType)}</option>
+                      </select>
+                    </span>
+                  </span>
+                </li>
+                <li class="mdl-list__item mdl-list__item--three-line" style="width: 100%;">
+                  <span class="mdl-list__item-primary-content">
+                    <span>${utils.episode(localType)}:</span>
+                    <span class="mdl-list__item-text-body">
+                      <input type="text" id="myinfo_watchedeps" name="myinfo_watchedeps" size="3" class="inputtext mdl-textfield__input" value="6" style="width: 35px; display: inline-block; visibility: hidden;"> / <span id="curEps" style="visibility: hidden;">?</span>
+                      <a href="javascript:void(0)" class="js-anime-increment-episode-button" target="_blank">
+                        <i class="fa fa-plus-circle ml4">
+                        </i>
+                      </a>
+                    </span>
+                  </span>
+                </li>
+            `;
+            if(localType == 'manga'){
+              html +=`
+                <li class="mdl-list__item mdl-list__item--three-line" style="width: 100%;">
+                  <span class="mdl-list__item-primary-content">
+                    <span>Volume:</span>
+                    <span class="mdl-list__item-text-body">
+                      <input type="text" id="myinfo_volumes" name="myinfo_volumes" size="3" class="inputtext mdl-textfield__input" value="6" style="width: 35px; display: inline-block; visibility: hidden;"> / <span id="curVolumes" style="visibility: hidden;">?</span>
+                      <a href="javascript:void(0)" class="js-anime-increment-episode-button" target="_blank">
+                        <i class="fa fa-plus-circle ml4">
+                        </i>
+                      </a>
+                    </span>
+                  </span>
+                </li>
+              `;
+            }
+            html +=`
+                <li class="mdl-list__item mdl-list__item--three-line" style="width: 100%;">
+                  <span class="mdl-list__item-primary-content">
+                    <span>Your Score:</span>
+                    <span class="mdl-list__item-text-body">
+                      <select name="myinfo_score" id="myinfo_score" class="inputtext mdl-textfield__input" style="outline: none; visibility: hidden;">
+                        <option value="" selected="selected">Select</option>
+                        <option value="10">(10) Masterpiece</option>
+                        <option value="9">(9) Great</option>
+                        <option value="8">(8) Very Good</option>
+                        <option value="7">(7) Good</option>
+                        <option value="6">(6) Fine</option>
+                        <option value="5">(5) Average</option>
+                        <option value="4">(4) Bad</option>
+                        <option value="3">(3) Very Bad</option>
+                        <option value="2">(2) Horrible</option>
+                        <option value="1">(1) Appalling</option>
+                      </select>
+                    </span>
+                  </span>
+                </li>
+                <li class="mdl-list__item" style="width: 100%;">
+                  <input type="button" name="myinfo_submit" value="Update" class="inputButton btn-middle flat js-anime-update-button mdl-button mdl-js-button mdl-button--raised mdl-button--colored" style="margin-right: 5px;" data-upgraded=",MaterialButton">
+                  <small>
+                    <a href="https://myanimelist.net/ownlist/${localType}/${utils.urlPart(this.url, 4)}/edit" target="_blank">Edit Details</a>
+                  </small>
+                </li>
+
+              </tbody>
+            </table>
+          </div>`;
+        }catch(e) {console.log('[iframeOverview] Error:',e);}
+
+        try{
+          var relatedBlock = data.split('Related ')[1].split('</h2>')[1].split('<h2>')[0];
+          var related = j.$.parseHTML( relatedBlock );
+          var relatedHtml = '<ul class="mdl-list">';
+          j.$.each(j.$(related).filter('table').find('tr'), function( index, value ) {
+            relatedHtml += '<li class="mdl-list__item mdl-list__item--two-line">';
+              relatedHtml += '<span class="mdl-list__item-primary-content">';
+                relatedHtml += '<span>';
+                  relatedHtml += j.$(value).find('.borderClass').first().text();
+                relatedHtml += '</span>';
+                relatedHtml += '<span class="mdl-list__item-sub-title">';
+                                  j.$(value).find('.borderClass').last().each(function( index, value){
+                                     j.$(value).html(j.$(value).children());
+                                  })
+                  relatedHtml += j.$(value).find('.borderClass').last().html();
+                relatedHtml += '</span>';
+              relatedHtml += '</span>';
+            relatedHtml += '</li>';
+          });
+          relatedHtml += '</ul>';
+
+          html += `<div class="mdl-grid mdl-grid--no-spacing mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-shadow--4dp related-block alternative-list mdl-grid malClear">
+                    ${relatedHtml}
+                  </div>`
+        }catch(e) {console.log('[iframeOverview] Error:',e);}
+
+        try{
+          html += `<div style="display: none;" class="mdl-grid mdl-grid--no-spacing mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-shadow--4dp mdl-grid alternative-list stream-block malClear">
+                    <ul class="mdl-list stream-block-inner">
+                    </ul>
+                  </div>`;
+        }catch(e) {console.log('[iframeOverview] Error:',e);}
+
+        try{
+          var characterBlock = data.split('detail-characters-list')[1].split('</h2>')[0];
+          var charHtml = j.$.parseHTML( '<div class="detail-characters-list '+characterBlock );
+          var temphtml = '';
+          var charFound = 0;
+          var tempWrapHtml = '<div class="mdl-grid mdl-grid--no-spacing mdl-cell mdl-cell--12-col mdl-shadow--4dp characters-block mdl-grid malClear">\
+          <div class="mdl-card__actions clicker" >\
+            <h1 class="mdl-card__title-text" style="float: left;">Characters</h1>\
+          </div>\
+          <div class="mdl-grid mdl-card__actions mdl-card--border" id="characterList" style="justify-content: space-between; ">';
+
+          j.$.each(j.$(charHtml).find(':not(td) > table'), ( index, value ) => {
+            if(!index) charFound = 1;
+            var regexDimensions = /\/r\/\d*x\d*/g;
+            var charImg = j.$(value).find('img').first().attr("data-src");
+            if ( regexDimensions.test(charImg)){
+              charImg = charImg.replace(regexDimensions, '');
+            }else{
+              charImg = 'https://myanimelist.cdn-dena.com/images/questionmark_23.gif';
+            }
+
+            charImg = utils.handleMalImages(charImg);
+
+            tempWrapHtml += '<div>';
+              tempWrapHtml += '<div class="mdl-grid" style="width: 126px;">';
+                tempWrapHtml += '<div style="width: 100%; height: auto;">';
+                  tempWrapHtml += '<img style="height: auto; width: 100%;"src="'+charImg+'">';
+                tempWrapHtml += '</div>';
+                tempWrapHtml += '<div class="">';
+                  tempWrapHtml += j.$(value).find('.borderClass .spaceit_pad').first().parent().html();
+                tempWrapHtml += '</div>';
+              tempWrapHtml += '</div>';
+            tempWrapHtml += '</div>';
+
+          });
+          for(var i=0; i < 10; i++){
+            tempWrapHtml +='<div class="listPlaceholder" style="height: 0;"><div class="mdl-grid" style="width: 126px;"></div></div>';
+          }
+          tempWrapHtml += '</div></div>';
+          if(charFound) html += tempWrapHtml;
+
+        }catch(e) {console.log('[iframeOverview] Error:',e);}
+
+        try{
+            var infoBlock = data.split('<h2>Information</h2>')[1].split('<h2>')[0];
+            var infoData = j.$.parseHTML( infoBlock );
+            var infoHtml = '<ul class="mdl-grid mdl-grid--no-spacing mdl-list mdl-cell mdl-cell--12-col">';
+            j.$.each(j.$(infoData).filter('div'), ( index, value ) => {
+                if((index + 4) % 4 == 0 && index != 0){
+                    //infoHtml +='</ul><ul class="mdl-list mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet">';
+                }
+                infoHtml += '<li class="mdl-list__item mdl-list__item--three-line mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet">';
+                    infoHtml += '<span class="mdl-list__item-primary-content">';
+                        infoHtml += '<span>';
+                            infoHtml += j.$(value).find('.dark_text').text();
+                        infoHtml += '</span>';
+                        infoHtml += '<span class="mdl-list__item-text-body">';
+                            j.$(value).find('.dark_text').remove();
+                            infoHtml += j.$(value).html();
+                            //j.$(value).find('*').each(function(){infoHtml += j.$(value)[0].outerHTML});
+                            //infoHtml += j.$(value).find('span[itemprop=ratingValue]').height() != null ? j.$(value).find('span[itemprop=ratingValue]').text() : j.$(value).clone().children().remove().end().text();
+                        infoHtml += '</span>';
+                    infoHtml += '</span>';
+                infoHtml += '</li>';
+            });
+            infoHtml += '</ul>';
+            html += '<div class="mdl-grid mdl-grid--no-spacing mdl-cell mdl-cell--12-col mdl-shadow--4dp info-block mdl-grid malClear">'+infoHtml+'</div>';
+        }catch(e) {console.log('[iframeOverview] Error:',e);}
+
+        return '<div class="mdl-grid">'+html+'</div>';
+      }
+    }
+  }
+
+  function overviewElement(url, title, image, description, altTitle, stats){
+    return `
+      <div class="mdl-cell mdl-cell--1-col mdl-cell--8-col-tablet mdl-cell--6-col-phone mdl-shadow--4dp stats-block malClear" style="min-width: 120px;">
+        ${stats}
+      </div>
+      <div class="mdl-grid mdl-cell mdl-shadow--4dp coverinfo malClear" style="display:block; flex-grow: 100; min-width: 70%;">
+        <div class="mdl-card__media mdl-cell mdl-cell--2-col" style="background-color: transparent; float:left; padding-right: 16px;">
+          <img class="malImage malClear" style="width: 100%; height: auto;" src="${image}"></img>
+        </div>
+        <div class="mdl-cell mdl-cell--12-col">
+          <a class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect malClear malLink" href="${url}" style="float: right;" target="_blank"><i class="material-icons">open_in_new</i></a>
+          <h1 class="malTitle mdl-card__title-text malClear" style="padding-left: 0px; overflow:visible;">${title}</h1>
+          <div class="malAltTitle mdl-card__supporting-text malClear" style="padding: 10px 0 0 0px; overflow:visible;">${altTitle}</div>
+        </div>
+        <div class="malDescription malClear mdl-cell mdl-cell--10-col" style="overflow: hidden;">
+          <p style="color: black;">
+            ${description}
+          </p>
+        </div>
+      </div>
+    `;
+  }
+</script>
