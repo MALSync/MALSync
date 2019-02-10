590094a43242fb7882f15af155b6587b3411c1ad
lolamtisch <francisco.seipel@yahoo.de>
2019-02-10 21:22:09 +0100

    [BUGFIX] Fix links not working

--- src/minimal/minimalApp/bookmarks.vue
+++ src/minimal/minimalApp/bookmarks.vue
@@ -3,10 +3,13 @@
     <div class="mdl-grid" id="malList" style="justify-content: space-around;">
 
       <div v-for="item in items" :key="item.id">
-        <a :href="item.url" class="mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--6-col-phone mdl-shadow--2dp mdl-grid bookEntry" style="color: black; position: relative; cursor: pointer; height: 250px; padding: 0; width: 210px; height: 293px;">
+        <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--6-col-phone mdl-shadow--2dp mdl-grid bookEntry" style="position: relative; height: 250px; padding: 0; width: 210px; height: 293px;">
           <div class="data title lazyBack init" :data-src="imageHi(item)" style="background-image: url(); background-color: grey; background-size: cover; background-position: center center; background-repeat: no-repeat; width: 100%; position: relative; padding-top: 5px;">
+            <a :href="item.url" style="position: absolute; top: 0; left: 0; right: 0; bottom: 0;"></a>
             <span class="mdl-shadow--2dp" style="position: absolute; bottom: 0; display: block; background-color: rgba(255, 255, 255, 0.9); padding-top: 5px; display: inline-flex; align-items: center; justify-content: space-between; left: 0; right: 0; padding-right: 8px; padding-left: 8px; padding-bottom: 8px;">
-              {{item.title}}
+              <a :href="item.url" style="color: black; text-decoration: none;">
+                {{item.title}}
+              </a>
               <div id="p1" class="mdl-progress" style="position: absolute; top: -4px; left: 0;">
                 <div class="progressbar bar bar1" :style="progress(item)"></div>
                 <div class="bufferbar bar bar2" style="width: calc(100% + 1px);"></div>
@@ -20,7 +23,7 @@
               </div>
             </span>
           </div>
-        </a>
+        </div>
       </div>
 
     </div>
