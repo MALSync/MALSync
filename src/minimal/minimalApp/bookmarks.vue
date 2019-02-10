83d583820929b57ff7ca1cb70b5cd03082aa8408
lolamtisch <francisco.seipel@yahoo.de>
2019-02-10 21:11:45 +0100

    [TASK] Use link and add streamurl

--- src/minimal/minimalApp/bookmarks.vue
+++ src/minimal/minimalApp/bookmarks.vue
@@ -3,7 +3,7 @@
     <div class="mdl-grid" id="malList" style="justify-content: space-around;">
 
       <div v-for="item in items" :key="item.id">
-        <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--6-col-phone mdl-shadow--2dp mdl-grid bookEntry" style="position: relative; cursor: pointer; height: 250px; padding: 0; width: 210px; height: 293px;">
+        <a :href="item.url" class="mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--6-col-phone mdl-shadow--2dp mdl-grid bookEntry" style="color: black; position: relative; cursor: pointer; height: 250px; padding: 0; width: 210px; height: 293px;">
           <div class="data title lazyBack init" :data-src="imageHi(item)" style="background-image: url(); background-color: grey; background-size: cover; background-position: center center; background-repeat: no-repeat; width: 100%; position: relative; padding-top: 5px;">
             <span class="mdl-shadow--2dp" style="position: absolute; bottom: 0; display: block; background-color: rgba(255, 255, 255, 0.9); padding-top: 5px; display: inline-flex; align-items: center; justify-content: space-between; left: 0; right: 0; padding-right: 8px; padding-left: 8px; padding-bottom: 8px;">
               {{item.title}}
@@ -14,10 +14,13 @@
               </div>
               <div class="data progress mdl-chip mdl-chip--contact mdl-color--indigo-100" style="float: right; line-height: 20px; height: 20px; padding-right: 4px; margin-left: 5px;">
                 <div class="link mdl-chip__contact mdl-color--primary mdl-color-text--white" style="line-height: 20px; height: 20px; margin-right: 0;">{{item.watchedEp}}</div>
+                <a class="mal-sync-stream" v-if="streamUrl(item)" :title="streamUrl(item).split('/')[2]" target="_blank" style="margin: 0 5px;" :href="streamUrl(item)">
+                  <img :src="favicon(streamUrl(item).split('/')[2])">
+                </a>
               </div>
             </span>
           </div>
-        </div>
+        </a>
       </div>
 
     </div>
@@ -64,6 +67,12 @@
       progress: function(item){
         var width = ( item.watchedEp / item.totalEp ) * 100;
         return 'width: '+width+'%;'
+      },
+      streamUrl: function(item){
+        return utils.getUrlFromTags(item.tags);
+      },
+      favicon: function(domain){
+        return utils.favicon(domain);
       }
     }
   }
