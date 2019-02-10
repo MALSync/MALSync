639106760126220b73001e680ded870b1e6fa941
lolamtisch <francisco.seipel@yahoo.de>
2019-02-10 18:48:08 +0100

    [TASK] Add bookmarks vue

---
+++ src/minimal/minimalApp/bookmarks.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="mdl-grid" id="malList" style="justify-content: space-around;">
+      <div v-for="item in items" :key="item.id">
+        {{item.title}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import * as provider from "./../../provider/provider.ts";
+  export default {
+    data: function(){
+      return {
+        items: [],
+      }
+    },
+    props: {
+      listType: {
+        type: String,
+        default: 'anime'
+      },
+      state: {
+        type: Number,
+        default: 1
+      },
+    },
+    mounted: function(){
+      provider.userList(this.state, this.listType, {
+        fullListCallback: (list) => {
+          this.items = list;
+        }
+      });
+    },
+  }
+</script>
