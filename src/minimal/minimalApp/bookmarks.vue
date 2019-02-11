30bb85b21f22e520c01d3aad2b20d8866f47b2e4
lolamtisch <francisco.seipel@yahoo.de>
2019-02-11 18:33:52 +0100

    [TASK] Add loading bar and no entry chip

--- src/minimal/minimalApp/bookmarks.vue
+++ src/minimal/minimalApp/bookmarks.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <div v-show="loading" id="loadMalSearchPop" class="mdl-progress mdl-js-progress mdl-progress__indeterminate" style="width: 100%; position: absolute;"></div>
     <div class="mdl-grid" id="malList" style="justify-content: space-around;">
-
+      <span v-if="!loading && !items.length" class="mdl-chip" style="margin: auto; margin-top: 16px; display: table;"><span class="mdl-chip__text">No Entries</span></span>
       <div v-for="item in items" :key="item.id">
         <bookmarksItem :item="item"/>
       </div>
@@ -20,6 +21,7 @@
     data: function(){
       return {
         items: [],
+        loading: true,
       }
     },
     props: {
@@ -33,8 +35,10 @@
       },
     },
     mounted: function(){
+      this.loading = true;
       provider.userList(this.state, this.listType, {
         fullListCallback: async (list) => {
+          this.loading = false;
           this.items = list;
         }
       });
