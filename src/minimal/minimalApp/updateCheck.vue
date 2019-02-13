1feeac539696d356d08f4f25656ef0975f28356a
lolamtisch <francisco.seipel@yahoo.de>
2019-02-13 12:46:19 +0100

    [TASK] Add history debugging

--- src/minimal/minimalApp/updateCheck.vue
+++ src/minimal/minimalApp/updateCheck.vue
@@ -30,6 +30,17 @@
         <th>{{item.error}}</th>
       </tr>
     </table>
+    <div class="history">
+      <h3>Notification History</h3>
+      <a  v-for="historyItem in history" :key="historyItem.id" :href="historyItem.url" class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet mdl-shadow--2dp mdl-grid" style="text-decoration: none !important; color: black;">
+        <img :src="historyItem.iconUrl" style="margin: -8px 0px -8px -8px; height: 100px; width: 64px; background-color: grey;">
+        <div style="flex-grow: 100; cursor: pointer; margin-top: 0; margin-bottom: 0;" class="mdl-cell">
+          <span style="font-size: 20px; font-weight: 400; line-height: 1;">{{historyItem.title}}</span>
+          <p style="margin-bottom: 0; line-height: 20px; padding-top: 3px;">{{historyItem.message}}</p>
+          <p style="margin-bottom: 0; line-height: 20px;">{{historyItem.timeDiff}}</p>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -40,6 +51,7 @@
     data: function(){
       return {
         items: [],
+        history: [],
       }
     },
     props: {
@@ -92,6 +104,18 @@
             }
             this.items = list;
           }
+        });
+        api.storage.get('notificationHistory').then((history) => {
+          var historyHtml = '<h3>Notification History</h3>';
+          history.forEach((entry) => {
+            var timeDiff = Date.now() - entry.timestamp;
+
+            timeDiff = utils.timeDiffToText(timeDiff);
+            timeDiff += 'ago';
+            entry.timeDiff = timeDiff;
+          });
+
+          this.history = history.reverse();
         });
       },
       deleteItem(item){
