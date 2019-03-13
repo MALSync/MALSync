ed26bceb0bf1ab12c81d09d99752d5fc0bbc3e79
lolamtisch <francisco.seipel@yahoo.de>
2019-03-13 16:33:14 +0100

    [TASK] Read player times

--- src/iframe.ts
+++ src/iframe.ts
@@ -1,1 +1,18 @@
-alert('test');
+setInterval(function(){
+  var players = document.getElementsByTagName('video');
+  for (var i = 0; i < players.length; i++) {
+    var player:any = players[i];
+    var duration = player.duration;
+    var current = player.currentTime;
+
+    if(duration){
+      var item = {
+        current,
+        duration
+      }
+      con.info(window.location.href, item);
+      break;
+    }
+
+  };
+}, 1000);
---
+++ src/pages/playerUrls.js
@@ -0,0 +1,7 @@
+module.exports = {
+  openload: {
+    match: [
+      '*://*.openload.co/*',
+    ]
+  },
+};
