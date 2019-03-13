5b137e4b7c69673c3881aa8f309b1764db955b2d
lolamtisch <francisco.seipel@yahoo.de>
2019-03-13 18:22:26 +0100

    [TASK] Add more streaming urls

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -4,4 +4,29 @@ module.exports = {
       '*://*.openload.co/*',
     ]
   },
+  streamango: {
+    match: [
+      '*://*.streamango.com/*',
+    ]
+  },
+  mp4upload: {
+    match: [
+      '*://*.mp4upload.com/*',
+    ]
+  },
+  mcloud: {
+    match: [
+      '*://*.mcloud.to/*',
+    ]
+  },
+  prettyfast: {
+    match: [
+      '*://*.prettyfast.to/*',
+    ]
+  },
+  rapidvideo: {
+    match: [
+      '*://*.rapidvideo.com/*',
+    ]
+  },
 };
