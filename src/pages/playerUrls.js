14b24fc4f9bbed42c0057cae928e13aeb158d409
lolamtisch <francisco.seipel@yahoo.de>
2019-03-14 11:55:35 +0100

    [TASK] Add more video provider

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -29,4 +29,86 @@ module.exports = {
       '*://*.rapidvideo.com/*',
     ]
   },
+  crunchyroll: {
+    match: [
+      '*://*.static.crunchyroll.com/*',
+    ]
+  },
+  vidstreaming: {
+    match: [
+      '*://*.vidstreaming.io/*',
+    ]
+  },
+  xstreamcdn: {
+    match: [
+      '*://*.xstreamcdn.com/*',
+    ]
+  },
+  oload: {
+    match: [
+      '*://*.oload.tv/*',
+    ]
+  },
+  mail: {
+    match: [
+      '*://*.mail.ru/*',
+    ]
+  },
+  myvi: {
+    match: [
+      '*://*.myvi.ru/*',
+    ]
+  },
+  sibnet: {
+    match: [
+      '*://*.sibnet.ru/*',
+    ]
+  },
+  tune: {
+    match: [
+      '*://*.tune.pk/*',
+    ]
+  },
+  vimple: {
+    match: [
+      '*://*.vimple.ru/*',
+    ]
+  },
+  crunchyroll: {
+    match: [
+      '*://*.static.crunchyroll.com/*',
+    ]
+  },
+  href: {
+    match: [
+      '*://*.href.li/*',
+      '*://*.vk.com/*'
+    ]
+  },
+  cloudvideo: {
+    match: [
+      '*://*.cloudvideo.tv/*',
+    ]
+  },
+  fembed: {
+    match: [
+      '*://*.fembed.net/*',
+      '*://*.fembed.com/*',
+    ]
+  },
+  youpload: {
+    match: [
+      '*://*.youpload.co/*',
+    ]
+  },
+  yourupload: {
+    match: [
+      '*://*.yourupload.com/*',
+    ]
+  },
+  vidlox: {
+    match: [
+      '*://*.vidlox.me/*',
+    ]
+  },
 };
