56dd66bf81371bfc57feb62b9c5d250b96c052c2
kaiserdj <gonferfr@gmail.com>
2019-03-20 18:19:58 +0100

    Limpiar comentarios

--- src/pages/Animeflv/main.ts
+++ src/pages/Animeflv/main.ts
@@ -4,19 +4,19 @@ export const animeflv: pageInterface = {
     name: 'animeflv',
     domain: 'https://animeflv.net',
     type: 'anime',
-    isSyncPage: function(url){ //Funciona
+    isSyncPage: function(url){
       if(j.$('h2.SubTitle').length){
         return true;
       }
       return false;
     },
     sync:{
-      getTitle: function(url){return j.$('h1.Title').text().split(' Episodio')[0].trim();}, //Funciona
-      getIdentifier: function(url){return utils.urlPart(animeflv.domain+j.$(".fa-th-list").attr('href'), 4)+'/'+utils.urlPart(animeflv.domain+j.$(".fa-th-list").attr('href'), 5);}, //Funciona
-      getOverviewUrl: function(url){return animeflv.domain+j.$(".fa-th-list").attr('href');}, //Funciona
-      getEpisode: function(url){return parseInt(j.$('h2.SubTitle').text().replace('Episodio ', '').trim());}, //Funciona
-      nextEpUrl: function(url){return animeflv.domain+j.$(".fa-chevron-right").attr('href');}, //Funciona
-      uiSelector: function(selector){selector.insertAfter(j.$(".CapOptns"));}, //Funciona
+      getTitle: function(url){return j.$('h1.Title').text().split(' Episodio')[0].trim();},
+      getIdentifier: function(url){return utils.urlPart(animeflv.domain+j.$(".fa-th-list").attr('href'), 4)+'/'+utils.urlPart(animeflv.domain+j.$(".fa-th-list").attr('href'), 5);},
+      getOverviewUrl: function(url){return animeflv.domain+j.$(".fa-th-list").attr('href');},
+      getEpisode: function(url){return parseInt(j.$('h2.SubTitle').text().replace('Episodio ', '').trim());},
+      nextEpUrl: function(url){return animeflv.domain+j.$(".fa-chevron-right").attr('href');},
+      uiSelector: function(selector){selector.insertAfter(j.$(".CapOptns"));},
     },
     overview:{
       getTitle: function(url){
@@ -47,14 +47,14 @@ export const animeflv: pageInterface = {
                 }
               }
             }
-        return j.$('h2.Title').text();}, //Funciona
-      getIdentifier: function(url){return utils.urlPart(url, 4)+'/'+utils.urlPart(url, 5);}, //Funciona
-      uiSelector: function(selector){selector.insertAfter(j.$(".Description"));}, //Funciona
+        return j.$('h2.Title').text();},
+      getIdentifier: function(url){return utils.urlPart(url, 4)+'/'+utils.urlPart(url, 5);},
+      uiSelector: function(selector){selector.insertAfter(j.$(".Description"));},
       list:{ //Bug no carga todos los episodios, si hay muchos
         offsetHandler: false,
-        elementsSelector: function(){return j.$(".MALSync a");}, //Funciona
-        elementUrl: function(selector){return utils.absoluteLink(selector.attr('href'), animeflv.domain);}, //Funciona
-        elementEp: function(selector){return selector.attr('epi')}, //Funciona
+        elementsSelector: function(){return j.$(".MALSync a");},
+        elementUrl: function(selector){return utils.absoluteLink(selector.attr('href'), animeflv.domain);},
+        elementEp: function(selector){return selector.attr('epi')},
       }
     },
     init(page){
