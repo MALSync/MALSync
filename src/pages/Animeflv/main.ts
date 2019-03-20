6a8f19f6091baa316f0878e541ae19e2506fc7ec
kaiserdj <gonferfr@gmail.com>
2019-03-20 18:07:09 +0100

    [Fix]lista de episodios

--- src/pages/Animeflv/main.ts
+++ src/pages/Animeflv/main.ts
@@ -19,14 +19,42 @@ export const animeflv: pageInterface = {
       uiSelector: function(selector){selector.insertAfter(j.$(".CapOptns"));}, //Funciona
     },
     overview:{
-      getTitle: function(url){return j.$('h2.Title').text();}, //Funciona
+      getTitle: function(url){
+        document.body.insertAdjacentHTML( 'afterbegin', '<div id="MALSync" class="MALSync" style="display: none;"><ul id="MALSyncUl" class="MALSyncUl"></ul></div>' );
+          var idMALSync = document.getElementById('MALSyncUl');
+          var patron = /<script>\s\s   var([^]*?)<\/script>/g;
+          var html = document.body.innerHTML;
+          var scriptEps = patron.exec(html);
+            if(scriptEps != null){
+              // @ts-ignore
+              scriptEps = scriptEps[1] || null;
+              if(scriptEps != null){
+                // @ts-ignore
+                console.log(scriptEps);
+                var patron2 = /\[([^\[\]]{0,10},{0,10})\]/g;
+                var eps = scriptEps.toString().match(patron2);
+                if(eps != null){
+                  // @ts-ignore
+                  console.log(eps);
+                  eps.forEach(element => {
+                    if(idMALSync != null){
+                      var Url = animeflv.domain+'/ver/'+element.split(',')[1].replace(']','')+'/'+utils.urlPart(url, 5)+'-'+element.split(',')[0].replace('[','');
+                      var Episodio = element.split(',')[0].replace('[','');
+                      console.log(element);
+                      idMALSync.innerHTML += '<li><a href="'+Url+'" epi="'+Episodio+'"></a> </li>';
+                    }
+                  });
+                }
+              }
+            }
+        return j.$('h2.Title').text();}, //Funciona
       getIdentifier: function(url){return utils.urlPart(url, 4)+'/'+utils.urlPart(url, 5);}, //Funciona
       uiSelector: function(selector){selector.insertAfter(j.$(".Description"));}, //Funciona
       list:{ //Bug no carga todos los episodios, si hay muchos
         offsetHandler: false,
-        elementsSelector: function(){return j.$(".ListCaps a");},
-        elementUrl: function(selector){return utils.absoluteLink(selector.attr('href'), animeflv.domain);},
-        elementEp: function(selector){return selector.find('p').first().text().replace('Episodio ','').trim();},
+        elementsSelector: function(){return j.$(".MALSync a");}, //Funciona
+        elementUrl: function(selector){return utils.absoluteLink(selector.attr('href'), animeflv.domain);}, //Funciona
+        elementEp: function(selector){return selector.attr('epi')}, //Funciona
       }
     },
     init(page){
