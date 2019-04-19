3681cac9a11076ebc0b01eae1c7719d7a68ff157
lolamtisch <francisco.seipel@yahoo.de>
2019-04-19 16:25:50 +0200

    [TASK] Use new Icons

--- webpackConfig/webextension.assets.js
+++ webpackConfig/webextension.assets.js
@@ -141,6 +141,7 @@ const generateManifest = () => {
     ],
     'icons': {
       '16': 'icons/icon16.png',
+      '32': 'icons/icon32.png',
       '48': 'icons/icon48.png',
       '128': 'icons/icon128.png'
     },
