4a717014ec9d8fea93ee0a9628d74fa04f018300
lolamtisch <francisco.seipel@yahoo.de>
2019-04-25 16:38:34 +0200

    [TASK] Add overview

--- src/pages/Vrv/main.ts
+++ src/pages/Vrv/main.ts
@@ -1,12 +1,30 @@
 import {pageInterface} from "./../pageInterface";
 
 var json:any = undefined;
+var ident:any = undefined;
 
-function getSeries(page){
+function getSeries(page, overview = ''){
+  json = undefined;
+  ident = undefined;
   api.request.xhr('GET', page.url).then((response) => {
     con.log(response);
     json = JSON.parse('{'+response.responseText.split('__INITIAL_STATE__ = {')[1].split('};')[0]+'}');
     con.log(json);
+
+    if(overview.length){
+      json.seriesPage.seasons.forEach(function(element) {
+        if(overview.indexOf(element.json.title) !== -1){
+          con.log('Season Found', element);
+          ident = element;
+        }
+      });
+    }else{
+      if(json.seriesPage.seasons.length){
+        con.log('Season', json.seriesPage.seasons[0]);
+        ident = json.seriesPage.seasons[0];
+      }
+    }
+
     page.handlePage();
   });
 }
@@ -16,6 +34,7 @@ export const Vrv: pageInterface = {
     domain: 'https://vrv.co',
     type: 'anime',
     isSyncPage: function(url){
+      if(utils.urlPart(window.location.href, 3) == 'series') return false;
       return true;
     },
     sync:{
@@ -26,6 +45,13 @@ export const Vrv: pageInterface = {
       nextEpUrl: function(url){
         if(typeof json.watch.mediaResource.json.next_episode_id === 'undefined') return '';
         return Vrv.domain+'/watch/'+json.watch.mediaResource.json.next_episode_id;
+      },
+    },
+    overview:{
+      getTitle: function(url){return json.seriesPage.series.json.title+' - '+ ident.json.title.replace(json.seriesPage.series.json.title, '');},
+      getIdentifier: function(url){return ident.json.id;},
+      uiSelector: function(selector){
+        selector.insertAfter($( '.erc-series-info .series-title' ).first());
       },
     },
     init(page){
@@ -42,6 +68,9 @@ export const Vrv: pageInterface = {
         $('#flashinfo-div, #flash-div-bottom, #flash-div-top, #malp').remove();
         if(utils.urlPart(window.location.href, 3) == 'watch'){
           getSeries(page);
+        }
+        if(utils.urlPart(window.location.href, 3) == 'series'){
+          getSeries(page, $('.controls-select-trigger .season-info').text().trim());
         }
       }
     }
