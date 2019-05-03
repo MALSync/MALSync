58180808e5e404628f4784c448dc284d692c6c24
lolamtisch <francisco.seipel@yahoo.de>
2019-05-03 14:27:12 +0200

    [BUGFIX] Fix missing title in kitsu #77

--- src/provider/Kitsu/helper.ts
+++ src/provider/Kitsu/helper.ts
@@ -101,3 +101,17 @@ export async function userId(){
     });
   }
 }
+
+export function getTitle(titles){
+  var title = titles.en;
+  if(typeof title == 'undefined' || !title) title = titles.en_jp;
+  if(typeof title == 'undefined' || !title) title = titles.ja_jp;
+  if(typeof title == 'undefined' || !title){
+    var keys = Object.keys(titles);
+    if(!keys.length){
+      return 'No Title';
+    }
+    title = titles[keys[0]];
+  }
+  return title;
+}
