cbcfe05b090f296c246ec8ae0f0e4970a133666e
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2019-06-03 18:13:12 +0200

    beta first try

--- src/pages/WonderfulSubs/main.ts
+++ src/pages/WonderfulSubs/main.ts
@@ -62,3 +62,65 @@ export const WonderfulSubs: pageInterface = {
     });
   }
 };
+var var betaWonderfulSubs: pageInterface = {
+  name: "betaWonderfulSubs",
+  domain: "https://beta.wonderfulsubs.com",
+  type: "anime",
+  isSyncPage: function(url) {
+    if (url.split("/")[3] === "watch") {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  sync: {
+    getTitle: function(url) {
+      return j.$("h6.subtitle").text();
+    },
+    getIdentifier: function(url) {
+      return j
+        .$("h6.subtitle")
+        .text()
+        .toLowerCase()
+        .replace(/ /g, "-");
+    },
+    getOverviewUrl: function(url) {
+      return "https://beta.wonderfulsubs.com" + "/watch/" + url.split("/")[4].replace(/\?[^?]*$/g, "");
+    },
+    getEpisode: function(url) {
+      return j
+        .$("div.episode-number")
+        .text()
+        .replace(/\D+/g, "");
+    }
+  },
+  init(page) {
+    api.storage.addStyle(require("./style.less").toString());
+    page.url = window.location.href;
+    if (page.url.split("/")[3] === "watch") {
+      utils.waitUntilTrue(
+        function() {
+          return j.$("h6.subtitle").text();
+        },
+        function() {
+          page.handlePage();
+        }
+      );
+    }
+    utils.urlChangeDetect(function() {
+      page.url = window.location.href;
+      page.UILoaded = false;
+      $("#flashinfo-div, #flash-div-bottom, #flash-div-top").remove();
+      if (page.url.split("/")[3] === "watch") {
+        utils.waitUntilTrue(
+          function() {
+            return j.$("h6.subtitle").text();
+          },
+          function() {
+            page.handlePage();
+          }
+        );
+      }
+    });
+  }
+};
