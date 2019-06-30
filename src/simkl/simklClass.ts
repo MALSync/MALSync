bad14ae36c58a039e03febab528cb78bf8411d40
lolamtisch <francisco.seipel@yahoo.de>
2019-06-30 16:42:20 +0200

    [TASK] Detect simkl page load

--- src/simkl/simklClass.ts
+++ src/simkl/simklClass.ts
@@ -1,15 +1,25 @@
 export class simklClass{
+
+  private interval;
+
   constructor(public url:string){
     utils.urlChangeDetect(() => {
-      this.url = window.location.href;
-      this.init();
+      con.log(this.url);
+      this.interval = utils.waitUntilTrue(function(){
+        return (!$('#global_div').length || parseInt($('#global_div').css('opacity')) === 1) &&
+        (!$('#tvMainTable').length || parseInt($('#tvMainTable').css('opacity')) === 1);
+      }, () => {
+        this.url = window.location.href;
+        this.init();
+      }, 1000)
     });
 
     api.storage.addStyle(require('./style.less').toString());
+    this.init();
   }
 
   init(){
-    con.log(this.url);
+    con.info('Init');
   }
 
 }
