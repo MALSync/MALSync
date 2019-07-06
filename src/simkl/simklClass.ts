31e7f1b1b68833ee26b6bf7c8ae10f3b598f0a9a
lolamtisch <francisco.seipel@yahoo.de>
2019-07-06 19:40:18 +0200

    [TASK] Remove unneeded UI

--- src/simkl/simklClass.ts
+++ src/simkl/simklClass.ts
@@ -106,7 +106,7 @@ export class simklClass{
 
   async streamingUI(){
     con.log('Streaming UI');
-    $('#mal-sync-stream-div').remove();
+
     var malObj = this.page.malObj;
 
     var streamUrl = malObj.getStreamingUrl();
@@ -115,38 +115,15 @@ export class simklClass{
       $(document).ready(async () => {
         this.malkiss.streamUrl = streamUrl;
 
-        $('h1').first().append(`
-        <div class="data title progress" id="mal-sync-stream-div" style="display: inline-block; position: relative; font-size: 20px; margin-left: -5px;">
-          <a class="mal-sync-stream" title="${streamUrl.split('/')[2]}" target="_blank" style="margin: 0 0; display: inline-block;" href="${streamUrl}">
-            <img src="${utils.favicon(streamUrl.split('/')[2])}">
-          </a>
-        </div>`);
-
         var resumeUrlObj = await malObj.getResumeWaching();
         var continueUrlObj = await malObj.getContinueWaching();
         con.log('Resume', resumeUrlObj, 'Continue', continueUrlObj);
         if(typeof continueUrlObj !== 'undefined' && continueUrlObj.ep === (malObj.getEpisode()+1)){
           this.malkiss.continueUrl = continueUrlObj.url;
-          $('#mal-sync-stream-div').append(
-            `<a class="nextStream" title="${api.storage.lang('overview_Continue_'+malObj.type)}" target="_blank" style="display: inline-block; margin: 0; width: 11px; color: #BABABA;" href="${continueUrlObj.url}">
-              <img src="${api.storage.assetUrl('double-arrow-16px.png')}" width="16" height="16">
-            </a>`
-            );
+
         }else if(typeof resumeUrlObj !== 'undefined' && resumeUrlObj.ep === malObj.getEpisode()){
           this.malkiss.resumeUrl = resumeUrlObj.url;
-          $('#mal-sync-stream-div').append(
-            `<a class="resumeStream" title="${api.storage.lang('overview_Resume_Episode_'+malObj.type)}" target="_blank" style="display: inline-block; margin: 0; width: 11px; color: #BABABA;" href="${resumeUrlObj.url}">
-              <img src="${api.storage.assetUrl('arrow-16px.png')}" width="16" height="16">
-            </a>`
-            );
         }
-
-        try{
-          window.dispatchEvent(new Event('resize'));
-        }catch(e){
-          con.info('Resize failed', e)
-        }
-
       });
     }
   }
