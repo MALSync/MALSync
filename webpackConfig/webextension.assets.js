55c16fbc62333efad9b857ea97dbf587cfbcb1e1
lolamtisch <francisco.seipel@yahoo.de>
2019-07-27 18:52:40 +0200

    [FEATURE] Add page showing page feature support

--- webpackConfig/webextension.assets.js
+++ webpackConfig/webextension.assets.js
@@ -277,3 +277,65 @@ mkdirp(path.join(__dirname, '../dist/webextension'), (err) => {
   }
 
 });
+
+
+// Supported pages list
+require('ts-node').register({
+  "project": "./tsconfig.node.json",
+  "files": "./globals.d.ts"
+})
+
+var pages = Object.values(require("./../src/pages/pages.ts").pages);
+
+createTable();
+function createTable(){
+  pages.sort(function(a, b) {
+    var textA = a.name.toUpperCase();
+    var textB = b.name.toUpperCase();
+    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+  });
+
+  var html = `
+  <table>
+    <thead>
+      <tr>
+        <th>Page</th>
+        <th>Overview Page</th>
+        <th>Next Episode</th>
+        <th>Database Support</th>
+        <th>Update Check</th>
+      </tr>
+    </thead>
+    <tbody>
+      `;
+  for (var page in pages){
+    page = pages[page];
+
+    if(typeof page.domain === 'object') page.domain = page.domain[0];
+
+    html += `<tr>
+              <td><a href="${page.domain}"><img src="https://www.google.com/s2/favicons?domain=${page.domain}"> ${page.name}</a></td>
+              ${rowCondition(typeof page.overview !== 'undefined')}
+              ${rowCondition(typeof page.sync.nextEpUrl !== 'undefined')}
+              ${rowCondition(typeof page.database !== 'undefined')}
+              ${rowCondition(typeof page.overview !== 'undefined' && typeof page.overview.list !== 'undefined')}
+            </tr>`;
+  }
+  html += `
+    </tbody>
+  </table>
+  `;
+
+  fs.writeFile(path.join(__dirname, '../pages.md'), html, (err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+
+  function rowCondition(con){
+    if(con) return '<td>:heavy_check_mark:</td>';
+    return '<td>:x:</td>';
+  }
+}
+
