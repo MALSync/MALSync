cec26ec8ee25f3afc7b62f349292ecd8ced4ecbc
lolamtisch <francisco.seipel@yahoo.de>
2019-08-14 13:56:55 +0200

    [BUGFIX] Fix error

--- src/pages/syncPage.ts
+++ src/pages/syncPage.ts
@@ -4,6 +4,8 @@ import {initIframeModal} from "./../minimal/iframe";
 import {providerTemplates} from "./../provider/templates";
 import {getPlayerTime} from "./../utils/player";
 
+declare var browser: any;
+
 var extensionId = "agnaejlkbiiggajjmnpmeheigkflbnoo"; //Chrome
 if(typeof browser !== 'undefined' && typeof chrome !== "undefined"){
   extensionId = "{57081fef-67b4-482f-bcb0-69296e63ec4f}"; //Firefox
