a713bc4ca2f34ede34ee07a2cc7240b7c8c79046
orell <22085664+henrik9999@users.noreply.github.com>
2019-10-05 16:37:11 +0200

    add rating after finish (#203)
    * show rating option
    
    * add finish score
    
    * change selector
    
    * style changes

--- src/pages/syncPage.ts
+++ src/pages/syncPage.ts
@@ -427,9 +427,27 @@ export class syncPage{
     this.malObj.setStartingDateToNow();
 
     if(this.malObj.getStatus() !== status.completed && parseInt(state.episode) === this.malObj.totalEp && parseInt(state.episode) != 0 ){
-      if (await utils.flashConfirm(api.storage.lang("syncPage_flashConfirm_complete"), 'complete')) {
+      if (await utils.flashConfirm(api.storage.lang("syncPage_flashConfirm_complete")+
+        `<div><select id="finish_score" style="margin-top:5px; color:white; background-color:#4e4e4e; border: none;">
+        <option value="0" selected="selected">${api.storage.lang("UI_Score_Not_Rated")}</option>
+        <option value="10">${api.storage.lang("UI_Score_Masterpiece")}</option>
+        <option value="9">${api.storage.lang("UI_Score_Great")}</option>
+        <option value="8">${api.storage.lang("UI_Score_VeryGood")}</option>
+        <option value="7">${api.storage.lang("UI_Score_Good")}</option>
+        <option value="6">${api.storage.lang("UI_Score_Fine")}</option>
+        <option value="5">${api.storage.lang("UI_Score_Average")}</option>
+        <option value="4">${api.storage.lang("UI_Score_Bad")}</option>
+        <option value="3">${api.storage.lang("UI_Score_VeryBad")}</option>
+        <option value="2">${api.storage.lang("UI_Score_Horrible")}</option>
+        <option value="1">${api.storage.lang("UI_Score_Appalling")}</option>
+        </select>
+        </div>`, 'complete')) {
         this.malObj.setStatus(status.completed);
-        this.malObj.setCompletionDateToNow()
+        this.malObj.setCompletionDateToNow();
+        if(j.$("#finish_score").val() !== undefined && j.$("#finish_score").val() > 0) {
+          console.log("finish_score: " + j.$('#finish_score :selected').val());
+          this.malObj.setScore(j.$("#finish_score :selected").val());
+        }
         return true;
       }
     }
