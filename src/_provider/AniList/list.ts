6a2917e50a4028be7a046b64e0b414460049eb73
lolamtisch <francisco.seipel@yahoo.de>
2019-10-06 21:05:08 +0200

    [TASK] Add new userlist provider

---
+++ src/_provider/AniList/list.ts
@@ -0,0 +1,213 @@
+import {ListAbstract, listElement} from './../listAbstract';
+import * as helper from "./helper";
+
+export class userlist extends ListAbstract {
+  public compact = false;
+
+  getUsername() {
+    var query = `
+    query {
+      Viewer {
+        name
+        id
+      }
+    }
+    `;
+
+    return this.api.request.xhr('POST', {
+      url: 'https://graphql.anilist.co',
+      headers: {
+        'Authorization': 'Bearer ' + this.accessToken(),
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      data: JSON.stringify({
+        query: query,
+        variables: []
+      })
+    }).then((response) => {
+      var res = JSON.parse(response.responseText);
+      con.log(res);
+      this.errorHandling(res);
+      return res.data.Viewer.name;
+    });
+  }
+
+  errorHandling(res) {
+    if(typeof res.errors != 'undefined'){
+      con.error(res.errors);
+      throw {
+        code: res.errors[0].status,
+        message: res.errors[0].message,
+      }
+    }
+  }
+
+  accessToken() {
+    return this.api.settings.get('anilistToken');
+  }
+
+  async getPart() {
+    if(this.offset < 1) this.offset = 1;
+    con.log('[UserList][AniList]', 'username: '+this.username, 'status: '+this.status, 'offset: '+this.offset);
+
+    if(this.username == null){
+        this.username = await this.getUsername();
+    }
+
+    var query = `
+    query ($page: Int, $userName: String, $type: MediaType, $status: MediaListStatus, $sort: [MediaListSort] ) {
+      Page (page: $page, perPage: 100) {
+        pageInfo {
+          hasNextPage
+        }
+        mediaList (status: $status, type: $type, userName: $userName, sort: $sort) {
+          status
+          score(format: POINT_100)
+          progress
+          progressVolumes
+          notes
+          media {
+            siteUrl
+            id
+            idMal
+            episodes
+            chapters
+            volumes
+            status
+            averageScore
+            coverImage{
+              large
+            }
+            title {
+              userPreferred
+            }
+          }
+        }
+      }
+    }
+    `;
+
+    if(this.compact){
+      query = `
+      query ($page: Int, $userName: String, $type: MediaType, $status: MediaListStatus, $sort: [MediaListSort]) {
+        Page (page: $page, perPage: 100) {
+          pageInfo {
+            hasNextPage
+          }
+          mediaList (status: $status, type: $type, userName: $userName, sort: $sort) {
+            progress
+            media {
+              id
+              idMal
+            }
+          }
+        }
+      }
+      `;
+    }
+    ​
+    var variables = {
+      page: this.offset,
+      userName: this.username,
+      type: this.listType.toUpperCase(),
+      status: helper.translateList(parseInt(this.status.toString()), parseInt(this.status.toString())),
+      sort: 'UPDATED_TIME_DESC'
+    };
+
+    if(this.status !== 1){
+      //@ts-ignore
+      variables.sort = null;
+    }
+
+    return this.api.request.xhr('POST', {
+      url: 'https://graphql.anilist.co',
+      headers: {
+        'Authorization': 'Bearer ' + this.accessToken(),
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      data: JSON.stringify({
+        query: query,
+        variables: variables
+      })
+    }).then((response) => {
+      var res = JSON.parse(response.responseText);
+      con.log('res', res);
+      this.errorHandling(res);
+      var data = res.data.Page.mediaList;
+      this.offset = this.offset + 1;
+      if(!res.data.Page.pageInfo.hasNextPage){
+        this.done = true;
+      }
+
+      if(this.compact){
+        return this.prepareCompact(data, this.listType);
+      }else{
+        return this.prepareData(data, this.listType);
+      }
+
+    });
+  }
+
+  private prepareData(data, listType): listElement[]{
+    var newData = [] as listElement[];
+    for (var i = 0; i < data.length; i++) {
+      var el = data[i];
+      if(listType === "anime"){
+        var tempData = {
+          uid: el.media.id,
+          malId: el.media.idMal,
+          cacheKey: helper.getCacheKey(el.media.idMal, el.media.id),
+          type: listType,
+          title: el.media.title.userPreferred,
+          url: el.media.siteUrl,
+          watchedEp: el.progress,
+          totalEp: el.media.episodes,
+          status: helper.translateList(el.status),
+          score: Math.round(el.score / 10),
+          image: el.media.coverImage.large,
+          tags: el.notes,
+          airingState: el['anime_airing_status'],
+        }
+      }else{
+        var tempData = {
+          uid: el.media.id,
+          malId: el.media.idMal,
+          cacheKey: helper.getCacheKey(el.media.idMal, el.media.id),
+          type: listType,
+          title: el.media.title.userPreferred,
+          url: el.media.siteUrl,
+          watchedEp: el.progress,
+          totalEp: el.media.chapters,
+          status: helper.translateList(el.status),
+          score: Math.round(el.score / 10),
+          image: el.media.coverImage.large,
+          tags: el.notes,
+          airingState: el['anime_airing_status'],
+        }
+      }
+
+      if(tempData.totalEp == null){
+        tempData.totalEp = 0;
+      }
+
+      newData.push(tempData);
+    }
+    return newData;
+  }
+
+  private prepareCompact(data, listType){
+    var newData = [] as {malid: number, id: number, watchedEp: number, cacheKey: string|number}[];
+    for (var i = 0; i < data.length; i++) {
+      var el = data[i];
+      newData.push({
+        malid: el.media.idMal,
+        id: el.media.id,
+        watchedEp: el.progress,
+        cacheKey: helper.getCacheKey(el.media.idMal, el.media.id),
+      })
+    }
+    return newData;
+  }
+}
---
+++ src/_provider/listAbstract.ts
@@ -0,0 +1,69 @@
+export interface listElement {
+  uid: number,
+  malId: number,
+  cacheKey: any,
+  type: "anime"|"manga"
+  title: string,
+  url: string,
+  watchedEp: number,
+  totalEp: number,
+  status: number,
+  score: number,
+  image: string,
+  tags: string,
+  airingState: number,
+}
+
+export abstract class ListAbstract {
+
+  protected done: boolean = false;
+
+  constructor(
+    protected status: number = 1,
+    protected listType:'anime'|'manga' = 'anime',
+    protected callbacks: {
+      singleCallback?: (el: listElement) => void,
+      continueCall?
+    } = {},
+    protected username = null,
+    protected offset = 0,
+    protected templist: listElement[] = []
+  ) {
+    return this;
+  }
+
+  public api = api;
+
+  async get(): Promise<listElement[]> {
+
+    var retList: listElement[] = [];
+    do {
+      retList = await this.getPart()
+
+      if(typeof this.callbacks.singleCallback !== 'undefined'){
+        // @ts-ignore
+        if(!retList.length) this.callbacks.singleCallback(false, 0, 0);
+        for (var i = 0; i < retList.length; i++) {
+          // @ts-ignore
+          this.callbacks.singleCallback(retList[i]);
+        }
+      }
+
+      this.templist = this.templist.concat(retList);
+
+      if(typeof this.callbacks.continueCall !== 'undefined'){
+        // @ts-ignore
+        await callbacks.continueCall(this.templist);
+      }
+
+
+    } while(!this.done);
+
+    if(typeof this.callbacks.continueCall !== 'undefined') this.callbacks.continueCall(this.templist);
+
+    return this.templist;
+  }
+
+  abstract getUsername(): Promise<String>|String;
+  abstract getPart(): Promise<listElement[]>;
+}
