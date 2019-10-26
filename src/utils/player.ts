f00c20a82d6d630c3f49ddf8363ffb1db8a5827b
lolamtisch <francisco.seipel@yahoo.de>
2019-10-26 11:16:05 +0200

    [TASK] Hide video controls after leaving full screen

--- src/utils/player.ts
+++ src/utils/player.ts
@@ -83,7 +83,21 @@ async function setFullscreen(player){
       }
     }
 
-    if(!found){
+    if(!found && !player.getAttribute('controls')){
+      if (document.addEventListener) {
+        document.addEventListener('fullscreenchange', exitHandler, false);
+        document.addEventListener('mozfullscreenchange', exitHandler, false);
+        document.addEventListener('MSFullscreenChange', exitHandler, false);
+        document.addEventListener('webkitfullscreenchange', exitHandler, false);
+      }
+
+      function exitHandler() {
+        //@ts-ignore
+        if (document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement !== null) {
+          player.removeAttribute("controls","controls");
+        }
+      }
+
       player.setAttribute("controls","controls");
     }
 
