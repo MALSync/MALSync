a8c7401ec14269de294ed341c72d58a66dac1644
lolamtisch <francisco.seipel@yahoo.de>
2020-02-03 22:54:41 +0100

    [TASK] Add search and styling

---
+++ src/_provider/Search/components/search.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="search">
+    <div v-show="loading" class="mdl-progress mdl-js-progress mdl-progress__indeterminate" style="width: 100%; position: absolute;"></div>
+    <slot></slot>
+    <div class="results">
+      <a v-for="item in items" :key="item.id" class="result" :href="item.url" @click="clickItem($event, item)">
+        <div class="image"><img :src="item.image"></img></div>
+        <div class="right">
+          <span class="title">{{item.name}}</span>
+          <p>{{lang("search_Type")}} {{item.media_type}}</p>
+          <p>{{lang("search_Score")}} {{item.score}}</p>
+          <p>{{lang("search_Year")}} {{item.year}}</p>
+        </div>
+      </a>
+
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import {search}  from "./../../../provider/provider";
+  export default {
+    components: {
+    },
+    data: function(){
+      return {
+        items: [],
+        loading: true,
+      }
+    },
+    props: {
+      type: {
+        type: String,
+        default: 'anime'
+      },
+      keyword: {
+        type: String,
+        default: ''
+      },
+    },
+    mounted: function(){
+      this.load();
+    },
+    activated: function(){
+    },
+    watch: {
+      keyword: function(type){
+        this.load();
+      },
+      type: function(type){
+        this.load();
+      }
+    },
+    methods: {
+      lang: api.storage.lang,
+      load: function(){
+        this.loading = true;
+
+        search(this.keyword, this.type).then((items) => {
+          this.loading = false;
+          this.items = items;
+        })
+      },
+      clickItem: function(e, item){
+        e.preventDefault();
+        this.$emit('clicked', item);
+      }
+    }
+  }
+</script>
--- src/_provider/Search/correctionApp.vue
+++ src/_provider/Search/correctionApp.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="material">
     <div>
-      material
+      <search></search>
     </div>
   </div>
 </template>
 
 <script type="text/javascript">
+  import search from './components/search.vue'
+
   export default {
     components: {
+      search
     },
     data: () => ({
     }),
