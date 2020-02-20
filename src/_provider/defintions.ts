f9a94b433e7b1e2b4e7efa9341553a0cd8d3158d
lolamtisch <francisco.seipel@yahoo.de>
2020-02-20 14:52:37 +0100

    [TASK] Add definitions

---
+++ src/_provider/defintions.ts
@@ -0,0 +1,29 @@
+export const enum type {
+  Anime = "anime",
+  Manga = "manga"
+}
+
+export enum status {
+  NoState = 0,
+  Watching = 1,
+  Completed = 2,
+  Onhold = 3,
+  Dropped = 4,
+  PlanToWatch = 6,
+  All = 7,
+  Rewatching = 23,
+}
+
+export const enum score {
+  NoScore = 0,
+  R1 = 1,
+  R2 = 2,
+  R3 = 3,
+  R4 = 4,
+  R5 = 5,
+  R6 = 6,
+  R7 = 7,
+  R8 = 8,
+  R9 = 9,
+  R10 = 10
+}
