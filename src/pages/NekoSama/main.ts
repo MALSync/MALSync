288b1c8ffdc0527fc688911587fb0b51709dd26e
Arias800 <azertypoil630@gmail.com>
2020-04-13 21:07:04 +0200

    Fix NekoSama (#352)
    - Fix next page detection on firefox
    - Add list

--- src/pages/NekoSama/main.ts
+++ src/pages/NekoSama/main.ts
@@ -16,14 +16,26 @@ export const NekoSama: pageInterface = {
     getIdentifier: function(url){return utils.urlPart(url, 5).match(/^\d*/)[0];},
     getOverviewUrl: function(url){return NekoSama.domain+j.$(".details > div > h1 > a").attr('href')},
     getEpisode:function(url){return j.$("#watch > div > div.row.no-gutters.anime-info > div.info > div > div > h2").text().split(" Episode ").pop()},
-    nextEpUrl: function(url){return utils.absoluteLink(j.$("#watch > div > div:nth-child(5) > div.anime-video-options > div.item.right > a.ui.button.small.with-svg-right").attr('href'), NekoSama.domain)},
+    nextEpUrl: function(url){return  utils.absoluteLink(j.$("a.ui:nth-child(2)").attr('href'), NekoSama.domain)},
   },
 
   overview:{
     getTitle: function(url){return utils.getBaseText($('#head > div.content > div > div > div > h1'))},
     getIdentifier: function(url){return NekoSama.sync.getIdentifier(url)},
     uiSelector: function(selector){selector.insertAfter(j.$("#stats > div > div.actions > div").first());},
-  },
+    list:{
+      offsetHandler: false,
+      elementsSelector: function(){
+        return j.$("#stats > div > div.episodes > div.row.no-gutters.js-list-episode-container > div > div > div.text");
+      },
+      elementUrl: function(selector){
+        return utils.absoluteLink(selector.find('a').first().attr('href'), NekoSama.domain);
+      },
+      elementEp: function(selector){
+        return selector.find('a').first().text().replace(/\D+/,"");
+      }
+    }
+    },
 
   init(page){
     if(document.title == "Just a moment..."){
