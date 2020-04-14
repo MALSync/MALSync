3667aba194a9713120d1f738aa5e19e314bea2e2
Arias800 <azertypoil630@gmail.com>
2020-04-14 10:55:42 +0200

    Create main.ts

---
+++ src/pages/OtakuFR/main.ts
@@ -0,0 +1,77 @@
+import { pageInterface } from "./../pageInterface";
+
+export const OtakuFR: pageInterface = {
+  name: "OtakuFR",
+  domain: "https://www.otakufr.com",
+  type: "anime",
+  isSyncPage: function(url) {
+    if (j.$(".vdo_wrp > iframe").length) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  sync: {
+    getTitle: function(url){return j.$("#sct_content > div > ul > li:nth-child(2) > a").text()},
+    getIdentifier: function(url){return utils.urlPart(url, 3)},
+    getOverviewUrl: function(url){return j.$(".breadcrumb > li:nth-child(2) > a").attr('href')},
+    getEpisode:function(url){return j.$("#sct_content > div > div.wpa_pag.anm_pyr > div > ul.nav_eps > li:nth-child(2) > select > option:selected").text().split(" ")[1]},
+    nextEpUrl: function(url){return  utils.absoluteLink(j.$("div.wpa_nav > ul:nth-child(2) > li > a").attr('href'), OtakuFR.domain)},
+  },
+
+  overview:{
+    getTitle: function(url){return utils.getBaseText($('h1.ttl'))},
+    getIdentifier: function(url){return OtakuFR.sync.getIdentifier(url)},
+    uiSelector: function(selector){selector.insertAfter(j.$("#sct_content > div.wpa_pag.anm_det > h1").first());},
+    list:{
+      offsetHandler: true,
+      elementsSelector: function(){
+        return j.$("#sct_content > div.wpa_pag.anm_det > ul > li");
+      },
+      elementUrl: function(selector){
+        return utils.absoluteLink(selector.find('a').first().attr('href'),OtakuFR.domain);
+      },
+      elementEp: function(selector){
+          var url = OtakuFR.overview!.list!.elementUrl(selector);
+          return episodeHelper(urlHandling(url), selector.find('a').text().trim());
+      }
+    }
+    },
+
+  init(page){
+    if(document.title == "Just a moment..."){
+        con.log("loading");
+        page.cdn();
+        return;
+    }
+    api.storage.addStyle(require('!to-string-loader!css-loader!less-loader!./style.less').toString());
+    j.$(document).ready(function(){
+      if($('.vdo_wrp > iframe').length || $('#sct_content > div.wpa_pag.anm_det > ul').length){
+        page.handlePage();
+      }
+    });
+  }
+};
+
+
+function urlHandling(url){
+  var langslug = j.$('#sct_banner_head > div > a').first().attr('href');
+  if(langslug == '/'){
+    return url;
+  }else{
+    return url.replace(langslug, '');
+  }
+
+}
+
+function episodeHelper(url, episodeText){
+  var episodePart = utils.urlPart(urlHandling(url), 1);
+  try{
+    if(/\d+\.\d+/.test(episodeText)){
+      episodePart = 'episode'+episodeText.match(/\d+\.\d+/)[0];
+    }
+  }catch(e){
+    con.error(e);
+  }
+  return episodePart;
+}
