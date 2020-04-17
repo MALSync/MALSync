5d1e5ebd3d5d8c1e37172c7ee3b6bc19be24fc16
Arias800 <azertypoil630@gmail.com>
2020-04-17 13:22:00 +0200

    Update Wakanim
    - Add list
    - Fix uiSelector position

--- src/pages/Wakanim/main.ts
+++ src/pages/Wakanim/main.ts
@@ -18,7 +18,7 @@ export const Wakanim: pageInterface = {
     },
     getOverviewUrl: function(url){return Wakanim.domain+j.$("body > section.episode > div > div > div.episode_info > div.episode_buttons > a:nth-child(2)").attr('href')},
 
-    getEpisode:function(url){return j.$("body > section.episode > div > div > div.episode_info > h1 > span.episode_subtitle > span:nth-child(1) > span").text()
+    getEpisode:function(url){return j.$("body > section.episode > div > div > div.episode_info > h1 > span.episode_subtitle > span > span").text()
     },
 
     nextEpUrl: function(url){return j.$("body > section.episode > div > div > div.episode_main > div.episode_video > div > div.episode-bottom > div.episodeNPEp-wrapperBlock > a.episodeNPEp.episodeNextEp.active").attr('href')},
@@ -29,24 +29,71 @@ export const Wakanim: pageInterface = {
       return url.split("/")[7].trim();
     },
     uiSelector: function(selector){
-      selector.insertBefore(j.$(".SerieV2-content").first());
+      selector.insertBefore(j.$("#nav-show").first());
     },
-  },
-  init(page){
-    if(document.title == "Just a moment..."){
-      con.log("loading");
-      page.cdn();
-      return;
-    }
-    api.storage.addStyle(require('!to-string-loader!css-loader!less-loader!./style.less').toString());
-    j.$(document).ready(function(){
-      if(Wakanim.isSyncPage(page.url)){
-        if (j.$("#jwplayer-container")[0]){
-          page.handlePage();
+      list:{
+        offsetHandler: false,
+        elementsSelector: function(){return j.$("li.-big");},
+        elementUrl: function(selector){return utils.absoluteLink(selector.find('a').attr('href'), Wakanim.domain);},
+        elementEp: function(selector){
+          var url = Wakanim.overview!.list!.elementUrl(selector);
+          return episodeHelper(url, selector.find('a').attr('title').trim());
+        },
+      }
+    },
+
+init(page){
+  if(document.title == "Just a moment..."){
+    con.log("loading");
+    page.cdn();
+    return;
+  }
+  api.storage.addStyle(require('!to-string-loader!css-loader!less-loader!./style.less').toString());
+  if (page.url.split("/")[6] === "show" || page.url.split("/")[6] === "episode") {
+    utils.waitUntilTrue(
+      function() {
+        if (j.$('body > div.SerieV2 > section > div.container > div > div.SerieV2-content').length || j.$('#jwplayer-container').length){
+          return true;
+        } else {
+          return false;
         }
-      }else{
+      },
+      function() {
         page.handlePage();
       }
-    });
+      );
   }
+  utils.urlChangeDetect(function() {
+    page.url = window.location.href;
+    page.UILoaded = false;
+    if (j.$('body > div.SerieV2 > section > div.container > div > div.SerieV2-content').length){
+    	page.handleList();
+    }
+  });
+}
 };
+
+function episodeHelper(url, episodeText){
+  var episodePart = utils.urlPart(url, 8);
+  try{
+    if(/\d+\.\d+/.test(episodeText)){
+      episodePart = 'episode'+episodeText.match(/\d+\.\d+/)[0];
+    }
+  }catch(e){
+    con.error(e);
+  }
+  var temp = [];
+  temp = episodePart.match(/[e,E][p,P][i,I]?[s,S]?[o,O]?[d,D]?[e,E]?\D?\d+/);
+  if(temp !== null){
+    episodePart = temp[0];
+  }else{
+    episodePart = '';
+  }
+  temp = episodePart.match(/\d+/);
+  if(temp === null){
+    episodePart = 1;
+  }else{
+    episodePart = temp[0];
+  }
+  return episodePart;
+}
