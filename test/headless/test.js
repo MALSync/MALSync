8553dd300e3f63a619b78e190ba2d5455827f8f1
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2020-04-30 20:02:49 +0200

    fix 9anime test

--- test/headless/test.js
+++ test/headless/test.js
@@ -98,7 +98,7 @@ var testsArray = [
           title: 'No Game, No Life (Dub)',
           identifier: 'y2p0',
           overviewUrl: 'https://9anime.ru/watch/no-game-no-life-dub.y2p0',
-          nextEpUrl: 'https://9anime.to/watch/no-game-no-life-dub.y2p0/5l0130',
+          nextEpUrl: 'https://9anime.to/watch/no-game-no-life-dub.y2p0/q457ww',
           episode: 4,
           uiSelector: true,
           epList: {
