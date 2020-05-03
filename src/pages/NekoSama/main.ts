5a985d67c37d5b20f98b55abd409b9d794c27701
Arias800 <azertypoil630@gmail.com>
2020-05-03 18:53:34 +0200

    Fix uiSelector and NextEp

--- src/pages/NekoSama/main.ts
+++ src/pages/NekoSama/main.ts
@@ -16,13 +16,13 @@ export const NekoSama: pageInterface = {
     getIdentifier: function(url){return utils.urlPart(url, 5).match(/^\d*/)[0];},
     getOverviewUrl: function(url){return NekoSama.domain+j.$(".details > div > h1 > a").attr('href')},
     getEpisode:function(url){return j.$("#watch > div > div.row.no-gutters.anime-info > div.info > div > div > h2").text().split(" Episode ").pop()},
-    nextEpUrl: function(url){return  utils.absoluteLink(j.$("a.ui:nth-child(2)").attr('href'), NekoSama.domain)},
+    nextEpUrl: function(url){return  utils.absoluteLink(j.$("#watch > div > div:nth-child(2) > div > div.item.right > a.ui.button.small.with-svg-right").attr('href'), NekoSama.domain)},
   },
 
   overview:{
     getTitle: function(url){return utils.getBaseText($('#head > div.content > div > div > div > h1'))},
     getIdentifier: function(url){return NekoSama.sync.getIdentifier(url)},
-    uiSelector: function(selector){selector.insertAfter(j.$("#stats > div > div.actions > div").first());},
+    uiSelector: function(selector){selector.insertAfter(j.$("#head > div.content > div > div > div > div").first());},
     list:{
       offsetHandler: false,
       elementsSelector: function(){
