739c261c6490ccc66b2564cda69a9f5db82bf56a
lolamtisch <francisco.seipel@yahoo.de>
2020-06-15 19:08:29 +0200

    [TASK] Add log module

--- src/utils/consoleBG.ts
+++ src/utils/consoleBG.ts
@@ -24,3 +24,53 @@ export const info = (function() {
     'background-color: wheat; color: black; padding: 2px 10px; border-radius: 3px;',
   );
 })();
+
+export const m = (name, color = '', blocks: { name: string; style: string }[] = []) => {
+  if (!color) color = '#2e51a2';
+  const style = `background-color: ${color}; color: white; padding: 2px 10px; border-radius: 3px; margin-left: -5px; border-left: 1px solid white;`;
+  blocks.push({
+    name,
+    style,
+  });
+
+  const temp: any = {};
+
+  temp.m = (name2, color2 = '') => {
+    return m(name2, color2, blocks);
+  };
+
+  const moduleText = blocks.reduce((sum, el) => `${sum}%c${el.name}`, '');
+  const moduleStyle = blocks.map(el => el.style);
+
+  temp.log = (function() {
+    return Function.prototype.bind.call(
+      console.log,
+      console,
+      `%cMAL-Sync-BG ${moduleText}`,
+      'background-color: #2e51a2; color: white; padding: 2px 10px; border-radius: 3px;',
+      ...moduleStyle,
+    );
+  })();
+
+  temp.error = (function() {
+    return Function.prototype.bind.call(
+      console.error,
+      console,
+      `%cMAL-Sync-BG ${moduleText}`,
+      'background-color: #8f0000; color: white; padding: 2px 10px; border-radius: 3px;',
+      ...moduleStyle,
+    );
+  })();
+
+  temp.info = (function() {
+    return Function.prototype.bind.call(
+      console.info,
+      console,
+      `%cMAL-Sync-BG ${moduleText}`,
+      'background-color: wheat; color: black; padding: 2px 10px; border-radius: 3px;',
+      ...moduleStyle,
+    );
+  })();
+
+  return temp;
+};
