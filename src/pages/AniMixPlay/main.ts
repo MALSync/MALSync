a9066639d107be59c72db52b8dc51d3420f9d6ee
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2020-06-27 22:04:37 +0200

    add list animixplay

--- src/pages/AniMixPlay/main.ts
+++ src/pages/AniMixPlay/main.ts
@@ -30,6 +30,36 @@ export const AniMixPlay: pageInterface = {
       selector.insertAfter(j.$('span.animetitle').first());
     },
   },
+  overview: {
+    getTitle(url) {
+      return '';
+    },
+    getIdentifier(url) {
+      return '';
+    },
+    uiSelector(selector) {
+      // no Ui
+    },
+    list: {
+      offsetHandler: false,
+      elementsSelector() {
+        return j.$('#epslistplace > button');
+      },
+      elementUrl(selector) {
+        const regex = /\/ep\d+$/;
+        const episode = selector.text().replace(/\D+/g, '');
+        const url = window.location.href;
+
+        if (regex.test(url)) {
+          return url.replace(regex, `ep${episode}`);
+        }
+        return `${url}/ep${episode}`;
+      },
+      elementEp(selector) {
+        return episode;
+      },
+    },
+  },
   init(page) {
     api.storage.addStyle(require('!to-string-loader!css-loader!less-loader!./style.less').toString());
 
