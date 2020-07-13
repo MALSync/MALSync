6d861c39227122adaeeaadba96b19cf5dd4e5b37
Sazzo <stolensoul32@gmail.com>
2020-07-13 19:06:03 -0300

    Fix some things

--- src/pages/YayAnimes/main.ts
+++ src/pages/YayAnimes/main.ts
@@ -1,54 +1,56 @@
 import { pageInterface } from '../pageInterface';
 
 export const YayAnimes: pageInterface = {
-    name: 'YayAnimes',
-    domain: 'https://yayanimes.net',
-    type: 'anime',
-    isSyncPage(url) {
-       if(url.includes('episodio')) {
-           return true;
-       }
-       return false;
+  name: 'YayAnimes',
+  domain: 'https://yayanimes.net',
+  type: 'anime',
+  isSyncPage(url) {
+    if (url.includes('episodio')) {
+      return true;
+    }
+    return false;
+  },
+  sync: {
+    getTitle(url) {
+      const content = j.$('#content > div:nth-child(1) > div > h1 > div > b > p > span').text();
+      const title = content.replace(/–.*$/, '');
+      return title.trim();
     },
-    sync: {
-        getTitle(url) {
-            const content = j.$('#content > div:nth-child(1) > div > h1 > div > b > p > span').text()
-            const title = content.replace(/\–.*$/, '')
-            return title.trim()
-        },
-        getIdentifier(url) {
-            return url.split('/')[3]
-        },
-        getOverviewUrl(url) {
-            return `${YayAnimes.domain}/${YayAnimes.sync.getIdentifier(url)}`
-        },
-        getEpisode(url) {
-            if(url.includes('filme')) {
-                return 1
-            }
-            const ep = Number(j
-                .$('#content > div:nth-child(1) > div > h1 > div > b > p > span').text()
-                .match(/\d+$/)
-                )
-            return ep
-        }
+    getIdentifier(url) {
+      return url.split('/')[3];
     },
-    overview: {
-        getTitle(url) {
-            return YayAnimes.sync.getTitle(url);
-        },
-        getIdentifier(url) {
-            return YayAnimes.sync.getIdentifier(url);
-        },
-        uiSelector(selector) {
-            selector.insertAfter('#content > div.contentBox > div > div > div.boxAnimeSobre')
-        }
+    getOverviewUrl(url) {
+      return `${YayAnimes.domain}/${YayAnimes.sync.getIdentifier(url)}`;
     },
-    init(page) {
-        api.storage.addStyle(require('!to-string-loader!css-loader!less-loader!./style.less').toString());
-        utils.fullUrlChangeDetect(function() {
-            page.reset();
-            page.handlePage();
-          });
+    getEpisode(url) {
+      if (url.includes('filme')) {
+        return 1;
+      }
+      const ep = Number(
+        j
+          .$('#content > div:nth-child(1) > div > h1 > div > b > p > span')
+          .text()
+          .match(/\d+$/),
+      );
+      return ep;
     },
+  },
+  overview: {
+    getTitle(url) {
+      return YayAnimes.sync.getTitle(url);
+    },
+    getIdentifier(url) {
+      return YayAnimes.sync.getIdentifier(url);
+    },
+    uiSelector(selector) {
+      selector.insertAfter('#content > div.contentBox > div > div > div.boxAnimeSobre');
+    },
+  },
+  init(page) {
+    api.storage.addStyle(require('!to-string-loader!css-loader!less-loader!./style.less').toString());
+    utils.fullUrlChangeDetect(function() {
+      page.reset();
+      page.handlePage();
+    });
+  },
 };
