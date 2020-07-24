ae62e87b86a53f1eb918bac37a73cffcb6a4096e
lolamtisch <francisco.seipel@yahoo.de>
2020-07-24 10:34:26 +0200

    [TASK] Use dompurify for descriptions for now

--- src/minimal/minimalApp/overview.vue
+++ src/minimal/minimalApp/overview.vue
@@ -74,7 +74,7 @@
           </div>
         </div>
         <div class="malDescription malClear mdl-cell mdl-cell--10-col" style="overflow: hidden;">
-          <p style="color: black;" v-html="description"></p>
+          <p style="color: black;" v-dompurify-html="description"></p>
           <div
             v-if="renderObj && renderObj.isAuthenticated()"
             class="mdl-card__actions mdl-card--border"
