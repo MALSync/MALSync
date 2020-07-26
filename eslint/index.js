fcc813b73edbe81edc1b1a816caf68372af1e6bb
lolamtisch <francisco.seipel@yahoo.de>
2020-07-26 10:25:48 +0200

    [TASK] Add new rules

--- eslint/index.js
+++ eslint/index.js
@@ -2,28 +2,12 @@
 
 module.exports = {
   rules: {
-    'no-after': require('./rules/no-after'),
-    'no-append': require('./rules/no-append'),
-    'no-appendTo': require('./rules/no-appendTo'),
-    'no-before': require('./rules/no-before'),
-    'no-html': require('./rules/no-html'),
-    'no-prepend': require('./rules/no-prepend'),
-    'no-prependTo': require('./rules/no-prependTo'),
-    'no-insertAfter': require('./rules/no-insertAfter'),
-    'no-insertBefore': require('./rules/no-insertBefore'),
+    'no-xss-jquery': require('./rules/no-xss-jquery'),
   },
   configs: {
     default: {
       rules: {
-        "no-after": 2,
-        "no-append": 2,
-        "no-appendTo": 2,
-        "no-before": 2,
-        "no-html": 2,
-        "no-prepend": 2,
-        "no-prependTo": 2,
-        "no-insertAfter": 2,
-        "no-insertBefore": 2,
+        "no-xss-jquery": 2,
       }
     }
   }
--- src/utils/j.ts
+++ src/utils/j.ts
@@ -1,1 +1,7 @@
+import * as DOMPurify from 'dompurify';
+
 export const $ = jQuery;
+
+export function html(html) {
+  return DOMPurify.sanitize(html, { SAFE_FOR_JQUERY: true });
+}
