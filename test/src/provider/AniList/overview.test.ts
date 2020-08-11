79dfc39bb9f1bc4b1fccb45548a5721c8731f843
lolamtisch <francisco.seipel@yahoo.de>
2020-08-11 09:55:09 +0200

    [TASK] Fix metadata tests

--- test/src/provider/AniList/overview.test.ts
+++ test/src/provider/AniList/overview.test.ts
@@ -47,62 +47,187 @@ describe('AniList overview', function() {
   });
   let resObj = {
     title: 'No Game No Life',
-    alternativeTitle: [ 'No Game, No Life', 'ノーゲーム・ノーライフ' ],
-    description: "<p>The story of No Game, No Life centers around Sora and Shiro, a brother and sister whose reputations as brilliant NEET (Not in Education, Employment, or Training) hikikomori (shut-in) gamers have spawned urban legends all over the Internet. These two gamers even consider the real world as just another &quot;crappy game.&quot; One day, they are summoned by a boy named &quot;God&quot; to an alternate world. There, God has prohibited war and declared this to be a world where &quot;everything is decided by games&quot; - even national borders. Humanity has been driven back into one remaining city by the other races. Will Sora and Shiro, the good-for-nothing brother and sister, become the &quot;Saviors of Humanity&quot; on this alternate world? &quot;Well, let's start playing.&quot;<br><br><br />\n" +
-      '(Source: Anime News Network)</p>',
+    alternativeTitle: ['No Game, No Life', 'ノーゲーム・ノーライフ'],
+    description:
+      '<p>The story of No Game, No Life centers around Sora and Shiro, a brother and sister whose reputations as brilliant NEET (Not in Education, Employment, or Training) hikikomori (shut-in) gamers have spawned urban legends all over the Internet. These two gamers even consider the real world as just another &quot;crappy game.&quot;<br><br><br />\nOne day, they are summoned to another world where a young boy named Tet appears before them, claiming to be the “God” of this world. In this world populated by magical creatures, violence is forbidden, humanity is on the brink of extinction and all matters are settled through games. Using their combined intellect, questionable morals and considerable guile, it is up to these siblings to swindle their way to the top in a series of increasingly intriguing and deceptively deadly games.<br><br><br />\n(Source: Anime News Network)</p>',
     image: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/nx19815-bIo51RMWWhLv.jpg',
     characters: [
       {
         img: 'https://s4.anilist.co/file/anilistcdn/character/large/n82523-W3XG4FLDGSQZ.jpg',
-        html: '<a href="https://anilist.co/character/82523">Sora</a>'
+        name: 'Sora',
+        url: 'https://anilist.co/character/82523',
       },
       {
         img: 'https://s4.anilist.co/file/anilistcdn/character/large/n82525-Le2tc8f6OQ0w.jpg',
-        html: '<a href="https://anilist.co/character/82525">Shiro</a>'
+        name: 'Shiro',
+        url: 'https://anilist.co/character/82525',
       },
       {
         img: 'https://s4.anilist.co/file/anilistcdn/character/large/b82527-vnRsuLvsinP3.png',
-        html: '<a href="https://anilist.co/character/82527">Dola, Stephanie</a>'
+        name: 'Dola, Stephanie',
+        url: 'https://anilist.co/character/82527',
       },
       {
         img: 'https://s4.anilist.co/file/anilistcdn/character/large/n87887-Qia1XfXbLP4u.jpg',
-        html: '<a href="https://anilist.co/character/87887">Jibril</a>'
+        name: 'Jibril',
+        url: 'https://anilist.co/character/87887',
       },
       {
         img: 'https://s4.anilist.co/file/anilistcdn/character/large/88001.jpg',
-        html: '<a href="https://anilist.co/character/88001">Zell, Chlammy</a>'
+        name: 'Zell, Chlammy',
+        url: 'https://anilist.co/character/88001',
       },
       {
         img: 'https://s4.anilist.co/file/anilistcdn/character/large/88002.jpg',
-        html: '<a href="https://anilist.co/character/88002">Nilvalen, Fiel</a>'
-      }
+        name: 'Nilvalen, Fiel',
+        url: 'https://anilist.co/character/88002',
+      },
     ],
     statistics: [
       { title: 'Score:', body: 80 },
       { title: 'Favourites:', body: 6930 },
       { title: 'Popularity:', body: 149219 },
-      { title: 'Highest rated:', body: '#176' }
+      { title: 'Highest rated:', body: '#176' },
     ],
     info: [
-      { title: 'Format:', body: 'Tv' },
-      { title: 'Episodes:', body: 12 },
-      { title: 'Episode Duration:', body: '24 mins' },
-      { title: 'Status:', body: 'Finished' },
-      { title: 'Start Date:', body: '2014-4-9' },
-      { title: 'End Date:', body: '2014-6-25' },
-      { title: 'Season:', body: 'Spring 2014' },
       {
-        title: 'Studios:',
-        body: '<a href="https://anilist.co/studio/11">MADHOUSE</a>'
-      },
-      { title: 'Source:', body: 'Light novel' },
-      {
-        title: 'Genres:',
-        body: '<a href="https://anilist.co/search/anime?includedGenres=Adventure">Adventure</a>, <a href="https://anilist.co/search/anime?includedGenres=Comedy">Comedy</a>, <a href="https://anilist.co/search/anime?includedGenres=Ecchi">Ecchi</a>, <a href="https://anilist.co/search/anime?includedGenres=Fantasy">Fantasy</a>'
+        "title": "Format:",
+        "body": [
+          {
+            "text": "Tv"
+          }
+        ]
       },
       {
-        title: 'External Links:',
-        body: '<a href="http://ngnl.jp/">Official Site</a>, <a href="http://www.crunchyroll.com/no-game-no-life">Crunchyroll</a>, <a href="http://www.hulu.com/no-game-no-life">Hulu</a>, <a href="https://twitter.com/ngnl_anime">Twitter</a>, <a href="https://www.animelab.com/shows/no-game-no-life">AnimeLab</a>, <a href="https://www.hidive.com/tv/no-game-no-life">Hidive</a>, <a href="https://www.netflix.com/title/80052669">Netflix</a>, <a href="https://vrv.co/series/G6ZJ48K4Y/No-Game-No-Life">VRV</a>, <a href="https://www.contv.com/details-movie/1323-19619-000/no-game-no-life">CONtv</a>, <a href="https://www.youtube.com/playlist?list=PLxSscENEp7JglGq-5PMtbewK96Z_7Au8J">Youtube</a>'
+        "title": "Episodes:",
+        "body": [
+          {
+            "text": 12
+          }
+        ]
+      },
+      {
+        "title": "Episode Duration:",
+        "body": [
+          {
+            "text": "24 mins"
+          }
+        ]
+      },
+      {
+        "title": "Status:",
+        "body": [
+          {
+            "text": "Finished"
+          }
+        ]
+      },
+      {
+        "title": "Start Date:",
+        "body": [
+          {
+            "text": "2014-4-9"
+          }
+        ]
+      },
+      {
+        "title": "End Date:",
+        "body": [
+          {
+            "text": "2014-6-25"
+          }
+        ]
+      },
+      {
+        "title": "Season:",
+        "body": [
+          {
+            "text": "Spring 2014"
+          }
+        ]
+      },
+      {
+        "title": "Studios:",
+        "body": [
+          {
+            "text": "MADHOUSE",
+            "url": "https://anilist.co/studio/11"
+          }
+        ]
+      },
+      {
+        "title": "Source:",
+        "body": [
+          {
+            "text": "Light novel"
+          }
+        ]
+      },
+      {
+        "title": "Genres:",
+        "body": [
+          {
+            "text": "Adventure",
+            "url": "https://anilist.co/search/anime?includedGenres=Adventure"
+          },
+          {
+            "text": "Comedy",
+            "url": "https://anilist.co/search/anime?includedGenres=Comedy"
+          },
+          {
+            "text": "Ecchi",
+            "url": "https://anilist.co/search/anime?includedGenres=Ecchi"
+          },
+          {
+            "text": "Fantasy",
+            "url": "https://anilist.co/search/anime?includedGenres=Fantasy"
+          }
+        ]
+      },
+      {
+        "title": "External Links:",
+        "body": [
+          {
+            "text": "Official Site",
+            "url": "http://ngnl.jp/"
+          },
+          {
+            "text": "Crunchyroll",
+            "url": "http://www.crunchyroll.com/no-game-no-life"
+          },
+          {
+            "text": "Hulu",
+            "url": "http://www.hulu.com/no-game-no-life"
+          },
+          {
+            "text": "Twitter",
+            "url": "https://twitter.com/ngnl_anime"
+          },
+          {
+            "text": "AnimeLab",
+            "url": "https://www.animelab.com/shows/no-game-no-life"
+          },
+          {
+            "text": "Hidive",
+            "url": "https://www.hidive.com/tv/no-game-no-life"
+          },
+          {
+            "text": "Netflix",
+            "url": "https://www.netflix.com/title/80052669"
+          },
+          {
+            "text": "VRV",
+            "url": "https://vrv.co/series/G6ZJ48K4Y/No-Game-No-Life"
+          },
+          {
+            "text": "CONtv",
+            "url": "https://www.contv.com/details-movie/1323-19619-000/no-game-no-life"
+          },
+          {
+            "text": "Youtube",
+            "url": "https://www.youtube.com/playlist?list=PLxSscENEp7JglGq-5PMtbewK96Z_7Au8J"
+          }
+        ]
       }
     ],
     openingSongs: [],
@@ -110,38 +235,46 @@ describe('AniList overview', function() {
     related: [
       {
         type: 'Side story',
-        links: [{
-          "statusTag": "",
-          "title": "No Game No Life Specials",
-          "url": "https://anilist.co/anime/20769"
-        }]
+        links: [
+          {
+            statusTag: '',
+            title: 'No Game No Life Specials',
+            url: 'https://anilist.co/anime/20769',
+          },
+        ],
       },
       {
         type: 'Source',
-        links: [{
-          "statusTag": "",
-          "title": "No Game No Life",
-          "url": "https://anilist.co/manga/78399",
-        }]
+        links: [
+          {
+            statusTag: '',
+            title: 'No Game No Life',
+            url: 'https://anilist.co/manga/78399',
+          },
+        ],
       },
       {
         type: 'Prequel',
-        links: [{
-          "statusTag": "",
-          "title": "No Game No Life Zero",
-          "url": "https://anilist.co/anime/21875",
-        }]
+        links: [
+          {
+            statusTag: '',
+            title: 'No Game No Life Zero',
+            url: 'https://anilist.co/anime/21875',
+          },
+        ],
       },
       {
         type: 'Alternative',
-        links: [{
-          "statusTag": "",
-          "title": "No Game No Life",
-          "url": "https://anilist.co/manga/78397",
-        }]
-      }
-    ]
-  }
+        links: [
+          {
+            statusTag: '',
+            title: 'No Game No Life',
+            url: 'https://anilist.co/manga/78397',
+          },
+        ],
+      },
+    ],
+  };
   let tests = [
     {
       url: 'https://anilist.co/anime/19815/No-Game-No-Life/',
--- test/src/provider/Kitsu/overview.test.ts
+++ test/src/provider/Kitsu/overview.test.ts
@@ -70,147 +70,211 @@ describe('Kitsu overview', function() {
     setGlobals();
   });
   let resObj = {
-    "title": "No Game, No Life",
-    "alternativeTitle": [
-      "NGNL",
-      "No Game No Life",
-      "ノーゲーム・ノーライフ"
+    title: 'No Game, No Life',
+    alternativeTitle: ['NGNL', 'No Game No Life', 'ノーゲーム・ノーライフ'],
+    description:
+      '<span style="white-space: pre-line;">No Game No Life is a surreal comedy that follows Sora and Shiro, shut-in NEET siblings and the online gamer duo behind the legendary username "Kuuhaku." They view the real world as just another lousy game; however, a strange e-mail challenging them to a chess match changes everything the brother and sister are plunged into an otherworldly realm where they meet Tet, the God of Games.\r\n\r\nThe mysterious god welcomes Sora and Shiro to Disboard, a world where all forms of conflict—from petty squabbles to the fate of whole countries—are settled not through war, but by way of high-stake games. This system works thanks to a fundamental rule wherein each party must wager something they deem to be of equal value to the other party\'s wager. In this strange land where the very idea of humanity is reduced to child’s play, the indifferent genius gamer duo of Sora and Shiro have finally found a real reason to keep playing games: to unite the sixteen races of Disboard, defeat Tet, and become the gods of this new, gaming-is-everything world.\r\n\r\n(Source: MAL Rewrite)</span>',
+    image: 'https://media.kitsu.io/anime/poster_images/7880/large.jpg?1418914006',
+    characters: [
+      {
+        img: 'https://media.kitsu.io/characters/images/82860/original.jpg?1485083465',
+        name: 'Shiro',
+        url: 'https://myanimelist.net/character/82525',
+      },
+      {
+        img: 'https://media.kitsu.io/characters/images/78882/original.jpg?1485081870',
+        name: 'Sora',
+        url: 'https://myanimelist.net/character/82523',
+      },
+      {
+        img: 'https://media.kitsu.io/characters/images/40375/original.jpg?1483096805',
+        name: 'Former King',
+        url: 'https://myanimelist.net/character/105919',
+      },
+      {
+        img: 'https://media.kitsu.io/characters/images/40378/original.jpg?1483096805',
+        name: 'Ino Hatsuse',
+        url: 'https://myanimelist.net/character/104849',
+      },
+      {
+        img: 'https://media.kitsu.io/characters/images/40377/original.jpg?1483096805',
+        name: 'Izuna Hatsuse',
+        url: 'https://myanimelist.net/character/97767',
+      },
+      {
+        img: 'https://media.kitsu.io/characters/images/40373/original.jpg?1483096805',
+        name: 'Jibril',
+        url: 'https://myanimelist.net/character/97761',
+      },
+      {
+        img: 'https://media.kitsu.io/characters/images/92594/original.jpg?1485087372',
+        name: 'Miko',
+        url: 'https://myanimelist.net/character/106839',
+      },
+      {
+        img: 'https://media.kitsu.io/characters/images/40379/original.jpg?1483096805',
+        name: 'Feel Nilvalen',
+        url: 'https://myanimelist.net/character/97765',
+      },
+      {
+        img: 'https://media.kitsu.io/characters/images/75916/original.jpg?1485080815',
+        name: 'Queen',
+        url: 'https://myanimelist.net/character/117075',
+      },
+      {
+        img: 'https://media.kitsu.io/characters/images/40374/original.jpg?1483096805',
+        name: 'Teto',
+        url: 'https://myanimelist.net/character/97769',
+      },
     ],
-    "description": "<span style=\"white-space: pre-line;\">No Game No Life is a surreal comedy that follows Sora and Shiro, shut-in NEET siblings and the online gamer duo behind the legendary username \"Kuuhaku.\" They view the real world as just another lousy game; however, a strange e-mail challenging them to a chess match changes everything the brother and sister are plunged into an otherworldly realm where they meet Tet, the God of Games.\r\n\r\nThe mysterious god welcomes Sora and Shiro to Disboard, a world where all forms of conflict—from petty squabbles to the fate of whole countries—are settled not through war, but by way of high-stake games. This system works thanks to a fundamental rule wherein each party must wager something they deem to be of equal value to the other party's wager. In this strange land where the very idea of humanity is reduced to child’s play, the indifferent genius gamer duo of Sora and Shiro have finally found a real reason to keep playing games: to unite the sixteen races of Disboard, defeat Tet, and become the gods of this new, gaming-is-everything world.\r\n\r\n(Source: MAL Rewrite)</span>",
-    "image": "https://media.kitsu.io/anime/poster_images/7880/large.jpg?1418914006",
-    "characters": [
+    statistics: [
       {
-        "img": "https://media.kitsu.io/characters/images/82860/original.jpg?1485083465",
-        "html": "<a href=\"https://myanimelist.net/character/82525\">Shiro</a>"
+        title: 'Score:',
+        body: '82.55',
       },
       {
-        "img": "https://media.kitsu.io/characters/images/78882/original.jpg?1485081870",
-        "html": "<a href=\"https://myanimelist.net/character/82523\">Sora</a>"
+        title: 'Ranked:',
+        body: '#39',
       },
       {
-        "img": "https://media.kitsu.io/characters/images/40375/original.jpg?1483096805",
-        "html": "<a href=\"https://myanimelist.net/character/105919\">Former King</a>"
+        title: 'Popularity:',
+        body: '#14',
       },
       {
-        "img": "https://media.kitsu.io/characters/images/40378/original.jpg?1483096805",
-        "html": "<a href=\"https://myanimelist.net/character/104849\">Ino Hatsuse</a>"
+        title: 'Members:',
+        body: 163461,
       },
-      {
-        "img": "https://media.kitsu.io/characters/images/40377/original.jpg?1483096805",
-        "html": "<a href=\"https://myanimelist.net/character/97767\">Izuna Hatsuse</a>"
-      },
-      {
-        "img": "https://media.kitsu.io/characters/images/40373/original.jpg?1483096805",
-        "html": "<a href=\"https://myanimelist.net/character/97761\">Jibril</a>"
-      },
-      {
-        "img": "https://media.kitsu.io/characters/images/92594/original.jpg?1485087372",
-        "html": "<a href=\"https://myanimelist.net/character/106839\">Miko</a>"
-      },
-      {
-        "img": "https://media.kitsu.io/characters/images/40379/original.jpg?1483096805",
-        "html": "<a href=\"https://myanimelist.net/character/97765\">Feel Nilvalen</a>"
-      },
-      {
-        "img": "https://media.kitsu.io/characters/images/75916/original.jpg?1485080815",
-        "html": "<a href=\"https://myanimelist.net/character/117075\">Queen</a>"
-      },
-      {
-        "img": "https://media.kitsu.io/characters/images/40374/original.jpg?1483096805",
-        "html": "<a href=\"https://myanimelist.net/character/97769\">Teto</a>"
-      }
     ],
-    "statistics": [
+    info: [
       {
-        "title": "Score:",
-        "body": "82.55"
-      },
-      {
-        "title": "Ranked:",
-        "body": "#39"
-      },
-      {
-        "title": "Popularity:",
-        "body": "#14"
-      },
-      {
-        "title": "Members:",
-        "body": 163461
-      }
-    ],
-    "info": [
-      {
-        "title": "Format:",
-        "body": "Tv"
-      },
-      {
-        "title": "Episodes:",
-        "body": 12
-      },
-      {
-        "title": "Episode Duration:",
-        "body": "23 mins"
-      },
-      {
-        "title": "Status:",
-        "body": "Finished"
-      },
-      {
-        "title": "Start Date:",
-        "body": "2014-04-09"
-      },
-      {
-        "title": "Start Date:",
-        "body": "2014-06-25"
-      },
-      {
-        "title": "Genres:",
-        "body": "<a href=\"https://kitsu.io/anime?categories=isekai\">Isekai</a>, <a href=\"https://kitsu.io/anime?categories=comedy\">Comedy</a>, <a href=\"https://kitsu.io/anime?categories=fantasy\">Fantasy</a>, <a href=\"https://kitsu.io/anime?categories=ecchi\">Ecchi</a>, <a href=\"https://kitsu.io/anime?categories=fantasy-world\">Fantasy World</a>, <a href=\"https://kitsu.io/anime?categories=parody\">Parody</a>"
-      },
-      {
-        "title": "Rating:",
-        "body": "PG"
-      },
-      {
-        "title": "Total playtime:",
-        "body": "276 mins"
-      }
-    ],
-    "openingSongs": [],
-    "endingSongs": [],
-    "related": [
-      {
-        "type": "Adaptation",
-        "links": [
+        title: 'Format:',
+        body: [
           {
-            "url": "https://kitsu.io/manga/no-game-no-life-novel",
-            "title": "No Game No Life",
-            "statusTag": ""
-          }
-        ]
+            text: 'Tv',
+          },
+        ],
       },
       {
-        "type": "Side story",
-        "links": [
+        title: 'Episodes:',
+        body: [
           {
-            "url": "https://kitsu.io/anime/no-game-no-life-specials",
-            "title": "No Game No Life Specials",
-            "statusTag": ""
-          }
-        ]
+            text: 12,
+          },
+        ],
       },
       {
-        "type": "Prequel",
-        "links": [
+        title: 'Episode Duration:',
+        body: [
           {
-            "url": "https://kitsu.io/anime/no-game-no-life-zero",
-            "title": "No Game No Life: Zero",
-            "statusTag": ""
-          }
-        ]
-      }
-    ]
-  }
+            text: '23 mins',
+          },
+        ],
+      },
+      {
+        title: 'Status:',
+        body: [
+          {
+            text: 'Finished',
+          },
+        ],
+      },
+      {
+        title: 'Start Date:',
+        body: [
+          {
+            text: '2014-04-09',
+          },
+        ],
+      },
+      {
+        title: 'Start Date:',
+        body: [
+          {
+            text: '2014-06-25',
+          },
+        ],
+      },
+      {
+        title: 'Genres:',
+        body: [
+          {
+            text: 'Isekai',
+            url: 'https://kitsu.io/anime?categories=isekai',
+          },
+          {
+            text: 'Comedy',
+            url: 'https://kitsu.io/anime?categories=comedy',
+          },
+          {
+            text: 'Fantasy',
+            url: 'https://kitsu.io/anime?categories=fantasy',
+          },
+          {
+            text: 'Ecchi',
+            url: 'https://kitsu.io/anime?categories=ecchi',
+          },
+          {
+            text: 'Fantasy World',
+            url: 'https://kitsu.io/anime?categories=fantasy-world',
+          },
+          {
+            text: 'Parody',
+            url: 'https://kitsu.io/anime?categories=parody',
+          },
+        ],
+      },
+      {
+        title: 'Rating:',
+        body: [
+          {
+            text: 'PG',
+          },
+        ],
+      },
+      {
+        title: 'Total playtime:',
+        body: [
+          {
+            text: '276 mins',
+          },
+        ],
+      },
+    ],
+    openingSongs: [],
+    endingSongs: [],
+    related: [
+      {
+        type: 'Adaptation',
+        links: [
+          {
+            url: 'https://kitsu.io/manga/no-game-no-life-novel',
+            title: 'No Game No Life',
+            statusTag: '',
+          },
+        ],
+      },
+      {
+        type: 'Side story',
+        links: [
+          {
+            url: 'https://kitsu.io/anime/no-game-no-life-specials',
+            title: 'No Game No Life Specials',
+            statusTag: '',
+          },
+        ],
+      },
+      {
+        type: 'Prequel',
+        links: [
+          {
+            url: 'https://kitsu.io/anime/no-game-no-life-zero',
+            title: 'No Game No Life: Zero',
+            statusTag: '',
+          },
+        ],
+      },
+    ],
+  };
   let tests = [
     {
       url: 'https://kitsu.io/anime/no-game-no-life',
