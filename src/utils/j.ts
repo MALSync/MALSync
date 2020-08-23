d90807268d64b39fa1c69709502f8156205bed4b
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2020-08-23 19:01:49 +0200

    dompurify allow attr target

--- src/utils/j.ts
+++ src/utils/j.ts
@@ -3,5 +3,9 @@ import * as DOMPurify from 'dompurify';
 export const $ = jQuery;
 
 export function html(htmlContent) {
-  return DOMPurify.sanitize(htmlContent, { SAFE_FOR_JQUERY: true, ALLOW_UNKNOWN_PROTOCOLS: true });
+  return DOMPurify.sanitize(htmlContent, {
+    SAFE_FOR_JQUERY: true,
+    ALLOW_UNKNOWN_PROTOCOLS: true,
+    ADD_ATTR: ['target'],
+  });
 }
