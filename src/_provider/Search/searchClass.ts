3d13ba14ce86868262c2a934bacd90e191ca9ed3
lolamtisch <francisco.seipel@yahoo.de>
2020-09-02 17:03:05 +0200

    [TASK] Truncate search title after 99 chars

--- src/_provider/Search/searchClass.ts
+++ src/_provider/Search/searchClass.ts
@@ -125,6 +125,7 @@ export class searchClass {
     let resTitle = title.replace(/ *(\(dub\)|\(sub\)|\(uncensored\)|\(uncut\)|\(subbed\)|\(dubbed\))/i, '');
     resTitle = resTitle.replace(/ *\([^)]+audio\)/i, '');
     resTitle = resTitle.replace(/ BD( |$)/i, '');
+    resTitle = resTitle.substring(0, 99); // truncate
     resTitle = resTitle.trim();
     return resTitle;
   }
