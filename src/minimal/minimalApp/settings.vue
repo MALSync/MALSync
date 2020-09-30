a6378124d7c8c04ea2ada8d7348a9268d2d13e20
lolamtisch <francisco.seipel@yahoo.de>
2020-09-30 09:51:49 +0200

    [TASK] Hide option in extension

--- src/minimal/minimalApp/settings.vue
+++ src/minimal/minimalApp/settings.vue
@@ -207,7 +207,7 @@
         <checkbox option="minimizeBigPopup">{{ lang('settings_miniMAL_minimizeBigPopup') }}</checkbox>
         <checkbox option="floatButtonCorrection">{{ lang('settings_miniMAL_floatButtonCorrection') }}</checkbox>
         <checkbox option="floatButtonHide">{{ lang('settings_miniMAL_floatButtonHide') }}</checkbox>
-        <checkbox option="autoCloseMinimal">{{ lang('settings_miniMAL_autoCloseMinimal') }}</checkbox>
+        <checkbox v-if="!isExtension()" option="autoCloseMinimal">{{ lang('settings_miniMAL_autoCloseMinimal') }}</checkbox>
 
         <li v-if="commands" class="mdl-list__item">
           <span class="mdl-list__item-primary-content">
