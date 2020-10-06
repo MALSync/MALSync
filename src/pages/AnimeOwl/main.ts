bcf8fd1f37e7f1d508374d27a6820a54ff4c527f
lolamtisch <francisco.seipel@yahoo.de>
2020-10-06 11:48:05 +0200

    [TASK] Fix styling

--- src/pages/AnimeOwl/main.ts
+++ src/pages/AnimeOwl/main.ts
@@ -66,7 +66,7 @@ export const AnimeOwl: pageInterface = {
     uiSelector(selector) {
       j.$('div.post-info > div:nth-child(2) > div.row:nth-child(1)')
         .first()
-        .after(j.html(`<div class="row">${selector}</div>`));
+        .after(j.html(`<div class="row"><div class="col-12">${selector}</div></div>`));
     },
     list: {
       offsetHandler: false,
