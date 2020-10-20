fa6c0cef4ec86b55c1c0b55fc983472bee837b5b
lolamtisch <francisco.seipel@yahoo.de>
2020-10-20 14:09:21 +0200

    [VERSION] 0.8.4

--- webpackConfig/userscript.adult.config.js
+++ webpackConfig/userscript.adult.config.js
@@ -33,7 +33,7 @@ const metadata = {
   name: `${package['productName']} Adult [FateXXXBlood Edition]`,
   namespace: 'https://greasyfork.org/users/92233',
   description: package['description'],
-  version: '0.2.0',
+  version: '0.2.1',
   author: package['author'],
   license: 'GPL-3.0',
   iconURL:
