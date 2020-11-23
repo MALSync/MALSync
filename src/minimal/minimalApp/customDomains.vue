b8fdf0071bb312ac8460352b485bfc39de07579a
lolamtisch <francisco.seipel@yahoo.de>
2020-11-23 15:22:54 +0100

    [TASK] Add wiki text

--- src/minimal/minimalApp/customDomains.vue
+++ src/minimal/minimalApp/customDomains.vue
@@ -1,64 +1,72 @@
 <template>
-  <div class="mdl-cell bg-cell mdl-cell--6-col mdl-cell--8-col-tablet mdl-shadow--4dp">
-    <div class="mdl-card__title mdl-card--border">
-      <h2 class="mdl-card__title-text">Custom Domains</h2>
+  <div>
+    <div class="mdl-cell bg-cell mdl-cell--6-col mdl-cell--8-col-tablet mdl-shadow--4dp">
+      <div class="mdl-card__title mdl-card--border">
+        <h2 class="mdl-card__title-text">Custom Domains</h2>
+      </div>
+      <div class="mdl-card__supporting-text">
+        Please only use if you know what you are doing!<br>
+        More info <a href="https://github.com/MALSync/MALSync/wiki/Custom-Domains">here</a>
+      </div>
     </div>
-    <div v-for="(perm, index) in permissions" :key="index">
-      <li class="mdl-list__item" style="padding-top: 0; padding-bottom: 0;">
-        <div class="icon material-icons close-icon" @click="removePermission(index)">
-          close
-        </div>
-        <span class="mdl-list__item-primary-content">
-          <select
-            v-model="perm.page"
-            name="myinfo_score"
-            class="inputtext mdl-textfield__input"
-            style="outline: none;"
-            :class="{ error: !pageCheck(perm.page) }"
-          >
-            <option value="" disabled selected>Select Page</option>
-            <option value="iframe">Video Iframe</option>
-            <option v-for="(page, pageKey) in pages" :key="pageKey" :value="pageKey">
-              {{ page.name }}
-            </option>
-          </select>
-        </span>
-        <span class="mdl-list__item-secondary-action">
-          <div class="mdl-textfield mdl-js-textfield">
-            <input
-              v-model="perm.domain"
-              class="mdl-textfield__input"
-              type="text"
-              placeholder="Domain"
-              style="outline: none;"
-              :class="{ error: !domainCheck(perm.domain) }"
-            />
+    <div class="mdl-cell bg-cell mdl-cell--6-col mdl-cell--8-col-tablet mdl-shadow--4dp">
+      <div v-for="(perm, index) in permissions" :key="index">
+        <li class="mdl-list__item" style="padding-top: 0; padding-bottom: 0;">
+          <div class="icon material-icons close-icon" @click="removePermission(index)">
+            close
           </div>
-        </span>
-      </li>
-    </div>
+          <span class="mdl-list__item-primary-content">
+            <select
+              v-model="perm.page"
+              name="myinfo_score"
+              class="inputtext mdl-textfield__input"
+              style="outline: none; margin-left: 10px; margin-right: 10px;"
+              :class="{ error: !pageCheck(perm.page) }"
+            >
+              <option value="" disabled selected>Select Page</option>
+              <option value="iframe">Video Iframe</option>
+              <option v-for="(page, pageKey) in pages" :key="pageKey" :value="pageKey">
+                {{ page.name }}
+              </option>
+            </select>
+          </span>
+          <span class="mdl-list__item-secondary-action">
+            <div class="mdl-textfield mdl-js-textfield">
+              <input
+                v-model="perm.domain"
+                class="mdl-textfield__input"
+                type="text"
+                placeholder="Domain"
+                style="outline: none;"
+                :class="{ error: !domainCheck(perm.domain) }"
+              />
+            </div>
+          </span>
+        </li>
+      </div>
 
-    <div>
-      <li class="mdl-list__item" style="padding-top: 0; padding-bottom: 0;">
-        <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" @click="addPermission()">
-          <i class="material-icons">add</i>
-        </button>
-        <span class="mdl-list__item-primary-content"></span>
-      </li>
-    </div>
+      <div>
+        <li class="mdl-list__item" style="padding-top: 0; padding-bottom: 0;">
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" @click="addPermission()">
+            <i class="material-icons">add</i>
+          </button>
+          <span class="mdl-list__item-primary-content"></span>
+        </li>
+      </div>
 
-    <div v-if="!hasPermissions || JSON.stringify(option) !== JSON.stringify(permissions)">
-      <li class="mdl-list__item" style="padding-top: 0; padding-bottom: 0;">
-        <input
-          type="button"
-          :value="lang('Update')"
-          class="inputButton btn-middle flat js-anime-update-button mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
-          style="margin-right: 5px;"
-          data-upgraded=",MaterialButton"
-          @click="savePermissions()"
-        />
-        <span class="mdl-list__item-primary-content"></span>
-      </li>
+      <div v-if="!hasPermissions || JSON.stringify(option) !== JSON.stringify(permissions)">
+        <li class="mdl-list__item" style="padding-top: 0; padding-bottom: 0;">
+          <input
+            type="button"
+            :value="lang('Update')"
+            class="inputButton btn-middle flat js-anime-update-button mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
+            style="margin-right: 5px;"
+            data-upgraded=",MaterialButton"
+            @click="savePermissions()"
+          />
+          <span class="mdl-list__item-primary-content"></span>
+        </li>
+      </div>
     </div>
   </div>
 </template>
