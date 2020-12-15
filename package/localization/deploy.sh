b4ba8ca51b204a07b25b531c2ca3fd635f17a240
lolamtisch <francisco.seipel@yahoo.de>
2020-12-15 12:58:39 +0100

    Revert "[TASK] Change env name"
    This reverts commit 3f750cb5aff2c3027bf4a05b75ddd81caad69fc8.

--- package/localization/deploy.sh
+++ package/localization/deploy.sh
@@ -18,7 +18,7 @@ git commit -a -m "Update locales.json for lolamtisch/MALSync"
 echo Deploy changes.
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
-git push "https://${OAUTH_TOKEN}@github.com/lolamtisch/MALSync-localization.git" master
+git push "https://${GITHUB_OAUTH_TOKEN}@github.com/lolamtisch/MALSync-localization.git" master
 
 exit 0
 
