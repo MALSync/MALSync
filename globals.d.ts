3e128799c537813c8fa01eeed1b4e270381cc03c
seahindeniz <seahindeniz@gmail.com>
2021-02-07 00:06:20 +0300

    Add types

--- globals.d.ts
+++ globals.d.ts
@@ -5,16 +5,23 @@ import * as _console from './src/utils/console';
 declare global {
   let con: typeof _console;
   let utils: typeof _utils;
-  let j: { $: JQueryStatic; html: (string) => string };
+  let j: {
+    $: JQueryStatic;
+    html: (content: string) => string;
+  };
   let api: typeof webextension;
 
   interface Window {
-    fullScreen: any;
+    /**
+     * @deprecated
+     * @see @link https://developer.mozilla.org/en-US/docs/Web/API/Window/fullScreen
+     */
+    fullScreen: boolean;
   }
 
   interface Document {
-    webkitIsFullScreen: any;
-    mozFullScreen: any;
-    msFullscreenElement: any;
+    webkitIsFullScreen: boolean;
+    mozFullScreen: boolean;
+    msFullscreenElement: Element;
   }
 }
