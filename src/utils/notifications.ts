1bb4480758329854d301a245abe00993f6dd7b7e
lolamtisch <francisco.seipel@yahoo.de>
2021-02-06 20:16:51 +0100

    [TASK] Add notification util

---
+++ src/utils/notifications.ts
@@ -0,0 +1,53 @@
+const defaultImg = 'https://raw.githubusercontent.com/MALSync/MALSync/master/assets/icons/icon128.png'
+
+// Only works on the background Page
+export async function sendNotification(options: {
+  url: string;
+  title: string;
+  text: string;
+  image?: string;
+  sticky?: boolean;
+}) {
+  if (!options.image) options.image = defaultImg;
+
+  const imgBlob = await getImageBlob(options.image);
+
+  chrome.notifications.create(options.url, {
+    type: 'basic',
+    title: options.title,
+    message: options.text,
+    iconUrl: imgBlob,
+    requireInteraction: options.sticky ?? false,
+    eventTime: Date.now(),
+  });
+}
+
+function getImageBlob(url, fallback = false): Promise<string> {
+  if (fallback) url = defaultImg;
+  return new Promise<string>((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+
+    xhr.responseType = 'blob';
+
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        const blob = xhr.response;
+        resolve(window.URL.createObjectURL(blob));
+      }
+      reject(xhr.status);
+    };
+
+    xhr.onerror = function(e) {
+      reject(e);
+    };
+
+    xhr.send();
+  }).catch(e => {
+    if (!fallback) {
+      con.info('Could not get image for notification', url);
+      return getImageBlob(url, true);
+    }
+    throw e;
+  });
+}
