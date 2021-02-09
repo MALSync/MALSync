7f7a8b59a7bdd43eaa0437d51b77cde74a269ab0
seahindeniz <seahindeniz@gmail.com>
2021-02-10 01:33:06 +0300

    Satisfy eslint's "no-undef" rule

--- globals.d.ts
+++ globals.d.ts
@@ -1,15 +1,20 @@
+/* eslint-disable no-redeclare */
 import * as _utils from './src/utils/general';
 import * as webextension from './src/api/webextension';
 import * as _console from './src/utils/console';
 
+type ConsoleType = typeof _console;
+type UtilsType = typeof _utils;
+type WebExtensionType = typeof webextension;
+
 declare global {
-  let con: typeof _console;
-  let utils: typeof _utils;
+  let con: ConsoleType;
+  let utils: UtilsType;
   let j: {
     $: JQueryStatic;
     html: (content: string) => string;
   };
-  let api: typeof webextension;
+  let api: WebExtensionType;
 
   interface Window {
     /**
