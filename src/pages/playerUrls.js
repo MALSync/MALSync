b5f393b74c862583c25ab5bad6aac9ae94df4444
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2021-03-30 17:15:25 +0200

    add missing kickassanime player

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -406,7 +406,7 @@ module.exports = {
   },
   // kickassanime
   kaaplay: {
-    match: ['*://kaa-play.com/*'],
+    match: ['*://kaa-play.com/*', '*://kaa-play.me/*'],
   },
   // animeshouse
   animeshouse: {
