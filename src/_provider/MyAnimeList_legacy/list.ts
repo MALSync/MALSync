6b8a8e54e62374b0db1fe15045f7107b8a1bf16d
lolamtisch <francisco.seipel@yahoo.de>
2021-04-16 14:55:28 +0200

    [TASK] Remove wrong line

--- src/_provider/MyAnimeList_legacy/list.ts
+++ src/_provider/MyAnimeList_legacy/list.ts
@@ -71,7 +71,6 @@ export class UserList extends ListAbstract {
       default:
         if (this.status === 1) return this.getOrder('updated');
         if (this.status === 6) return this.getOrder('updated');
-        return this.getOrder('updated'); // TODO: remove when fixed in anilist
         return this.getOrder('alpha');
     }
   }
