1b818594cbbacc1a030faf95d82bdd2c5eed3790
lolamtisch <francisco.seipel@yahoo.de>
2021-05-23 19:39:11 +0200

    [TASK] Fix logging error

--- src/utils/consoleBG.ts
+++ src/utils/consoleBG.ts
@@ -99,6 +99,8 @@ export const m = (name, color = '', blocks: { name: string; style: string }[] = 
 };
 
 function stringToColour(str) {
+  if (!str) return '#ffffff';
+  str = String(str);
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
