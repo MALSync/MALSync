e7786d1f753ec2b01f210f8c54779ec46d90dea7
lolamtisch <francisco.seipel@yahoo.de>
2021-06-08 11:06:37 +0200

    [TASK] Lint

--- src/pages/Mangadex/main.ts
+++ src/pages/Mangadex/main.ts
@@ -1,6 +1,6 @@
 import { pageInterface } from '../pageInterface';
 
-let mangaData = {
+const mangaData = {
   id: '',
   title: '',
   coverId: '',
@@ -11,7 +11,7 @@ let mangaData = {
   },
 };
 
-let chapterData = {
+const chapterData = {
   id: '',
   chapter: '',
   volume: '',
