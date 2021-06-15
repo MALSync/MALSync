46eeaaf3606d4fcc7845e9735881a8b5575e5a46
lolamtisch <francisco.seipel@yahoo.de>
2021-06-15 10:01:39 +0200

    [TASK] Add more animixplay github domains

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -436,7 +436,7 @@ module.exports = {
   },
   // animixplay
   github: {
-    match: ['*://akaneshinjou.github.io/play/player.html*', '*://akane-shinjou.github.io/play/player.html*'],
+    match: ['*://akaneshinjou.github.io/*', '*://akane-shinjou.github.io/*', '*://andhaetg.github.io/*'],
   },
   // zoro
   vidcloud: {
