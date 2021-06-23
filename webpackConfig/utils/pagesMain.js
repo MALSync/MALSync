68a332410f891c61a381ebc5793f2aefd5aa780d
lolamtisch <francisco.seipel@yahoo.de>
2021-06-23 14:36:13 +0200

    [TASK] Add complete page proxy

--- webpackConfig/utils/pagesMain.js
+++ webpackConfig/utils/pagesMain.js
@@ -1,3 +1,5 @@
+const pagesUtils = require('./pages');
+
 require('ts-node').register({
   project: './tsconfig.node.json',
   files: './globals.d.ts',
@@ -9,5 +11,15 @@ module.exports = {
   pages: function(path = '../../src/pages/pages.ts') {
     const pages = ts.open(path).pages;
     return pages;
+  },
+  completePages: function() {
+    const pages = ts.open('../../src/pages/pages.ts').pages;
+    return Object.keys(pages).map(key => {
+      return {
+        key,
+        main: pages[key],
+        meta: pagesUtils.meta(key),
+      };
+    });
   },
 };
