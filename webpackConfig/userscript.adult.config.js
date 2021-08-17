7abd7f8c6c523b86691639adde6fbfd20cc8cbd8
lolamtisch <francisco.seipel@yahoo.de>
2021-08-17 14:22:36 +0200

    [TASK] Increase adult version

--- webpackConfig/userscript.adult.config.js
+++ webpackConfig/userscript.adult.config.js
@@ -33,7 +33,7 @@ const metadata = {
   name: `${package['productName']} Adult [FateXXXBlood Edition]`,
   namespace: 'https://greasyfork.org/users/92233',
   description: package['description'],
-  version: '0.2.9',
+  version: '0.2.10',
   author: package['author'],
   license: 'GPL-3.0',
   iconURL: 'https://raw.githubusercontent.com/MALSync/MALSync/master/assets/icons/icon128.png',
