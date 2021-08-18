6291c0c3242cf2f46df0d06e296fbae08107707f
lolamtisch <francisco.seipel@yahoo.de>
2021-08-18 16:44:09 +0200

    [TASK] New search syntax

---
+++ test/src/utils/quicklinksBuilder.test.ts
@@ -0,0 +1,96 @@
+import { expect } from 'chai';
+
+import { searchSyntax, titleSearch } from '../../../src/utils/quicklinksBuilder';
+
+describe('quicklinksBuilder', function() {
+
+  describe('searchSyntax', function() {
+    const title = 'No game_no Life³4 1 ! ';
+    [
+      {
+        title: 'No Searchterm',
+        search: '--',
+        result: '--',
+      },
+      {
+        title: 'Default',
+        search: '-{searchterm}-',
+        result: '-no%20game_no%20life%C2%B34%201%20!-',
+      },
+      {
+        title: 'Replace Space',
+        search: '{searchterm(.)}',
+        result: 'no.game_no.life%C2%B34.1.!',
+      },
+      {
+        title: 'No Encoding',
+        search: '{searchterm(.)[noEncode]}',
+        result: 'no.game_no.life³4.1.!',
+      },
+      {
+        title: 'Option only',
+        search: '{searchterm[noEncode]}',
+        result: 'no game_no life³4 1 !',
+      },
+      {
+        title: 'No Lowercase',
+        search: '{searchterm(+)[noLowercase]}',
+        result: 'No+game_no+Life%C2%B34+1+!',
+      },
+      {
+        title: 'No Special Characters',
+        search: '{searchterm( )[noSpecial]}',
+        result: 'no gameno life',
+      },
+      {
+        title: 'All',
+        search: '{searchterm(_)[noSpecial,noLowercase,noEncode]}',
+        result: 'No_gameno_Life',
+      },
+    ].forEach(el => {
+      it(el.title, function() {
+        expect(el.result).to.equal(searchSyntax(el.search, title));
+      });
+    });
+  });
+
+  describe('Title Search', function() {
+    const title = 'No game_no Life³4 1 ! ';
+    [
+      {
+        title: 'Default',
+        search: '-{searchterm}-',
+        result: '-no%20game_no%20life%C2%B34%201%20!-',
+      },
+      {
+        title: 'Plus',
+        search: '-{searchtermPlus}-',
+        result: '-no+game_no+life%C2%B34+1+!-',
+      },
+      {
+        title: 'Underscore',
+        search: '+{searchterm_}+',
+        result: '+no_game_no_life%C2%B34_1_!+',
+      },
+      {
+        title: 'Raw',
+        search: '-{searchtermRaw}-',
+        result: '-No game_no Life³4 1 !-',
+      },
+      {
+        title: 'Cache Id',
+        search: '-{cacheId}-',
+        result: '-123-',
+      },
+    ].forEach(el => {
+      it(el.title, function() {
+        expect(el.result).to.equal(titleSearch(el.search, title, '123'));
+      });
+    });
+
+    it('/', function() {
+      expect('-no game no Life-').to.equal(titleSearch('-{searchtermRaw}-', 'no game/no Life', '123'));
+      expect('-no%20game%2Fno%20life-').to.equal(titleSearch('-{searchterm}-', 'no game/no Life', '123'));
+    });
+  });
+});
