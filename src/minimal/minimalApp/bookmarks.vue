c222fdd7eab6c0e3f20df409dd37e86e15f00f12
lolamtisch <francisco.seipel@yahoo.de>
2021-10-06 15:45:13 +0200

    [TASK] Fix duplicate key error

--- src/minimal/minimalApp/bookmarks.vue
+++ src/minimal/minimalApp/bookmarks.vue
@@ -26,7 +26,7 @@
 
         <div
           v-for="n in 10"
-          :key="n"
+          :key="'placeholder_'+n"
           class="listPlaceholder mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--6-col-phone mdl-shadow--2dp mdl-grid "
           style="cursor: pointer; padding: 0; width: 210px; height: 0; margin-top:0; margin-bottom:0; visibility: hidden;"
         ></div>
