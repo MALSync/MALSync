bb4fe50889aa80492c0433a31f7b14dcc798828b
lolamtisch <francisco.seipel@yahoo.de>
2021-10-22 17:55:42 +0200

    [TASK] Dont trigger on mangajar 404 Fixes MALSYNC-N2

--- src/pages/MangaJar/main.ts
+++ src/pages/MangaJar/main.ts
@@ -89,6 +89,10 @@ export const MangaJar: pageInterface = {
   init(page) {
     api.storage.addStyle(require('!to-string-loader!css-loader!less-loader!./style.less').toString());
     j.$(document).ready(function() {
+      if (document.title.includes('Not Found')) {
+        con.error('404');
+        return;
+      }
       page.handlePage();
     });
   },
