704e61859b68421e59dcd437b6b7bc02327cf13a
lolamtisch <francisco.seipel@yahoo.de>
2022-02-09 17:55:21 +0100

    [TASK] Add shadowdom support

--- src/utils/Vue.ts
+++ src/utils/Vue.ts
@@ -1,19 +1,49 @@
-import { createApp as vueCreateApp, Component } from 'vue';
+import { createApp as vueCreateApp } from 'vue';
 import VueDOMPurifyHTML from 'vue-dompurify-html';
 
-export function createApp(component, selector: string) {
+export function createApp(component, selector: string | HTMLElement, option?: { shadowDom?: boolean }) {
   const app = vueCreateApp(component);
   app.use(VueDOMPurifyHTML, { default: { ADD_ATTR: ['target'] } });
-  const root = app.mount(selector);
+
+  let rootElement = typeof selector === 'string' ? document.querySelector(selector) : selector;
+
+  if (!rootElement) throw new Error("Can't find root element");
+
+  if (option && option.shadowDom) {
+    let shadowRoot: ShadowRoot;
+    if (rootElement.shadowRoot) {
+      shadowRoot = rootElement.shadowRoot;
+      shadowRoot.innerHTML = '';
+    } else {
+      shadowRoot = rootElement.attachShadow({ mode: 'open' });
+    }
+
+    if (component.styles) {
+      shadowRoot.appendChild(createStyleTag(component.styles));
+    }
+
+    rootElement = shadowRoot.appendChild(document.createElement('div'));
+  }
+
+  const root = app.mount(rootElement);
 
   // inject Styles
-  if (component.styles) {
+  if (component.styles && (!option || !option.shadowDom)) {
     const style = document.createElement('style');
     style.type = 'text/css';
     const styleText = document.createTextNode(component.styles);
     style.appendChild(styleText);
-    root.$el.after(style);
+    root.$el.after(createStyleTag(component.styles));
   }
 
   return root;
+}
+
+
+function createStyleTag(styleString: string) {
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  const styleText = document.createTextNode(styleString);
+  style.appendChild(styleText);
+  return style;
 }
