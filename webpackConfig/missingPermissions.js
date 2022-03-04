bab417a97d9a62dcbf91113c22763b82e9518071
lolamtisch <francisco.seipel@yahoo.de>
2022-03-04 17:12:20 +0100

    [TASK] Save missing permissions based on the version

--- webpackConfig/missingPermissions.js
+++ webpackConfig/missingPermissions.js
@@ -14,12 +14,20 @@ async function main() {
   console.log('Extracting');
   await ex(`unzip -o dist/lastExtension.zip -d dist/lastExtension`);
 
-  const oldUrls = getUrls(require('../dist/lastExtension/manifest.json'));
+  const manifest = require('../dist/lastExtension/manifest.json');
+
+  const version = manifest.version;
+  const oldUrls = getUrls(manifest);
   const diffUrls = getDiff(oldUrls);
   const descFile = path.join(__dirname, '../src/pages/diffUrls.json');
-  console.log(diffUrls);
-  fs.writeFile(descFile, JSON.stringify(diffUrls, null, 2), 'utf8', function (err) {
-    if (err) throw err;
+  console.log(version, diffUrls);
+  fs.readFile(descFile, 'utf8', function(err, data) {
+    const currentData = JSON.parse(data);
+    currentData[version] = diffUrls;
+
+    fs.writeFile(descFile, JSON.stringify(currentData, null, 2), 'utf8', function (err) {
+      if (err) throw err;
+    });
   });
 }
 
