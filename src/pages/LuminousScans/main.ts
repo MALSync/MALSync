672b262876125d6376902a721e7df264b3a72db5
macbestcam <macbestcam@gmail.com>
2022-05-26 22:44:32 -0400

    Used getInter from RealmScans

--- src/pages/LuminousScans/main.ts
+++ src/pages/LuminousScans/main.ts
@@ -1,78 +1,9 @@
 import { pageInterface } from '../pageInterface';
+import { getInter } from '../RealmScans/main';
 
-export const LuminousScans: pageInterface = {
-  name: 'LuminousScans',
-  domain: 'https://luminousscans.com',
-  languages: ['English'],
-  type: 'manga',
-  isSyncPage(url) {
-    return j.$('div.allc').length > 0;
-  },
-  isOverviewPage(url) {
-    if (utils.urlPart(url, 3) === 'series' && utils.urlPart(url, 4)) return true;
-    return false;
-  },
-  getImage() {
-    return j.$('div.thumb img').attr('src');
-  },
-  sync: {
-    getTitle(url) {
-      return j.$('.ts-breadcrumb li:nth-child(2) [itemprop="name"]').text();
-    },
-    getIdentifier(url) {
-      return LuminousScans.overview!.getIdentifier(LuminousScans.sync.getOverviewUrl(url));
-    },
-    getOverviewUrl(url) {
-      return j.$('.ts-breadcrumb li:nth-child(2) a').attr('href') || '';
-    },
-    getEpisode(url) {
-      const episodePart = utils.urlPart(url, 3);
+const clone = getInter();
 
-      const temp = episodePart.match(/-chapter-(\d+)/im);
+clone.name = 'LuminousScans';
+clone.domain = 'https://luminousscans.com';
 
-      if (!temp) return NaN;
-
-      return Number(temp[1]);
-    },
-    nextEpUrl(url) {
-      const nextButton = j.$('a.ch-next-btn:first').attr('href');
-      if (nextButton === '#/next/') return undefined;
-      return nextButton;
-    },
-  },
-  overview: {
-    getTitle(url) {
-      return j.$('#titlemove [itemprop="name"]').text();
-    },
-    getIdentifier(url) {
-      return utils.urlPart(url, 4);
-    },
-    uiSelector(selector) {
-      j.$('#series-history').first().after(j.html(selector));
-    },
-    list: {
-      offsetHandler: false,
-      elementsSelector() {
-        return j.$('#chapterlist .chbox');
-      },
-      elementUrl(selector) {
-        return utils.absoluteLink(selector.find('a').first().attr('href'), LuminousScans.domain);
-      },
-      elementEp(selector) {
-        return LuminousScans.sync.getEpisode(LuminousScans.overview!.list!.elementUrl!(selector));
-      },
-    },
-  },
-  init(page) {
-    api.storage.addStyle(
-      require('!to-string-loader!css-loader!less-loader!./style.less').toString(),
-    );
-    j.$(() => {
-      if (document.title.includes('Page not found')) {
-        con.error('404');
-        return;
-      }
-      page.handlePage();
-    });
-  },
-};
+export const LuminousScans: pageInterface = clone;
