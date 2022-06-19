3272fad4eb19f0c49e46c41509a5d200d379c5b3
Ernesto Montada Romero <e.montada0@gmail.com>
2022-06-19 19:15:49 +0200

    Add Animeworld page support

---
+++ src/pages/Animeworld/main.ts
@@ -0,0 +1,44 @@
+import { pageInterface } from '../pageInterface';
+
+export const Animeworld: pageInterface = {
+  name: 'Animeworld',
+  domain: 'https://www.animeworld.tv',
+  languages: ['Italian'],
+  type: 'anime',
+  isSyncPage(url) {
+    if (url.split('/')[4] !== undefined && url.split('/')[4].length) {
+      return true;
+    }
+    return false;
+  },
+  sync: {
+    getTitle(url) {
+      return j.$("#anime-title").text().trim()
+    },
+    getIdentifier(url) {
+      return utils.urlPart(url, 4);
+    },
+    getOverviewUrl(url) {
+      return `${Animeworld.domain}/${Animeworld.sync.getIdentifier(url)}`;
+    },
+    getEpisode(url) {
+      const episode = Number(
+        j.$("div.widget-body>div.server.active>ul.episodes.range.active>li.episode>a.active").text().trim()
+      );
+      if (episode) return episode;
+      return 1;
+    },
+    uiSelector(selector) {
+      j.$('span.clearfix').after(j.html(selector));
+    },
+  },
+  init(page) {
+    api.storage.addStyle(
+      require('!to-string-loader!css-loader!less-loader!./style.less').toString(),
+    );
+    utils.fullUrlChangeDetect(function () {
+      page.reset();
+      page.handlePage();
+    });
+  },
+};
