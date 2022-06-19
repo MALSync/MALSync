f0ed12e4620d59e09db7e12e22f889375c814048
Ernesto Montada Romero <e.montada0@gmail.com>
2022-06-19 19:16:12 +0200

    Add Animeworld player url

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -716,4 +716,8 @@ module.exports = {
   simplyaweeb: {
     match: ['*://player.simplyaweeb.to/*'],
   },
+  // animeworld
+  animeworld: {
+    match: ['*://www.animeworld.tv/api/episode/serverPlayerAnimeWorld?id=*']
+  }
 };
