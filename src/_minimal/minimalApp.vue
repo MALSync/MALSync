bc76a34f0218f36b24615dfcad5ae2e84406de8a
lolamtisch <francisco.seipel@yahoo.de>
2022-07-27 18:37:05 +0200

    [TASK] Add content styling and split less

--- src/_minimal/minimalApp.vue
+++ src/_minimal/minimalApp.vue
@@ -1,7 +1,16 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="content">
+    <h1>Hello World</h1>
+  </div>
 </template>
 
 <style lang="less">
 @import './less/_main.less';
+</style>
+
+<style lang="less" scoped>
+@import './less/_globals.less';
+.content {
+  padding: 0 @spacer;
+}
 </style>
