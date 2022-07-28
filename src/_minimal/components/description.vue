329df9043024bce1daa17466175d58f7592a4542
lolamtisch <francisco.seipel@yahoo.de>
2022-07-28 12:14:17 +0200

    [TASK] Add description component

---
+++ src/_minimal/components/description.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="description" :class="{ close: !open && overflow }">
+    <div ref="inner" class="open-gradient">
+      <slot></slot>
+    </div>
+    <FormButton v-show="!open && overflow" class="open-button" @click="open = true">Read More</FormButton>
+    <div v-show="open" class="close-button-box">
+      <FormButton class="close-button" @click="open = false">Read Less</FormButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, Ref, ref } from 'vue';
+
+import FormButton from './form/form-button.vue';
+
+const open = ref(false);
+const overflow = ref(true);
+
+const inner = ref(null) as Ref<HTMLElement | null>;
+
+onMounted(() => {
+  if (inner.value) {
+    const { scrollHeight, clientHeight } = inner.value;
+    overflow.value = Boolean(scrollHeight > clientHeight);
+  }
+});
+</script>
+
+<style lang="less" scoped>
+.description {
+  position: relative;
+
+  .open-button {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .close-button-box {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 10px;
+  }
+
+  &.close {
+    overflow: hidden;
+
+    .open-gradient {
+      max-height: 240px;
+      mask-image: linear-gradient(180deg, #000 60%, transparent);
+      /* stylelint-disable-next-line property-no-vendor-prefix */
+      -webkit-mask-image: linear-gradient(180deg, #000 60%, transparent);
+    }
+  }
+}
+</style>
--- src/_minimal/minimalApp.vue
+++ src/_minimal/minimalApp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="content">
-    <Grid min-width="191">
+    <Grid :min-width="191">
       <imageLazy
         mode="cover"
         src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx19815-bIo51RMWWhLv.jpg"
@@ -21,6 +21,30 @@
       style="width: 600px; height: 300px"
       src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx19815-bIo51RMWWhLv.jpg"
     />
+    <Description
+      >When a mysterious creature chops the moon down to a permanent crescent, the students of class
+      3-E of Kunugigaoka Middle School find themselves confronted with an enormous task: assassinate
+      the creature responsible for the disaster before Earth suffers a similar fate. However, the
+      monster, dubbed Koro-sensei (the indestructible teacher), is able to fly at speeds of up to
+      Mach 20, which he demonstrates freely, leaving any attempt to subdue him in his
+      extraterrestrial dust. Furthermore, the misfits of 3-E soon find that the strange, tentacled
+      beast is more than just indomitable—he is the best teacher they have ever had! Adapted from
+      the humorous hit manga by Yuusei Matsui, Ansatsu Kyoushitsu tells the story of these junior
+      high pupils as they polish their assassination skills and grow in order to stand strong
+      against the oppressive school system, their own life problems, and one day,
+      Koro-sensei.[Written by MAL Rewrite]When a mysterious creature chops the moon down to a
+      permanent crescent, the students of class 3-E of Kunugigaoka Middle School find themselves
+      confronted with an enormous task: assassinate the creature responsible for the disaster before
+      Earth suffers a similar fate. However, the monster, dubbed Koro-sensei (the indestructible
+      teacher), is able to fly at speeds of up to Mach 20, which he demonstrates freely, leaving any
+      attempt to subdue him in his extraterrestrial dust. Furthermore, the misfits of 3-E soon find
+      that the strange, tentacled beast is more than just indomitable—he is the best teacher they
+      have ever had! Adapted from the humorous hit manga by Yuusei Matsui, Ansatsu Kyoushitsu tells
+      the story of these junior high pupils as they polish their assassination skills and grow in
+      order to stand strong against the oppressive school system, their own life problems, and one
+      Koro-sensei.[Written by MAL Rewrite]</Description
+    >
+    <Description>Short Description</Description>
   </div>
 </template>
 
@@ -29,6 +53,7 @@ import imageLazy from './components/image-lazy.vue';
 import ImageFit from './components/image-fit.vue';
 import Grid from './components/grid.vue';
 import FormButton from './components/form/form-button.vue';
+import Description from './components/description.vue';
 </script>
 
 <style lang="less">
