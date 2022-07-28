dade3ea6d0b134034df17d9922903eff94598c73
lolamtisch <francisco.seipel@yahoo.de>
2022-07-28 04:49:52 +0200

    [TASK] Add lazy-image component

--- src/_minimal/minimalApp.vue
+++ src/_minimal/minimalApp.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="content">
-    <h1>Hello World</h1>
+    <lazyImage
+      mode="cover"
+      src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx19815-bIo51RMWWhLv.jpg"
+      style="width: 300px"
+    />
+    <lazyImage src="" mode="cover" style="width: 300px" />
+    <lazyImage src="https://s4.anilist.co/file/anilistcdn/medi" mode="cover" style="width: 300px" />
   </div>
 </template>
+
+<script lang="ts" setup>
+import lazyImage from './components/lazy-image.vue';
+</script>
 
 <style lang="less">
 @import './less/_main.less';
