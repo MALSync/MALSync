fbad8a8443964410248d10cd389591a02d3effdc
lolamtisch <francisco.seipel@yahoo.de>
2022-08-14 12:55:44 +0200

    [TASK] Add media url handling

--- src/_minimal/components/media-link.vue
+++ src/_minimal/components/media-link.vue
@@ -1,14 +1,28 @@
 <template>
-  <a :href="href" class="media-link"><slot /></a>
+  <router-link
+    v-if="slugObj.path"
+    class="media-link"
+    :to="{ name: 'Overview', params: slugObj.path }"
+    :href="slugObj.url"
+    rel="noopener"
+  >
+    <slot />
+  </router-link>
+  <a v-else :href="slugObj.url" class="media-link" target="_blank" rel="noopener"><slot /></a>
 </template>
 
 <script lang="ts" setup>
-defineProps({
+import { computed } from 'vue';
+import { urlToSlug } from '../../utils/slugs';
+
+const props = defineProps({
   href: {
     type: String,
     required: true,
   },
 });
+
+const slugObj = computed(() => urlToSlug(props.href));
 </script>
 
 <style lang="less" scoped>
--- src/_minimal/views/overview.vue
+++ src/_minimal/views/overview.vue
@@ -1,1 +1,13 @@
-<template>Overview</template>
+<template>
+  <div>{{ url }}</div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { Path, pathToUrl } from '../../utils/slugs';
+
+const route = useRoute();
+
+const url = computed(() => pathToUrl(route.params as Path));
+</script>
