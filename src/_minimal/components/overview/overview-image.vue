5e3208896fb481ca8879b9a79dc4fd6eb01e1782
lolamtisch <francisco.seipel@yahoo.de>
2022-08-15 11:00:06 +0200

    [TASK] Add overview image

---
+++ src/_minimal/components/overview/overview-image.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="overviewImage">
+    <ImageFit mode="cover" :src="src" :loading="loading" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ImageFit from '../image-fit.vue';
+
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
