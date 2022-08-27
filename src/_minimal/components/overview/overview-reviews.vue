e0a50a5bffc47379c94a775e650a4292e607fac7
lolamtisch <francisco.seipel@yahoo.de>
2022-08-27 11:44:35 +0200

    [TASK] Style reviews

--- src/_minimal/components/overview/overview-reviews.vue
+++ src/_minimal/components/overview/overview-reviews.vue
@@ -2,16 +2,28 @@
   <div>
     <Header :spacer="true">{{ lang('minimalApp_Reviews') }}</Header>
     <div v-if="!metaRequest.loading">
-      <div v-for="review in metaRequest.data" :key="review.user.name">
-        <div>
-          <MediaLink :href="review.user.href">{{ review.user.name }}</MediaLink>
+      <Section v-for="review in metaRequest.data" :key="review.user.name">
+        <div class="head">
+          <MediaLink :href="review.user.href" class="img">
+            <ImageLazy :src="review.user.image" class="img-el" />
+          </MediaLink>
+          <div class="info">
+            <Header>
+              <MediaLink :href="review.user.href">{{ review.user.name }}</MediaLink>
+            </Header>
+            <div>{{ review.body.date }}</div>
+            <div>
+              Overall Rating: <strong>{{ review.body.rating }}</strong>
+            </div>
+            <div>
+              <strong>{{ review.body.people }}</strong> people found this review helpful
+            </div>
+          </div>
         </div>
-        <ImageLazy :src="review.user.image" />
-        <div>{{review.body.people}}</div>
-        <div>{{review.body.date}}</div>
-        <div>{{review.body.rating}}</div>
-        <div class="text">{{review.body.text}}</div>
-      </div>
+        <div class="text">
+          <Description>{{ review.body.text }}</Description>
+        </div>
+      </Section>
     </div>
   </div>
 </template>
@@ -23,6 +35,8 @@ import Header from '../header.vue';
 import { reviewMeta } from './overview-reviews-meta';
 import ImageLazy from '../image-lazy.vue';
 import MediaLink from '../media-link.vue';
+import Description from '../description.vue';
+import Section from '../section.vue';
 
 const props = defineProps({
   malUrl: {
@@ -44,5 +58,28 @@ const metaRequest = createRequest(parameters, params => {
 <style lang="less" scoped>
 .text {
   white-space: pre-line;
+}
+
+.head {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+  .img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    overflow: hidden;
+    &-el {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .info {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
 }
 </style>
