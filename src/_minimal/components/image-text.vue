c8472dbbb22c18011a476ae4a1e36c45aa4471ce
lolamtisch <francisco.seipel@yahoo.de>
2022-08-28 21:23:43 +0200

    [TASK] Add image text element

---
+++ src/_minimal/components/image-text.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="head">
+    <MediaLink :href="href" class="img">
+      <ImageLazy :src="image" class="img-el" />
+    </MediaLink>
+    <div class="info">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ImageLazy from './image-lazy.vue';
+import MediaLink from './media-link.vue';
+
+defineProps({
+  href: {
+    type: String,
+    default: '',
+  },
+  image: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style lang="less" scoped>
+.head {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+  .img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    overflow: hidden;
+    &-el {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .info {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+}
+</style>
--- src/_minimal/components/overview/overview-reviews.vue
+++ src/_minimal/components/overview/overview-reviews.vue
@@ -5,23 +5,18 @@
       <Pagination :entries-per-page="3" :elements="metaRequest.data">
         <template #elements="{ elements }">
           <Section v-for="review in elements" :key="review.user.name">
-            <div class="head">
-              <MediaLink :href="review.user.href" class="img">
-                <ImageLazy :src="review.user.image" class="img-el" />
-              </MediaLink>
-              <div class="info">
-                <Header>
-                  <MediaLink :href="review.user.href">{{ review.user.name }}</MediaLink>
-                </Header>
-                <div>{{ review.body.date }}</div>
-                <div>
-                  Overall Rating: <strong>{{ review.body.rating }}</strong>
-                </div>
-                <div>
-                  <strong>{{ review.body.people }}</strong> people found this review helpful
-                </div>
+            <ImageText :href="review.user.href" :image="review.user.image">
+              <Header>
+                <MediaLink :href="review.user.href">{{ review.user.name }}</MediaLink>
+              </Header>
+              <div>{{ review.body.date }}</div>
+              <div>
+                Overall Rating: <strong>{{ review.body.rating }}</strong>
               </div>
-            </div>
+              <div>
+                <strong>{{ review.body.people }}</strong> people found this review helpful
+              </div>
+            </ImageText>
             <div class="text">
               <Description>{{ review.body.text }}</Description>
             </div>
@@ -40,7 +35,6 @@ import { ref } from 'vue';
 import { createRequest } from '../../utils/reactive';
 import Header from '../header.vue';
 import { reviewMeta } from './overview-reviews-meta';
-import ImageLazy from '../image-lazy.vue';
 import MediaLink from '../media-link.vue';
 import Description from '../description.vue';
 import Section from '../section.vue';
@@ -68,28 +62,5 @@ const metaRequest = createRequest(parameters, params => {
 <style lang="less" scoped>
 .text {
   white-space: pre-line;
-}
-
-.head {
-  display: flex;
-  gap: 20px;
-  margin-bottom: 20px;
-  .img {
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    overflow: hidden;
-    &-el {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-  }
-  .info {
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-  }
 }
 </style>
