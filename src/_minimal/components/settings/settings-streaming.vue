2da678d4bc70177f4f06bd3e25d2b562d5a5f92e
lolamtisch <francisco.seipel@yahoo.de>
2022-09-06 15:59:06 +0200

    [TASK] Add quicklinks placeholder

---
+++ src/_minimal/components/settings/settings-streaming.vue
@@ -0,0 +1,1 @@
+<template>test213214</template>
--- src/_minimal/components/settings/settings-structure.ts
+++ src/_minimal/components/settings/settings-structure.ts
@@ -2,6 +2,7 @@ import { Component } from 'vue';
 import SettingsGroup from './settings-group.vue';
 import SettingsGeneral from './settings-general.vue';
 import SettingsProfile from './settings-profile.vue';
+import SettingsStreaming from './settings-streaming.vue';
 import ColorPreview from '../color-preview.vue';
 
 export interface ConfObj {
@@ -128,5 +129,37 @@ export const structure: ConfObj[] = [
     },
     component: SettingsGroup,
     children: theming,
+  },
+  {
+    key: 'streaming',
+    title: api.storage.lang('settings_StreamingSite'),
+    props: {
+      icon: 'rss_feed',
+    },
+    component: SettingsGroup,
+    children: [
+      {
+        key: 'quicklinksPosition',
+        title: api.storage.lang('settings_custom_domains_position'),
+        condition: () =>
+          api.settings.get('syncMode') === 'ANILIST' || api.settings.get('syncMode') === 'MAL',
+        props: {
+          component: 'dropdown',
+          option: 'quicklinksPosition',
+          props: {
+            options: [
+              { title: 'Default', value: 'default' },
+              { title: 'Below Information', value: 'below' },
+            ],
+          },
+        },
+        component: SettingsGeneral,
+      },
+      {
+        key: 'streamingSite',
+        title: api.storage.lang('settings_StreamingSite'),
+        component: SettingsStreaming,
+      },
+    ],
   },
 ];
