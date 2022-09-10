9560cca4a8252f6fb88ad25410667d628cc7f320
hoangvu12 <hggaming91@gmail.com>
2022-09-10 16:07:28 +0700

    Add Kaguya (kaguya.live)

---
+++ src/pages/Kaguya/main.ts
@@ -0,0 +1,95 @@
+import { pageInterface } from '../pageInterface';
+
+let jsonData = {
+  title: '',
+  episode: null,
+  aniId: null,
+  id: null,
+  nextEpUrl: null,
+};
+
+const domain = 'https://kaguya.live';
+
+export const Kaguya: pageInterface = {
+  name: 'Kaguya',
+  domain,
+  languages: ['Vietnamese', 'English'],
+  type: 'anime',
+  isSyncPage(url) {
+    console.log('sync', url);
+
+    return url.includes('/anime/watch/');
+  },
+  isOverviewPage(url) {
+    console.log('overview', url);
+
+    return url.includes('/anime/details/');
+  },
+  sync: {
+    getTitle() {
+      return jsonData.title;
+    },
+    getIdentifier(url) {
+      return utils.urlPart(url, 5);
+    },
+    getOverviewUrl(url) {
+      const id = utils.urlPart(url, 5);
+
+      return `${domain}/anime/details/${id}`;
+    },
+    nextEpUrl() {
+      return `${domain}${jsonData.nextEpUrl}`;
+    },
+    getEpisode() {
+      return jsonData.episode || 0;
+    },
+  },
+  overview: {
+    getTitle() {
+      return jsonData.title;
+    },
+    getIdentifier() {
+      return (jsonData.aniId || 0).toString();
+    },
+    uiSelector(selector) {
+      j.$('#mal-sync').first().after(j.html(selector));
+    },
+    getMalUrl(provider) {
+      if (provider === 'ANILIST' && jsonData.aniId)
+        return `https://anilist.co/anime/${jsonData.aniId}`;
+
+      return false;
+    },
+  },
+  init(page) {
+    api.storage.addStyle(
+      require('!to-string-loader!css-loader!less-loader!./style.less').toString(),
+    );
+
+    j.$(document).ready(function () {
+      const check = () => {
+        utils.waitUntilTrue(
+          function () {
+            const tempJsonData = JSON.parse(j.$('#syncData').text() || '{}');
+
+            return Object.keys(tempJsonData).length !== 0;
+          },
+          function () {
+            jsonData = JSON.parse(j.$('#syncData').text() || '{}');
+
+            console.log(jsonData);
+
+            page.handlePage();
+          },
+        );
+      };
+
+      utils.fullUrlChangeDetect(function () {
+        console.log('fullUrlChangeDetect');
+
+        page.reset();
+        check();
+      });
+    });
+  },
+};
