6cef49dcaae42b41dc3b58c7ec28d0b744ebf546
lolamtisch <francisco.seipel@yahoo.de>
2022-09-29 13:38:49 +0200

    [TASK] Add website tracking component

---
+++ src/_minimal/components/settings/settings-disabled-websites.vue
@@ -0,0 +1,92 @@
+<template>
+  <Card v-if="Object.keys(pageArray).length" class="disabledWebsites">
+    <Header v-if="onlyDisabled" :spacer="true">Tracking Disabled</Header>
+    <SettingsGeneral
+      v-for="(page, key) in pageArray"
+      :key="key"
+      component="checkbox"
+      title=""
+      :value="true"
+    >
+      <template #title>
+        <MediaLink :href="getDomain(page)">
+          <TextIcon :src="favicon(getDomain(page))">{{ page.name }}</TextIcon>
+        </MediaLink>
+      </template>
+      <template #component>
+        <FormCheckbox
+          :model-value="getPageState(page)"
+          @update:model-value="newValue => setPageState(page, newValue)"
+        />
+      </template>
+    </SettingsGeneral>
+  </Card>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import Card from '../card.vue';
+import SettingsGeneral from './settings-general.vue';
+import { pages } from '../../../pages/pages';
+import MediaLink from '../media-link.vue';
+import TextIcon from '../text-icon.vue';
+import FormCheckbox from '../form/form-checkbox.vue';
+import Header from '../header.vue';
+
+const props = defineProps({
+  onlyDisabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(['change']);
+
+const model = computed({
+  get() {
+    return api.settings.get('enablePages');
+  },
+  set(value) {
+    api.settings.set('enablePages', value);
+    emit('change', value);
+  },
+});
+
+function getPageState(page) {
+  return Boolean(typeof model.value[page.name] === 'undefined' || model.value[page.name]);
+}
+
+function setPageState(page, state) {
+  const curState = JSON.parse(JSON.stringify(model.value));
+  curState[page.name] = state;
+  model.value = curState;
+}
+
+const pageArray = computed(() => {
+  const res = {};
+  Object.keys(pages).forEach(key => {
+    if (props.onlyDisabled && getPageState(pages[key])) return;
+    res[key] = pages[key];
+  });
+  return res;
+});
+
+function getDomain(page) {
+  if (typeof page.domain === 'object') {
+    return page.domain[0];
+  }
+  return page.domain;
+}
+
+function favicon(url: string) {
+  return utils.favicon(url.split('/')[2]);
+}
+</script>
+
+<style lang="less" scoped>
+@import '../../less/_globals.less';
+
+.disabledWebsites {
+  margin: @spacer-half 0;
+}
+</style>
