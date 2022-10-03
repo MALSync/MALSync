387a0ef6fd8a409bb3f9f0d173aa77f5bd6c7aee
lolamtisch <francisco.seipel@yahoo.de>
2022-10-03 22:46:47 +0200

    [TASK] Fix userscript pwa

--- src/_minimal/components/settings/settings-structure-custom-domains.ts
+++ src/_minimal/components/settings/settings-structure-custom-domains.ts
@@ -1,8 +1,10 @@
 import SettingsCustomDomains from './settings-custom-domains.vue';
 import SettingsCustomDomainsMissingPermissions from './settings-custom-domains-missing-permissions.vue';
+import { ConfObj } from '../../../_provider/definitions';
 
-export const missingPermissions = {
+export const missingPermissions: ConfObj = {
   key: 'permissions',
+  system: 'webextension',
   title: () => api.storage.lang('settings_custom_domains_button'),
   component: SettingsCustomDomainsMissingPermissions,
 };
--- src/floatbutton/userscriptPwa.ts
+++ src/floatbutton/userscriptPwa.ts
@@ -1,27 +1,11 @@
-import { Minimal } from '../minimal/minimalClass';
-
-declare let componentHandler: any;
+import { Minimal } from '../_minimal/minimalClass';
 
 export function pwa() {
   $(document).ready(async function () {
-    document.getElementsByTagName('head')[0].onclick = function (e) {
-      try {
-        componentHandler.upgradeDom();
-      } catch (e2) {
-        console.log(e2);
-        setTimeout(function () {
-          componentHandler.upgradeDom();
-        }, 500);
-      }
-    };
     const head = j.$('head');
-    api.storage.injectjsResource('material.js', head);
-    api.storage.updateDom(head);
-
-    api.storage.injectCssResource('material.css', head);
     api.storage.injectCssResource('materialFont.css', head);
+    api.storage.injectCssResource('montserrat.css', head);
 
     const minimalObj = new Minimal($('html'));
-    minimalObj.fillBase(null);
   });
 }
