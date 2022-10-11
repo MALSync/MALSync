ee4456c684d7cbf822e7b6d56df0c885a4187244
henrik9999 <22085664+henrik9999@users.noreply.github.com>
2022-10-11 17:27:25 +0200

    replaceState

--- src/anilist/oauth.ts
+++ src/anilist/oauth.ts
@@ -12,6 +12,7 @@ export function anilistOauth() {
         $('body').removeClass();
         $('body').addClass('success');
 
+        window.history.replaceState('', '', '/anilist/oauth');
         return;
       }
       error('No tokens found');
