fe3b6c615a07a1156d7b17239db0d955bdebc68c
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2022-11-10 00:14:38 +0100

    [TASK] Add regex to text collector

--- src/utils/mangaProgress/modes/text.ts
+++ src/utils/mangaProgress/modes/text.ts
@@ -2,15 +2,23 @@ import { ModeAbstract } from '../ModeAbstract';
 
 type arguements = {
   selector: string;
+  regex: string;
 };
 
 export class text extends ModeAbstract<arguements> {
   execute(args: arguements) {
-    const textString = j.$(args.selector).first().text();
+    let textString = j.$(args.selector).first().text().trim();
+    if (args.regex) {
+      const regex = new RegExp(args.regex);
+      const match = textString.match(regex);
+      if (match) {
+        [textString] = match;
+      }
+    }
     const n = Number(textString);
     if (Number.isNaN(n) || n === 0)
       throw new Error(`No number found in text '${textString}' (${n})`);
 
-    return j.$(args.selector).first().text();
+    return n;
   }
 }
