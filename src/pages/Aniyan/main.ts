39dfdfbbc83923db1d5657d25b5f1ed6aae48efc
Th1rty <122520882+Th1rty@users.noreply.github.com>
2023-01-24 09:25:48 +0000

    Changes completed

--- src/pages/Aniyan/main.ts
+++ src/pages/Aniyan/main.ts
@@ -16,10 +16,13 @@ export const Aniyan: pageInterface = {
   },
   sync: {
     getTitle(url) {
-      return j.$('.pag_episodes > .item:nth-child(2) > a').attr('title') || '';
+      return getTitle(j.$('.anipage_tituloep > h1').text().replace(/\s+/g, ' '));
     },
     getIdentifier(url) {
-      return Aniyan.sync.getOverviewUrl(url).split('/')[4];
+      return getIdentifiers(
+        Aniyan.sync.getOverviewUrl(url).split('/')[4],
+        j.$('.anipage_tituloep > h1').text().replace(/\s+/g, ' '),
+      );
     },
     getOverviewUrl(url) {
       return j.$('.pag_episodes > .item:nth-child(2) > a').attr('href') || '';
@@ -49,4 +52,27 @@ function getEpisode(text) {
   if (!matches || matches.length === 0) return 1;
 
   return Number(matches[0].replace(/\D+/g, ''));
+}
+function getTitle(text) {
+  if (text.length === 0) return 'N/A1';
+  const regex = /(.*?) - T([0-9]+):E([0-9]+)/gim;
+  const matches = regex.exec(text);
+
+  if (!matches || matches.length === 0) return 'N/A2';
+  if (matches[2] && matches[2] > '1') {
+    return `${matches[1]} Season ${matches[2]}`;
+  }
+  return `${matches[1]}`;
+}
+function getIdentifiers(text, texttwo) {
+  if (text.length === 0) return 'N/A';
+  const regex = /(.*?) - T([0-9]+):E([0-9]+)/gim;
+  const matches = regex.exec(texttwo);
+
+  if (!matches || matches.length === 0) return 'N/A';
+
+  if (matches[2] && matches[2] > '1') {
+    return `${text}?s=${matches[2]}`;
+  }
+  return text;
 }
