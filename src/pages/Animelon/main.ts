99e0663a4aed51d7d1f390f3341063b5c100c2a6
Sayan Dutta <sdutta@relias.com>
2023-02-22 16:33:00 -0500

    animelon complete initial

--- src/pages/Animelon/main.ts
+++ src/pages/Animelon/main.ts
@@ -1,8 +1,11 @@
+import { urlChangeDetect } from '../../utils/general';
 import { pageInterface } from '../pageInterface';
+
+let syncPageEpTitle;
 
 export const Animelon: pageInterface = {
   name: 'Animelon',
-  domain: 'https://www.animelon.com/',
+  domain: 'https://www.animelon.com',
   languages: ['English', 'Japanese'],
   type: 'anime',
   isSyncPage(url) {
@@ -13,37 +16,30 @@ export const Animelon: pageInterface = {
   },
   sync: {
     getTitle(url) {
-      return j
-        .$('ng-bind="languageVideo.title"')
-        .text()
-        .replace(/\sEpisode\s\d+$/, '');
+      return syncPageEpTitle.replace(/\sEpisode\s\d+$/, '');
     },
     getIdentifier(url) {
-      return j
-        .$('ng-bind="languageVideo.title"')
-        .text()
-        .replace(/\sEpisode\s\d+$/, '')
-        .replace(/\s/g, '%20');
+      return encodeURI(syncPageEpTitle.replace(/\sEpisode\s\d+$/, ''));
     },
     getOverviewUrl(url) {
-      return j
-        .$('ng-bind="languageVideo.title"')
-        .text()
-        .replace(/\sEpisode\s\d+$/, '');
+      return utils.absoluteLink(
+        `/series/${encodeURI(syncPageEpTitle.replace(/\sEpisode\s\d+$/, ''))}`,
+        Animelon.domain,
+      );
     },
     getEpisode(url) {
-      return parseInt(j.$('ng-bind="languageVideo.title"').text().match(/\d+$/)![0]) ?? 0;
+      return parseInt(syncPageEpTitle.match(/\d+$/)![0]) ?? 0;
     },
   },
   overview: {
     getTitle(url) {
-      return utils.urlPart(url, 4);
+      return decodeURI(utils.urlPart(url, 4));
     },
     getIdentifier(url) {
       return utils.urlPart(url, 4);
     },
     uiSelector(selector) {
-      j.$('.series-page .right-head-container').children().last().after(j.html(selector));
+      j.$('.right-head-container').append(j.html(selector));
     },
     list: {
       offsetHandler: false,
@@ -54,7 +50,10 @@ export const Animelon: pageInterface = {
         return utils.absoluteLink(selector.attr('href'), Animelon.domain);
       },
       elementEp(selector) {
-        return parseInt(selector.find('ng-bind="episode.title').text().match(/\d+$/)![0]) ?? 0;
+        return parseInt(selector.find('[ng-bind="episode.title"]').text().match(/\d+$/)![0]) ?? 0;
+      },
+      getTotal() {
+        return parseInt(j.$('ng-bind="series.totalEpisodes"').text()) ?? 0;
       },
     },
   },
@@ -62,8 +61,32 @@ export const Animelon: pageInterface = {
     api.storage.addStyle(
       require('!to-string-loader!css-loader!less-loader!./style.less').toString(),
     );
-    j.$(function () {
-      page.handlePage();
-    });
+    j.$(waitPageLoad);
+    urlChangeDetect(waitPageLoad);
+    function waitPageLoad() {
+      if (Animelon.isSyncPage(window.location.href)) {
+        utils.waitUntilTrue(
+          function () {
+            return j.$('[ng-bind="languageVideo.title"]').text().length > 0;
+          },
+          function () {
+            syncPageEpTitle = j.$('[ng-bind="languageVideo.title"]').text();
+            page.handlePage();
+          },
+        );
+      } else {
+        utils.waitUntilTrue(
+          function () {
+            return (
+              j.$('.series-page .right-head-container').length &&
+              j.$('.season-episodes-container > .episode-container').length
+            );
+          },
+          function () {
+            page.handlePage();
+          },
+        );
+      }
+    }
   },
 };
