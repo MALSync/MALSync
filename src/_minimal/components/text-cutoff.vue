6b2e2de382eafc37778f17c69e4dc1d535cdeb20
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2023-03-15 15:40:12 +0100

    [TASK] Add more text cutoff

--- src/_minimal/components/text-cutoff.vue
+++ src/_minimal/components/text-cutoff.vue
@@ -2,14 +2,21 @@
   <div class="cutoff"><slot /></div>
 </template>
 
+<script lang="ts" setup>
+defineProps({
+  lines: {
+    type: Number,
+    default: 2,
+  },
+});
+</script>
+
 <style lang="less" scoped>
 .cutoff {
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  line-height: 21px;
-  max-height: 48px;
-  -webkit-line-clamp: 2;
+  -webkit-line-clamp: v-bind(lines);
   -webkit-box-orient: vertical;
 }
 </style>
