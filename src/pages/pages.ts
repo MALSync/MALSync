5e05fd8518eafe522b53e1137e4276cb6c43c022
Geistful <1567nix14@gmail.com>
2023-03-21 21:41:29 +0300

    Add MangaFire

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -137,6 +137,7 @@ import { Isekaiscan } from './Isekaiscan/main';
 import { FRAnime } from './FRAnime/main';
 import { FMTeam } from './FMTeam/main';
 import { Animelon } from './Animelon/main';
+import { MangaFire } from './MangaFire/main';
 
 export const pages = {
   nineAnime,
@@ -278,4 +279,5 @@ export const pages = {
   FRAnime,
   FMTeam,
   Animelon,
+  MangaFire,
 };
