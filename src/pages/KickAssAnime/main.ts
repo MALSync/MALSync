0f2fd87e3c9283f49b99e766f2e456bd224f086c
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2023-04-10 21:29:14 +0200

    [TASK] Add new kickassanime

--- src/pages/KickAssAnime/main.ts
+++ src/pages/KickAssAnime/main.ts
@@ -2,81 +2,44 @@ import { pageInterface } from '../pageInterface';
 
 export const KickAssAnime: pageInterface = {
   name: 'KickAssAnime',
-  domain: 'https://www.kickassanime.ro',
+  domain: 'https://kaas.am',
   languages: ['English'],
   type: 'anime',
   isSyncPage(url) {
-    return Boolean(utils.urlPart(url, 5));
+    return Boolean(utils.urlPart(url, 4));
+  },
+  isOverviewPage(url) {
+    return Boolean($('.banner-section').length);
   },
   sync: {
     getTitle(url) {
-      return utils.getBaseText($('#animeInfoTab > a'));
+      return $('.v-card__title > span').first().text();
     },
     getIdentifier(url) {
-      return url.split('/')[4];
+      return utils.urlPart(url, 3);
     },
     getOverviewUrl(url) {
-      return `${KickAssAnime.domain}/anime/${KickAssAnime.sync.getIdentifier(url)}`;
+      return `${KickAssAnime.domain}/${KickAssAnime.sync.getIdentifier(url)}`;
     },
     getEpisode(url) {
-      const urlParts = url.split('/');
+      const epText = $('.v-card__title > .text-overline').first().text();
 
-      if (!urlParts || urlParts.length === 0) return NaN;
+      const epParts = epText.match(/episode (\d+)/im);
 
-      const episodePart = urlParts[5];
+      if (!epParts) return NaN;
 
-      if (episodePart.length === 0) return NaN;
-
-      const temp = episodePart.match(/episode-\d*/gi);
-
-      if (!temp || temp.length === 0) return NaN;
-
-      return Number(temp[0].replace(/\D+/g, ''));
-    },
-    nextEpUrl(url) {
-      const href = j.$("#sidebar-anime-info > div > div > a:contains('Next Episode')").attr('href');
-      if (href) return utils.absoluteLink(href, KickAssAnime.domain);
-      return '';
+      return Number(epParts[1]);
     },
   },
   overview: {
     getTitle(url) {
-      return j.$('h1.title').text();
+      return j.$('.anime-info-card .v-card__title').text();
     },
     getIdentifier(url) {
-      return utils.urlPart(url, 4);
+      return utils.urlPart(url, 3);
     },
     uiSelector(selector) {
-      j.$('div.anime-info')
-        .first()
-        .after(
-          j.html(
-            `<div class="border rounded mb-2 p-3"><div class="font-weight-bold">MAL-Sync</div>${selector}</div`,
-          ),
-        );
-    },
-    list: {
-      offsetHandler: false,
-      elementsSelector() {
-        return j.$('div.anime-list tbody > tr');
-      },
-      elementUrl(selector) {
-        return utils.absoluteLink(selector.find('a').attr('href'), KickAssAnime.domain);
-      },
-      elementEp(selector) {
-        const text = selector.find('a').text();
-        if (!text.toLowerCase().includes('episode')) return NaN;
-        return Number(text.replace(/\D+/g, ''));
-      },
-      paginationNext(updateCheck) {
-        con.log('updatecheck', updateCheck);
-        const el = j.$(' div.main-episode-list > div > div > div > button:nth-child(2):enabled');
-        if (typeof el[0] === 'undefined') {
-          return false;
-        }
-        el[0].click();
-        return true;
-      },
+      j.$('.anime-info-card .v-card__text').first().prepend(j.html(selector));
     },
   },
   init(page) {
@@ -84,14 +47,25 @@ export const KickAssAnime: pageInterface = {
       require('!to-string-loader!css-loader!less-loader!./style.less').toString(),
     );
     j.$(document).ready(function () {
-      if (page.url.split('/')[3] === 'anime') {
-        utils.waitUntilTrue(
-          function () {
-            return (
-              KickAssAnime.sync.getTitle(page.url) || KickAssAnime.overview!.getTitle(page.url)
-            );
-          },
-          function () {
+      let until: NodeJS.Timer;
+
+      utils.changeDetect(
+        () => {
+          check();
+        },
+        () => j.$('.v-card__title').text(),
+      );
+
+      check();
+
+      function check() {
+        clearInterval(until);
+        until = utils.waitUntilTrue(
+          () =>
+            KickAssAnime.sync.getTitle(window.location.href) ||
+            KickAssAnime.overview!.getTitle(window.location.href),
+          () => {
+            page.reset();
             page.handlePage();
           },
         );
