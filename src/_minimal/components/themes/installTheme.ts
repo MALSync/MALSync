d6de18c56e754dceb0cb386bc284f81e96254b8e
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2023-04-26 16:08:35 +0200

    [TASK] Small color change

--- src/_minimal/components/themes/installTheme.ts
+++ src/_minimal/components/themes/installTheme.ts
@@ -1,10 +1,13 @@
 import type { Theme } from './index';
 import { nordtheme } from './nordtheme';
 
+const { colors } = nordtheme;
+colors['secondary-text'] = '#f24b00';
+
 export const installTheme: Theme = {
   name: 'Install',
   base: 'dark',
-  colors: nordtheme.colors,
+  colors,
   overrides: {
     sidebars: false,
     image: 'https://malsync.moe/images/install-full.webp',
