395d73b0a5885ba8a949e5ad95c3b5e53fa6df60
bnm <57193455+kyanbasu@users.noreply.github.com>
2023-08-25 10:40:12 +0200

    Update main.ts
    fine tuning and prettier

--- src/pages/OgladajAnime/main.ts
+++ src/pages/OgladajAnime/main.ts
@@ -6,33 +6,28 @@ export const OgladajAnime: pageInterface = {
   languages: ['Polish'],
   type: 'anime',
   isSyncPage(url) {
-      if (url.split('/')[3] == 'anime') return true;
-        return false;
+    if (url.split('/')[3] == 'anime') return true;
+    return false;
   },
   sync: {
     getTitle(url) {
-        return j.$('#anime_name_id').text();
+      return j.$('#anime_name_id').text();
     },
     getIdentifier(url) {
-        return url.split('/')[4];
+      return url.split('/')[4];
     },
     getOverviewUrl(url) {
-  	  let o = url.split('/')
-  	  if(o.length == 6) o.pop()
-        return o.join('/');
-      },
-    getEpisode(url) {
-  	  if(url.split('/')[5] != undefined) return parseInt(url.split('/')[5]);
-  	  return parseInt(j.$('ul > li.active').first().attr('value')!);
+      return url.split('/').length == 6 ? url.split('/').slice(0, -1).join('/') : url
     },
-  	getVolume(url) {
-  	  return Number(j.$('div.vjs-volume-bar.vjs-slider-bar.vjs-slider.vjs-slider-horizontal').attr('aria-valuenow'))
-  	},
+    getEpisode(url) {
+      if (url.split('/')[5] != undefined) return parseInt(url.split('/')[5]);
+      return parseInt(j.$('ul > li.active').first().attr('value')!);
+    },
     nextEpUrl(url) {
       const onclick = j.$('#next_ep_button').attr('onclick');
       if (onclick) {
-		const nextep = onclick.split(", ")[1]
-        return OgladajAnime.sync.getOverviewUrl(url) + "/" + nextep;
+        const nextep = onclick.split(', ')[1];
+        return OgladajAnime.sync.getOverviewUrl(url) + '/' + nextep;
       }
       return '';
     },
@@ -47,23 +42,23 @@ export const OgladajAnime: pageInterface = {
     uiSelector(selector) {
       j.$('#anime_name_id').first().before(j.html(selector));
     },
-
   },
   init(page) {
     api.storage.addStyle(
       require('!to-string-loader!css-loader!less-loader!./style.less').toString(),
     );
     j.$(() => {
-		utils.urlChangeDetect(function() {
-		  utils.waitUntilTrue(
-		  function () {
-			return j.$('ul > li.active').length;
-		  },
-		  function () {
-			page.handlePage();
-		  }
+      page.handlePage();
+      utils.urlChangeDetect(function () {
+        utils.waitUntilTrue(
+          function () {
+            return j.$('ul > li.active').length;
+          },
+          function () {
+            page.handlePage();
+          },
         );
-		})
+      });
     });
   },
 };
