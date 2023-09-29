78590cf594d2e3d4d1ee288b0eb9a2e2636fbb10
sbhat8 <pingbodhi8@gmail.com>
2023-09-29 05:06:14 -0400

    new file:   src/pages/hachi/main.ts 	new file:   src/pages/hachi/meta.json 	new file:   src/pages/hachi/style.less 	new file:   src/pages/hachi/tests.json 	modified:   src/pages/pages.ts

---
+++ src/pages/hachi/main.ts
@@ -0,0 +1,76 @@
+import { pageInterface } from '../pageInterface';
+
+export const hachi: pageInterface = {
+  domain: 'hachi.moe',
+  languages: ['English'],
+  name: 'hachi',
+  type: 'manga',
+  isSyncPage(url) {
+    return /\/chapter\/\d+/.test(url);
+  },
+  isOverviewPage(url) {
+    return url.includes('/article/') && !url.includes('/chapter/');
+  },
+  sync: {
+    getTitle: () => {
+      return j.$('h1').text();
+    },
+    getIdentifier: (url: string) => {
+      return utils.urlPart(url, 4);
+    },
+    getOverviewUrl: (url: string) => {
+      return utils.absoluteLink(url.replace(/\/chapter\/[\d.]+$/, ''), hachi.domain);
+    },
+    getEpisode: (url: string) => {
+      return parseInt(utils.urlPart(url, 6)) || -1;
+    },
+    nextEpUrl: () => {
+      return utils.absoluteLink(
+        j.$('svg.tabler-icon.tabler-icon-arrow-badge-right-filled').parent('a').attr('href')!,
+        hachi.domain,
+      );
+    },
+  },
+  overview: {
+    getTitle: () => {
+      return j
+        .$('h1')
+        .text()
+        .replace(/(.+?)\1+/g, '$1');
+    },
+    getIdentifier: (url: string) => {
+      return utils.urlPart(url, 4);
+    },
+    uiSelector: (selector: string) => {
+      j.$('div.mantine-Grid-root').first().prepend(j.html(selector));
+    },
+    list: {
+      offsetHandler: true,
+      elementsSelector() {
+        return j.$('div.mantine-SimpleGrid-root > a');
+      },
+      elementUrl(selector) {
+        return utils.absoluteLink(selector.attr('href')!, hachi.domain);
+      },
+      elementEp(selector) {
+        return hachi.sync.getEpisode(hachi.overview!.list!.elementUrl!(selector));
+      },
+    },
+  },
+  init: page => {
+    api.storage.addStyle(
+      require('!to-string-loader!css-loader!less-loader!./style.less').toString(),
+    );
+    utils.fullUrlChangeDetect(() => {
+      page.reset();
+      utils.waitUntilTrue(
+        () => {
+          return !!j.$('h1').length;
+        },
+        () => {
+          page.handlePage().then(() => {});
+        },
+      );
+    });
+  },
+};
