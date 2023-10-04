af97f6b1629f17138dee01bb02fc1902c97bb381
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2023-10-04 12:02:48 +0200

    [TASK] Update more tests

--- src/pages/AsuraScans/main.ts
+++ src/pages/AsuraScans/main.ts
@@ -7,7 +7,12 @@ export function getInter(): pageInterface {
   /* eslint-disable-next-line prefer-const */
   thisSelf = {
     name: 'AsuraScans',
-    domain: ['https://asuracomics.com', 'https://asura.gg', 'https://asurascans.com'],
+    domain: [
+      'https://asuratoon.com',
+      'https://asuracomics.com',
+      'https://asura.gg',
+      'https://asurascans.com',
+    ],
     languages: ['English'],
     type: 'manga',
     isSyncPage(url) {
