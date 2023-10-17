6bba97a6415e1adce4434021c3fae9056faf8095
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2023-10-17 13:32:40 +0200

    [TASK] Use same getPage for tests and userscript

---
+++ src/utils/test.ts
@@ -0,0 +1,29 @@
+export function getPageConfig(url, thispages) {
+  for (const key in thispages) {
+    const page = thispages[key];
+    if (j.$.isArray(page.domain)) {
+      let resPage;
+      page.domain.forEach(singleDomain => {
+        if (checkDomain(url, singleDomain)) {
+          page.domain = singleDomain;
+          resPage = page;
+        }
+      });
+      if (resPage) return resPage;
+    } else if (checkDomain(url, page.domain)) {
+      return page;
+    }
+  }
+  return null;
+}
+
+function checkDomain(url, domain) {
+  if (
+    url.indexOf(
+      `${utils.urlPart(domain, 2).replace('.com.br', '.br').split('.').slice(-2, -1)[0]}.`,
+    ) > -1
+  ) {
+    return true;
+  }
+  return false;
+}
