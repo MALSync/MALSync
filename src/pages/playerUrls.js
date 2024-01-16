fbef4d6fcd3e61ae2b44b71014f5c9c604689dad
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2024-01-17 00:49:47 +0100

    [TASK] Add new kwik player

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -96,7 +96,7 @@ module.exports = {
     match: ['*://*.vidlox.me/*'],
   },
   kwik: {
-    match: ['*://*.kwik.cx/*'],
+    match: ['*://*.kwik.cx/*', '*://*.kwik.si/*'],
   },
   mega: {
     match: ['*://*.mega.nz/*'],
