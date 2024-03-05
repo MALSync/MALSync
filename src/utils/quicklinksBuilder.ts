d42833e81a47425cacc8c04ded89a2c4e4e2b358
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2024-03-05 12:25:33 +0100

    [TASK] Remove local [L] from search title

--- src/utils/quicklinksBuilder.ts
+++ src/utils/quicklinksBuilder.ts
@@ -49,7 +49,7 @@ export function titleSearch(url, title, id) {
 type option = 'noEncode' | 'noSpecial' | 'noLowercase' | 'specialReplace';
 
 export function searchSyntax(url, title) {
-  let resTitle = title.trim();
+  let resTitle = title.replace(/^\[l\]/i, '').trim();
   let options: option[] = [];
 
   const found = url.match(/{searchterm(\(.\))?(\[[^[\]]*\])?}/);
