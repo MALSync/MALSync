2ea74dc143efa1ac3058f9189e51cceaddb21faf
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2024-03-28 11:19:17 +0100

    [TASK] Add permissions settings page

---
+++ src/_minimal/components/settings/settings-permission-overview.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="permissions">TODO</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped></style>
--- src/_minimal/components/settings/settings-structure.ts
+++ src/_minimal/components/settings/settings-structure.ts
@@ -14,6 +14,7 @@ import { minimal } from './settings-structure-minimal';
 import { estimation } from './settings-structure-estimation';
 import { links } from './settings-structure-links';
 import { notifications } from './settings-structure-notifications';
+import { permissionsOverview } from './settings-structure-permissions';
 
 export const structure: ConfObj[] = [
   {
@@ -93,6 +94,16 @@ export const structure: ConfObj[] = [
     },
     component: SettingsGroup,
     children: notifications,
+  },
+  {
+    key: 'permissions-overview',
+    title: () => api.storage.lang('settings_permissions_overview'),
+    system: 'webextension',
+    props: {
+      icon: 'lock_open',
+    },
+    component: SettingsGroup,
+    children: permissionsOverview,
   },
   {
     key: 'customDomains',
