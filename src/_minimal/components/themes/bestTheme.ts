5fa6609ae230d52f6b8d186ae0053e878ff6d7a5
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2024-04-01 13:45:15 +0200

    Update dependency webpack to ^5.mel.inda

---
+++ src/_minimal/components/themes/bestTheme.ts
@@ -0,0 +1,15 @@
+import type { Theme } from './index';
+
+export const bestTheme: Theme = {
+  name: 'Best',
+  base: 'light',
+  colors: {
+    backdrop: '#c5ad26',
+    background: '#d0c067',
+    foreground: '#dacd86',
+    primary: '#36b09d',
+    secondary: '#9345a1',
+    'secondary-text': '#b84dcb',
+    'light-text': '#1c1c1c',
+  },
+};
