9fc0755f7a7395dcb90443fb581e3aa8f8e36aa0
Debjoy <debjoybhowal7@gmail.com>
2024-05-12 02:22:08 +0530

    Add miruro website support to malsync

---
+++ src/pages/Miruro/main.ts
@@ -0,0 +1,72 @@
+import { pageInterface } from '../pageInterface';
+
+export const Miruro: pageInterface = {
+  name: 'Miruro',
+  domain: ['https://www.miruro.tv', 'https://www.miruro.online'],
+  languages: ['English'],
+  type: 'anime',
+  isSyncPage(url) {
+    return Boolean(j.$('.player[data-media-player]').length);
+  },
+  isOverviewPage(url) {
+    return false;
+  },
+  getImage() {
+    return $('.player[data-media-player] .vds-poster').attr('src');
+  },
+  sync: {
+    getTitle(url) {
+      return utils.urlPart(url, 5).replace(/-/g, ' ').trim();
+    },
+    getIdentifier(url) {
+      // '139630-1' for https://www.miruro.tv/watch/139630/boku-no-hero-academia-6th-season/1
+      return `${utils.urlPart(url, 4)}-${utils.urlPart(url, 6)}`;
+    },
+    getOverviewUrl(url) {
+      return url;
+    },
+    getEpisode(url) {
+      const temp = utils.urlPart(url, 6);
+      if (!temp) return NaN;
+      return Number(temp);
+    },
+    nextEpUrl(url) {
+      const href = `https://${window.location.hostname}/watch/${utils.urlPart(url, 4)}/${utils.urlPart(url, 5)}/${Miruro.sync.getEpisode(url) + 1}`;
+      if (typeof href !== 'undefined') {
+        return utils.absoluteLink(href, Miruro.domain);
+      }
+      return '';
+    },
+    getMalUrl(provider) {
+      if (provider === 'ANILIST')
+        return `https://anilist.co/anime/${utils.urlPart(window.location.href, 4)}`;
+      return false;
+    },
+  },
+  init(page) {
+    api.storage.addStyle(
+      require('!to-string-loader!css-loader!less-loader!./style.less').toString(),
+    );
+    page.url = window.location.href;
+    ready();
+    utils.urlChangeDetect(function () {
+      ready();
+    });
+    function ready() {
+      page.reset();
+      if (page.url.split('/')[3] === 'watch') {
+        utils.waitUntilTrue(
+          function () {
+            if (j.$('.player[data-media-player] .vds-poster').length) {
+              return true;
+            }
+            return false;
+          },
+          function () {
+            page.handlePage();
+          },
+        );
+      }
+    }
+  },
+};
--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -129,6 +129,7 @@ import { WitAnime } from './WitAnime/main';
 import { Suwayomi } from './Suwayomi/main';
 import { ManhuaUS } from './ManhuaUS/main';
 import { Taiyo } from './Taiyo/main';
+import { Miruro } from './Miruro/main';
 
 export const pages = {
   nineAnime,
@@ -262,4 +263,5 @@ export const pages = {
   Suwayomi,
   ManhuaUS,
   Taiyo,
+  Miruro,
 };
