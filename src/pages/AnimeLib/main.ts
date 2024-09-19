f6e371320971f8503c3a2859949dcc07bd939c29
HappyUni <60145703+DFreezerR@users.noreply.github.com>
2024-09-19 19:01:46 +0300

    AnimeLib fixes with detecting pages + selector fixes

--- src/pages/AnimeLib/main.ts
+++ src/pages/AnimeLib/main.ts
@@ -23,23 +23,19 @@ export const AnimeLib: pageInterface = {
       return utils.urlPart(url, 5);
     },
     getOverviewUrl(url: string): string {
-      const partUrl = j.$('.am_ap').find('a').attr('href')!.split('?')[0];
+      const partUrl = j.$('.am_ap a').attr('href')!.split('?')[0];
       const overview = utils.absoluteLink(partUrl, AnimeLib.domain);
       return overview;
     },
     getEpisode(url: string): number {
-      const ep = parseInt(
-        j.$('.lh_e8.ef_aa').find('.t4_f5').find('span').text().split(' ')[0].trim(),
-      );
-      return ep;
+      return parseInt(j.$('.t4_f5').text());
     },
     nextEpUrl(url: string): string | undefined {
-      const id = (j.$('.lh_e8.ef_aa').find('.t4_f5').next().attr('id') || '').replace('_', '=');
+      let id = j.$('.t4_f5').next().attr('id');
+      if (!id) return undefined;
+      id = id.replace('_', '=');
       const ep = url.split('?').shift();
       return `${ep}?${id}`;
-    },
-    uiSelector(selector) {
-      j.$('.lh_e').after(j.html(`<div class="_container lh_e">${selector}</div>`));
     },
   },
   overview: {
@@ -58,50 +54,58 @@ export const AnimeLib: pageInterface = {
       // eslint-disable-next-line global-require
       require('!to-string-loader!css-loader!less-loader!./style.less').toString(),
     );
-    let Interval;
-    j.$(() => {
-      utils.fullUrlChangeDetect(() => {
-        page.reset();
-        check();
-      });
+
+    let interval: number | NodeJS.Timeout;
+    utils.changeDetect(check, () => {
+      return j.$('meta[property="og:url').attr('content');
     });
+    check();
+
     function check() {
-      // check for correct manga url
+      page.reset();
       con.info('Start checking current page');
-      if (
-        document.title.includes('Страница не найдена 404') ||
-        document.title === '404' ||
-        j.$('.errors-page-text__title').text().trim() === '404'
-      ) {
-        con.error('Error 404');
-        return;
-      }
       // when we are on SYNC page
-      if (AnimeLib.isSyncPage(window.location.href)) {
+      if (AnimeLib.isSyncPage(page.url)) {
         con.info('This is a sync page');
-        clearInterval(Interval);
-        Interval = utils.waitUntilTrue(
+        clearInterval(interval);
+        interval = utils.changeDetect(
           () => {
-            return !!j.$('.menu-item.is-active').length;
-          },
-          () => {
+            page.reset();
             page.handlePage();
           },
+          () => {
+            return detectChangeInPlayer();
+          },
+          true,
         );
       }
       // when we are on OVERVIEW page
-      if (AnimeLib.isOverviewPage!(window.location.href)) {
+      if (AnimeLib.isOverviewPage!(page.url)) {
         con.info('This is an overview page');
-        clearInterval(Interval);
-        Interval = utils.waitUntilTrue(
+        clearInterval(interval);
+        interval = utils.waitUntilTrue(
           () => {
             return !!j.$('.tabs-item').length;
           },
           () => {
             page.handlePage();
           },
+          500,
         );
       }
+    }
+
+    function detectChangeInPlayer() {
+      const episode = j.$('.t4_f5').text();
+      // .i_m   - AnimeLib player (video)
+      // .mn_mo - Kodik player (iframe)
+      const player = j.$(j.$('.i_m') || j.$('.mn_mo')).attr('src');
+      const dubSub = j.$('.tabs-item.is-active').text();
+      return JSON.stringify({
+        episode,
+        player,
+        dubSub,
+      });
     }
   },
 };
