e9684300ed4e627497d2223968356b39921c16ae
HappyUni <60145703+DFreezerR@users.noreply.github.com>
2024-09-24 16:53:03 +0300

    MangaLib sync new UI

--- src/pages/MangaLib/main.ts
+++ src/pages/MangaLib/main.ts
@@ -1,139 +1,177 @@
 import { pageInterface } from '../pageInterface';
+import { SyncPage } from '../syncPage';
+
+const { asyncWaitUntilTrue: awaitReaderLoading, reset: resetAwaitReader } =
+  utils.getAsyncWaitUntilTrue(() => j.$('main img').length);
+
+const { asyncWaitUntilTrue: awaitOverviewLoading, reset: resetAwaitOverview } =
+  utils.getAsyncWaitUntilTrue(() => j.$('.n1_n3').length);
+
+type TMangaData = {
+  id: string;
+  title: string;
+  cover: string;
+  reader: {
+    volume: number;
+    chapter: number;
+  };
+};
+
+const mangaData: TMangaData = {
+  id: '',
+  title: '',
+  cover: '',
+  reader: {
+    volume: 0,
+    chapter: 0,
+  },
+};
 
 export const MangaLib: pageInterface = {
   name: 'MangaLib',
-  domain: 'https://mangalib.me',
+  domain: ['https://test-front.mangalib.me'],
   languages: ['Russian'],
   type: 'manga',
   getImage() {
-    return $('.media-sidebar__cover.paper img').first().attr('src');
+    con.info('getImage', mangaData.cover);
+    return mangaData.cover;
   },
   isSyncPage(url) {
-    if ($('.reader').length) return true;
-    return false;
+    con.info('isSyncPage', utils.urlPart(url, 5) === 'read');
+    return utils.urlPart(url, 5) === 'read';
   },
   isOverviewPage(url) {
-    if ($('.media-container').length) return true;
-    return false;
+    con.info('isOverviewPage', utils.urlPart(url, 4) === 'manga');
+    return utils.urlPart(url, 4) === 'manga';
   },
   sync: {
     getTitle(url) {
-      return $('.reader-header__wrapper > .reader-header-action > .reader-header-action__text')
-        .text()
-        .trim();
+      con.info('getTitle', mangaData.title);
+      return mangaData.title;
     },
     getIdentifier(url) {
-      return utils.urlPart(url, 3);
+      con.info('getIdentifier', mangaData.id);
+      return mangaData.id;
     },
     getOverviewUrl(url) {
-      return $('.reader-header__wrapper a').first().attr('href') || '';
+      con.info('getOverviewUrl', utils.absoluteLink(`ru/manga/${mangaData.id}`, MangaLib.domain));
+      return utils.absoluteLink(`ru/manga/${mangaData.id}`, MangaLib.domain);
     },
     getEpisode(url) {
-      const episodePart = utils.urlPart(url, 5);
-      return Number(episodePart.split('?')[0].replace('c', ''));
+      con.info('getEpisode', mangaData.reader.chapter);
+      return mangaData.reader.chapter;
     },
     getVolume(url) {
-      const volumePart = utils.urlPart(url, 4);
-      return Number(volumePart.replace('v', ''));
+      con.info('getVolume', mangaData.reader.volume);
+      return mangaData.reader.volume;
     },
     nextEpUrl(url) {
-      return $('[class="reader-header-actions"] > a').last().attr('href');
+      const next = j.$('a.q2_bj.q2_r').last().attr('href');
+      con.info('nextEpUrl', utils.absoluteLink(next, MangaLib.domain));
+      return utils.absoluteLink(next, MangaLib.domain);
     },
     readerConfig: [
       {
+        // TODO - Check this conditions
+        condition: '.jv_jw[data-reader-mode="vertical"]',
         current: {
-          selector: '.reader-pages > label > span',
+          selector: '.x7_k0',
           mode: 'text',
-          regex: '\\d+',
+          regex: '\n(\\d+) / (\\d+)',
+          group: 1,
         },
         total: {
-          selector: '.reader-pages > label > span',
+          selector: '.x7_k0',
           mode: 'text',
-          regex: '\\d+$',
+          regex: '\n(\\d+) / (\\d+)',
+          group: 2,
+        },
+      },
+      {
+        condition: '.jv_jw[data-reader-mode="horizontal"]',
+        current: {
+          selector: '.xy_j8',
+          mode: 'countAbove',
+        },
+        total: {
+          selector: '.xy_j8',
+          mode: 'count',
         },
       },
     ],
   },
   overview: {
     getTitle(url) {
-      return $('.media-name__main').text();
+      con.info('getTitle', mangaData.title);
+      return mangaData.title;
     },
     getIdentifier(url) {
-      return utils.urlPart(url, 3);
+      con.info('getIdentifier', mangaData.id);
+      return mangaData.id;
     },
     uiSelector(selector) {
-      $('#media_type')
-        .first()
-        .after(
-          j.html(
-            `<div class="paper section tabs"><div class="media-section media-section_info"><div class=media-section__head><div class=media-section__title>MAL-Sync</div></div><hr><div class=media-description>${selector}</div></div></div>`,
-          ),
-        );
+      j.$('.tabs._border').before(j.html(selector));
     },
   },
-  init(page) {
+  init(page: SyncPage) {
     api.storage.addStyle(
       // eslint-disable-next-line global-require
       require('!to-string-loader!css-loader!less-loader!./style.less').toString(),
     );
-    let Interval;
 
-    $(() => {
-      check();
-      utils.changeDetect(
-        () => {
-          page.reset();
-          check();
-        },
-        () => {
-          return $('head > meta[property="og:title"]').attr('content') || '';
-        },
-      );
+    j.$(() => {
+      utils.fullUrlChangeDetect(check, true);
     });
 
-    function check() {
-      // check for correct manga url
-      if (
-        document.title.includes('Страница не найдена 404') ||
-        document.title === '404' ||
-        $('.text > .title').first().text() === '404'
-      ) {
-        con.error('Error 404');
-        return;
+    async function check() {
+      page.reset();
+      resetAwaitOverview();
+      resetAwaitReader();
+      con.info('Start checking', page.url);
+
+      if (!MangaLib.isSyncPage(page.url) && !MangaLib.isOverviewPage!(page.url)) return;
+
+      let manga_id: string = '';
+
+      // NOTE - if we are on sync page
+      if (MangaLib.isSyncPage(page.url)) {
+        con.info('This is a sync page');
+        const v = utils.urlPart(page.url, 6).substring(1);
+        const c = utils.urlPart(page.url, 7).substring(1);
+        manga_id = utils.urlPart(page.url, 4);
+        mangaData.reader.chapter = Number(c);
+        mangaData.reader.volume = Number(v);
+        await awaitReaderLoading();
       }
-      // check if it's a SYNC/OVERVIEW page
-      if (
-        !MangaLib.isSyncPage(window.location.href) &&
-        !MangaLib.isOverviewPage!(window.location.href)
-      )
-        return;
-      // when we are on SYNC page
-      if (MangaLib.isSyncPage(window.location.href)) {
-        clearInterval(Interval);
-        Interval = utils.waitUntilTrue(
-          () => {
-            return $('#reader-pages > option[selected="true"]').length;
-          },
-          () => {
-            page.handlePage();
-          },
-        );
+      // NOTE - if we are on overview page
+      if (MangaLib.isOverviewPage!(page.url)) {
+        con.info('This is a overview page');
+        manga_id = utils.urlPart(page.url, 5);
+
+        await awaitOverviewLoading();
       }
-      // when we are on OVERVIEW page
-      if (MangaLib.isOverviewPage!(window.location.href)) {
-        if ($('#malthing').length) {
-          $('#malthing').remove();
-        }
-        clearInterval(Interval);
-        Interval = utils.waitUntilTrue(
-          () => {
-            return !!$('.media-name__main').length;
-          },
-          () => {
-            page.handlePage();
-          },
-        );
-      }
+
+      // NOTE - handling data for manga
+      const { data } = await getData(manga_id);
+      con.log('manga_id', manga_id);
+      con.log('data', data);
+      mangaData.id = data.slug_url;
+      mangaData.cover = data.cover.default;
+      if (data.eng_name) {
+        mangaData.title = data.eng_name;
+      } else if (data.name) mangaData.title = data.name;
+      else if (data.ru_name) mangaData.title = data.ru_name;
+
+      page.handlePage();
     }
   },
 };
+
+function apiRequest(path: string) {
+  return api.request.xhr('GET', `https://api.mangalib.me/api/${path}`);
+}
+
+async function getData(manga_id: string) {
+  const data = await apiRequest(`manga/${manga_id}`);
+  return JSON.parse(data.responseText);
+}
