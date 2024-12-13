238b7d7c1a917a2e280413a605acd1633b33222e
Ethan Conneely <econn50@outlook.com>
2024-12-13 00:32:09 +0000

    Add fallback

--- src/_provider/MyAnimeList_api/list.ts
+++ src/_provider/MyAnimeList_api/list.ts
@@ -133,7 +133,7 @@ export class UserList extends ListAbstract {
             apiCacheKey: el.node.id,
             cacheKey: el.node.id,
             type: this.listType,
-            title: useAltTitle ? el.node.alternative_titles.en : el.node.title,
+            title: useAltTitle ? el.node.alternative_titles.en || el.node.title : el.node.title,
             url: `https://myanimelist.net/${this.listType}/${el.node.id}`,
             score: el.list_status.score,
             watchedEp: el.list_status.num_episodes_watched,
