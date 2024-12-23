2cd2f09733d9a7d8ccfa61f132e26cfd360bda52
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2024-12-23 16:59:12 +0100

    [TASK] Rename implementation

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -135,7 +135,7 @@ import { VortexScans } from './VortexScans/main';
 import { AnimeLib } from './AnimeLib/main';
 import { Kavita } from './Kavita/main';
 import { MangaLib } from './MangaLib/main';
-import { Anistream } from './Anistream/main';
+import { Aninexus } from './Aninexus/main';
 
 export const pages = {
   Crunchyroll,
@@ -275,5 +275,5 @@ export const pages = {
   AnimeLib,
   Kavita,
   MangaLib,
-  Anistream,
+  Aninexus,
 };
