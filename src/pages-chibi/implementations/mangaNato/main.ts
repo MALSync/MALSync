4153c86cf45ba894c431dc765bacf519021fc129
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2025-03-19 19:05:25 +0100

    [TASK] Add manganato

---
+++ src/pages-chibi/implementations/mangaNato/main.ts
@@ -0,0 +1,106 @@
+import { PageInterface } from '../../pageInterface';
+
+export const mangaNato: PageInterface = {
+  name: 'MangaMato',
+  database: 'MangaNato',
+  domain: [
+    'https://manganato.gg',
+    'https://natomanga.com',
+    'https://nelomanga.com',
+    'https://mangakakalot.gg',
+  ],
+  languages: ['English'],
+  type: 'manga',
+  urls: {
+    match: [
+      '*://*.manganato.gg/*',
+      '*://*.natomanga.com/*',
+      '*://*.nelomanga.com/*',
+      '*://*.mangakakalot.gg/*',
+    ],
+  },
+  search: 'https://manganato.gg/search/story/{searchterm(_)[noSpecial]}',
+  sync: {
+    isSyncPage($c) {
+      return $c
+        .and(
+          $c.url().urlPart(4).boolean().run(),
+          $c.url().urlPart(3).equals('manga').run(),
+          $c.url().urlPart(5).boolean().run(),
+        )
+        .run();
+    },
+    getTitle($c) {
+      return $c
+        .querySelectorAll('[itemtype="http://schema.org/Thing"]')
+        .at(1)
+        .getAttribute('title')
+        .trim()
+        .run();
+    },
+    getIdentifier($c) {
+      return $c.url().urlPart(4).run();
+    },
+    getOverviewUrl($c) {
+      return $c.url().split('/').slice(0, 5).join('/').run();
+    },
+    getEpisode($c) {
+      return $c.url().urlPart(5).regex('chapter[_-](\\d+)', 1).number().run();
+    },
+    nextEpUrl($c) {
+      return $c
+        .querySelector('.btn-navigation-chap .back')
+        .getAttribute('href')
+        .ifThen($c => $c.urlAbsolute().return().run())
+        .boolean(false)
+        .run();
+    },
+  },
+  overview: {
+    isOverviewPage($c) {
+      return $c
+        .and(
+          $c.url().urlPart(4).boolean().run(),
+          $c.url().urlPart(3).equals('manga').run(),
+          $c.url().urlPart(5).boolean().not().run(),
+        )
+        .run();
+    },
+    getTitle($c) {
+      return $c.querySelector('.manga-info-text h1').text().trim().run();
+    },
+    getIdentifier($c) {
+      return $c.url().this('sync.getIdentifier').run();
+    },
+    uiInjection($c) {
+      return $c.querySelector('.manga-info-chapter').uiBefore().run();
+    },
+  },
+  list: {
+    elementsSelector($c) {
+      return $c.querySelectorAll('.chapter-list .row').run();
+    },
+    elementUrl($c) {
+      return $c.find('a').getAttribute('href').urlAbsolute().run();
+    },
+    elementEp($c) {
+      return $c.this('list.elementUrl').this('sync.getEpisode').run();
+    },
+  },
+  lifecycle: {
+    setup($c) {
+      return $c
+        .addStyle(require('!to-string-loader!css-loader!less-loader!./style.less').toString())
+        .run();
+    },
+    ready($c) {
+      return $c
+        .title()
+        .contains('Error 404')
+        .ifThen($c => $c.string('404').log().return().run())
+        .domReady()
+        .trigger()
+        .run();
+    },
+  },
+};
--- src/pages-chibi/pages.ts
+++ src/pages-chibi/pages.ts
@@ -1,6 +1,9 @@
-import { test } from './implementations/test/main';
 import { PageInterface } from './pageInterface';
+
+import { test } from './implementations/test/main';
+import { mangaNato } from './implementations/mangaNato/main';
 
 export const pages: { [key: string]: PageInterface } = {
   test,
+  mangaNato,
 };
