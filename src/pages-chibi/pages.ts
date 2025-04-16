ca4ab191d0eb7c48b90460abeb480bc24601a32c
HappyUni <60145703+DFreezerR@users.noreply.github.com>
2025-04-17 01:03:34 +0300

    AnimeLib - added pages

--- src/pages-chibi/pages.ts
+++ src/pages-chibi/pages.ts
@@ -3,9 +3,11 @@ import { PageInterface } from './pageInterface';
 import { test } from './implementations/test/main';
 import { mangaNato } from './implementations/mangaNato/main';
 import { gojo } from './implementations/gojo/main';
+import { animeLib } from './implementations/animeLib/main';
 
 export const pages: { [key: string]: PageInterface } = {
   test,
   mangaNato,
   gojo,
+  animeLib,
 };
