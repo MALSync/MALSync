b50f2dc9a9ae34038aa4a8b83131387314c97afa
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2025-04-17 18:27:22 +0200

    [TASK] Remove test page

--- src/pages-chibi/pages.ts
+++ src/pages-chibi/pages.ts
@@ -1,11 +1,9 @@
 import { PageInterface } from './pageInterface';
 
-import { test } from './implementations/test/main';
 import { mangaNato } from './implementations/mangaNato/main';
 import { gojo } from './implementations/gojo/main';
 
 export const pages: { [key: string]: PageInterface } = {
-  test,
   mangaNato,
   gojo,
 };
