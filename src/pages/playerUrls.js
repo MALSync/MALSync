320bad63b3b473a4b9b3e492bd0158f879e2ba7e
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2025-05-17 05:11:22 +0000

    Auto Urls

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -527,6 +527,7 @@ module.exports = {
       '*://diananatureforeign.com/e/*',
       '*://jonathansociallike.com/e/*',
       '*://mariatheserepublican.com/e/*',
+      '*://johnalwayssame.com/e/*',
       // auto-voe-replace-dont-remove
     ],
   },
