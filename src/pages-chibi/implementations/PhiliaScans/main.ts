e7acb91098f1ea233931166443b9166b4269b59d
Gickass <faheemmahidin@gmail.com>
2025-10-08 02:12:59 +0800

    add support to Philia Scans

---
+++ src/pages-chibi/implementations/PhiliaScans/main.ts
@@ -0,0 +1,106 @@
+import { PageInterface } from '../../pageInterface';
+
+export const PhiliaScans: PageInterface = {
+  name: 'PhiliaScans',
+  domain: 'https://philiascans.org/',
+  languages: ['English'],
+  type: 'manga',
+  urls: {
+    match: ['*://philiascans.org/*'],
+  },
+  sync: {
+    isSyncPage($c) {
+      return $c
+        .and(
+          $c.url().urlPart(5).boolean().run(),
+          $c.url().urlPart(3).equals('series').run(),
+          $c.url().urlPart(5).matches('chapter').run(),
+        )
+        .run();
+    },
+    getTitle($c) {
+      return $c.querySelector('.manga-title').text().trim().run();
+    },
+    getIdentifier($c) {
+      return $c.url().this('sync.getOverviewUrl').this('overview.getIdentifier').run();
+    },
+    getImage($c) {
+      return $c.querySelector('[property="og:image"]').getAttribute('content').ifNotReturn().run();
+    },
+    getOverviewUrl($c) {
+      return $c
+        .querySelector('.manga-title')
+        .getAttribute('href')
+        .ifNotReturn()
+        .urlAbsolute()
+        .run();
+    },
+    getEpisode($c) {
+      return $c.url().urlPart(5).regex('chapter[ _-](\\d+)', 1).number().run();
+    },
+    nextEpUrl($c) {
+      return $c
+        .querySelector('.fa-chevron-right')
+        .find('a')
+        .getAttribute('href')
+        .ifNotReturn()
+        .urlAbsolute()
+        .run();
+    },
+    readerConfig: [
+      {
+        current: {
+          selector: '.page img',
+          mode: 'countAbove',
+        },
+        total: {
+          selector: '.page img',
+          mode: 'count',
+        },
+      },
+    ],
+  },
+  overview: {
+    isOverviewPage($c) {
+      return $c
+        .and($c.url().urlPart(4).boolean().run(), $c.url().urlPart(3).equals('series').run())
+        .run();
+    },
+    getTitle($c) {
+      return $c.querySelector('.serie-title').text().trim().run();
+    },
+    getIdentifier($c) {
+      return $c.url().urlPart(4).run();
+    },
+    getImage($c) {
+      return $c.querySelector('[property="og:image"]').getAttribute('content').ifNotReturn().run();
+    },
+    uiInjection($c) {
+      return $c.querySelector('.alternative-title').uiBefore().run();
+    },
+  },
+  list: {
+    elementsSelector($c) {
+      return $c.querySelectorAll('.list-body-hh .item[data-chapter] a').run();
+    },
+    elementUrl($c) {
+      return $c.getAttribute('href').urlAbsolute().run();
+    },
+    elementEp($c) {
+      return $c
+        .closest('.item[data-chapter]')
+        .getAttribute('data-chapter')
+        .regex('chapter[ _-]?(\\d+)', 1)
+        .number()
+        .run(); // include locked chapter
+    },
+  },
+  lifecycle: {
+    setup($c) {
+      return $c.addStyle(require('./style.less?raw').toString()).run();
+    },
+    ready($c) {
+      return $c.detectURLChanges($c.trigger().run()).domReady().trigger().run();
+    },
+  },
+};
