45fff43968f69db941c0a0b458494afa9ade9057
Hagen Lai <107430751+CaptPlasma@users.noreply.github.com>
2025-11-02 11:28:05 -0800

    Implemented fixes
    from faheem3490 (lapis) on the MAL-sync discord

--- src/pages-chibi/implementations/AnimeHeaven/main.ts
+++ src/pages-chibi/implementations/AnimeHeaven/main.ts
@@ -1,4 +1,4 @@
-import { PageInterface } from 'src/pages-chibi/pageInterface';
+import { PageInterface } from '../../pageInterface';
 
 export const AnimeHeaven: PageInterface = {
   name: 'AnimeHeaven',
@@ -17,24 +17,24 @@ export const AnimeHeaven: PageInterface = {
       return $c.querySelector('.c2.ac2').ifNotReturn().text().trim().run();
     },
     getIdentifier($c) {
-      return $c.querySelector('.c2.ac2').getAttribute('href').ifNotReturn().split('?').slice(1,2).join('').run();
+      return $c.this('sync.getOverviewUrl').this('overview.getIdentifier').run();
     },
     getOverviewUrl($c) {
       return $c.querySelector('.c2.ac2').getAttribute('href').ifNotReturn().urlAbsolute().run();
     },
     getEpisode($c) {
-      return $c.querySelector('.linetitle3.c').getBaseText().split(' ').slice(1,2).number().run();
+      return $c.querySelector('.linetitle3.c').text().split('Episode ').at(1).number().run();
     }
   },
   overview: {
     isOverviewPage($c) {
-      return $c.url().urlPart(3).split('?').slice(0,1).join('').equals('anime.php').run();
+      return $c.url().urlPart(3).equals('anime.php').run();
     },
     getTitle($c) {
       return $c.querySelector('.infotitle.c').text().trim().run();
     },
     getIdentifier($c) {
-      return $c.url().urlPart(3).split('?').slice(1,2).join('').run();
+      return $c.url().split('anime.php?').at(1).run();
     },
     uiInjection($c) {
       return $c.querySelector('.infoyear.c').uiAfter().run();
@@ -45,10 +45,10 @@ export const AnimeHeaven: PageInterface = {
   },
   list: {
     elementsSelector($c) {
-      return $c.querySelectorAll('.linetitle2.c2 *').run();
+      return $c.querySelectorAll('.trackep0').run();
     },
     elementEp($c) {
-      return $c.querySelector('.watch2.bc').number().run();
+      return $c.find('.watch2').text().number().run();
     },
   },
   lifecycle: {
@@ -56,18 +56,8 @@ export const AnimeHeaven: PageInterface = {
       return $c.addStyle(require('./style.less?raw').toString()).run();
     },
     ready($c) {
-      return $c
-        .detectURLChanges($c.trigger().run())
-        .waitUntilTrue($c
-          .this('sync.getTitle')
-          .boolean()
-          .or($c
-            .this('overview.getTitle')
-            .boolean()
-            .run()
-          )
-          .run()
-        )
+      return $c.detectURLChanges($c.trigger().run())
+        .domReady()
         .trigger()
         .run();
     },
