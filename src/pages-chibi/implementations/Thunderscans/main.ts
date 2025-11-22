28c7b4a050e3d14a7f74b7a69e46ebb40fd75a7e
Gickass <faheemmahidin@gmail.com>
2025-11-23 04:25:07 +0800

    Thunderscans, add list to include paid chapter, customize style.less

--- src/pages-chibi/implementations/Thunderscans/main.ts
+++ src/pages-chibi/implementations/Thunderscans/main.ts
@@ -19,7 +19,7 @@ export const Thunderscans: PageInterface = {
       return $c.querySelector('.allc a').text().trim().run();
     },
     getIdentifier($c) {
-      return $c.url().this('sync.getOverviewUrl').this('overview.getIdentifier').run();
+      return $c.this('sync.getOverviewUrl').this('overview.getIdentifier').run();
     },
     getOverviewUrl($c) {
       return $c.querySelector('.allc a').getAttribute('href').ifNotReturn().urlAbsolute().run();
@@ -29,37 +29,23 @@ export const Thunderscans: PageInterface = {
     },
     nextEpUrl($c) {
       return $c
-        .querySelector('.ch-next-btn')
-        .getAttribute('href')
+        .querySelector('.ch-next-btn:not(.disabled)')
         .ifNotReturn()
+        .getAttribute('href')
         .urlAbsolute()
         .run();
     },
     readerConfig: [
       {
-        condition: '#select-paged',
-        current: {
-          selector: '#select-paged option:selected',
-          mode: 'text',
-          regex: '(\\d+)/(\\d+)$',
-          group: 1,
-        },
-        total: {
-          selector: '#select-paged option:selected',
-          mode: 'text',
-          regex: '(\\d+)/(\\d+)$',
-          group: 2,
-        },
-      },
-      {
-        current: {
-          selector: '#readerarea img',
-          mode: 'countAbove',
-        },
-        total: {
-          selector: '#readerarea img',
-          mode: 'count',
-        },
+        current: $c =>
+          $c
+            .querySelector('#select-paged option:checked')
+            .text()
+            .regex('(\\d+)/(\\d+)$', 1)
+            .number()
+            .run(),
+        total: $c =>
+          $c.querySelector('#select-paged option').text().regex('(\\d+)/(\\d+)$', 2).number().run(),
       },
     ],
   },
@@ -70,14 +56,7 @@ export const Thunderscans: PageInterface = {
         .run();
     },
     getTitle($c) {
-      return $c
-        .querySelector('h1')
-        .text()
-        .trim()
-        .replaceAll('\n', ' ')
-        .replaceRegex('\\s+', ' ')
-        .trim()
-        .run();
+      return $c.querySelector('h1').text().trim().run();
     },
     getIdentifier($c) {
       return $c.url().urlPart(4).trim().run();
@@ -97,7 +76,7 @@ export const Thunderscans: PageInterface = {
       return $c.closest('a').getAttribute('href').urlAbsolute().run();
     },
     elementEp($c) {
-      return $c.this('list.elementUrl').this('sync.getEpisode').run();
+      return $c.find('.chapternum').text().regex('chapter\\s*(\\d+)', 1).number().run();
     },
   },
   lifecycle: {
