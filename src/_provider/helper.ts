af8c235205c2f3230e45bf6f32ed41c7b21e4788
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2025-12-03 15:29:22 +0100

    [TASK] Use slug util to detect type in singleFactory

--- src/_provider/helper.ts
+++ src/_provider/helper.ts
@@ -3,7 +3,7 @@ export type SyncTypes = 'MAL' | 'ANILIST' | 'KITSU' | 'SIMKL' | 'SHIKI' | 'MALAP
 export function getSyncMode(type = '') {
   const mode = api.settings.get('syncMode');
   //
-  if (mode === 'SIMKL' && (type === 'manga' || type.indexOf('/manga/') !== -1)) {
+  if (mode === 'SIMKL' && type === 'manga') {
     return api.settings.get('syncModeSimkl');
   }
   //
