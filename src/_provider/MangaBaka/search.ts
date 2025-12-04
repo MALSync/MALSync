2c4c39ca01338f88d8a5a5ef27e8183800caa3e9
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2025-12-04 17:35:05 +0100

    [TASK] Add mangabaka search

---
+++ src/_provider/MangaBaka/search.ts
@@ -0,0 +1,37 @@
+import { searchInterface } from '../definitions';
+import { call, getAlternativeTitles, urls } from './helper';
+import type { SearchResponse } from './types';
+
+export const search: searchInterface = async function (
+  keyword,
+  type: 'anime' | 'manga',
+  options = {},
+  sync = false,
+) {
+  const json = (await call(urls.search(keyword))) as SearchResponse;
+
+  return json.data.map(item => {
+    return {
+      id: item.id,
+      name: item.title,
+      altNames: getAlternativeTitles(item),
+      url: `https://mangabaka.org/${item.id}`,
+      malUrl: () => {
+        return Promise.resolve(
+          item.source.my_anime_list?.id
+            ? `https://myanimelist.net/manga/${item.source.my_anime_list.id}`
+            : null,
+        );
+      },
+      image: item.cover.x150.x2 || '',
+      imageLarge: item.cover.x350.x2 || '',
+      media_type: item.type
+        ? (item.type.charAt(0) + item.type.slice(1).toLowerCase()).replace('_', ' ')
+        : '',
+      isNovel: item.type === 'novel',
+      score: item.rating?.toFixed(0) || '',
+      year: String(item.year),
+      totalEp: Number(item.total_chapters) || 0,
+    };
+  });
+};
