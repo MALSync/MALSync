4fce5afd93d39aa1b892ee5f962623239c86e827
Dom <dominiquedomenco@gmail.com>
2026-01-15 04:57:50 -0500

    Improve autoUrls logging

--- webpackConfig/autoUrls.mjs
+++ webpackConfig/autoUrls.mjs
@@ -5,151 +5,210 @@ import path from 'path';
 
 // players
 async function voe() {
-    const response = await fetch('https://voe.sx/e/2sqxhth1ukzh');
-    const body = await response.text();
-    const loc = body.match(/window\.location\.href = '(.*)'/i);
+  const response = await fetch('https://voe.sx/e/2sqxhth1ukzh');
+  const body = await response.text();
+  const loc = body.match(/window\.location\.href = '(.*)'/i);
 
-    const url = new URL(loc[1]);
+  const url = new URL(loc[1]);
 
-    addPlayerUrls('voe', [url.hostname + '/e/*']);
+  addPlayerUrls('voe', [url.hostname + '/e/*']);
 }
 
 async function vidmoly() {
     const response = await fetch("https://vidmoly.me/embed-1abcdefghi1j.html", {redirect: 'manual'})
     const url = new URL(response.headers.get("Location"));
 
-    addPlayerUrls('vidmoly', [url.hostname + '/*']);
+  addPlayerUrls('vidmoly', [url.hostname + '/*']);
 }
 
 async function mixdrop() {
     const response = await fetch("https://mdfx9dc8n.net/e/3nl0j0lec477v9", {redirect: 'manual'})
     const url = new URL(response.headers.get("Location"));
 
-    addPlayerUrls('mixdrop', ['*.' + url.hostname + '/e/*']);
+  addPlayerUrls('mixdrop', ['*.' + url.hostname + '/e/*']);
 }
 
 // pages
 
 async function zoro() {
     const response = await fetch("https://hianime.tv");
-    const body = await response.text();
+  const body = await response.text();
 
-    const $ = cheerio.load(body);
+  const $ = cheerio.load(body);
 
     const urls = $('ul.site-opt > li > a').map((i,el) =>  new URL($(el).attr('href'))).get();
 
-    for(const url of urls) {
-        addpageUrls('Zoro', [
-            "*://" + url.hostname + "/*"
-        ])
-    }
+  let formattedUrls = [];
+  for(const url of urls) {
+    formattedUrls.push("*://" + url.hostname + "/*");
+  }
+  addpageUrls('Zoro', formattedUrls);
 }
 
 async function kickassanime() {
     const response = await fetch("https://watchanime.io");
-    const body = await response.text();
+  const body = await response.text();
 
-    const $ = cheerio.load(body);
+  const $ = cheerio.load(body);
 
     const urls = $('.domain-btn').map((i,el) =>  new URL($(el).attr('href'))).get();
 
+    let formattedUrls = [];
     for(let url of urls) {
-        addpageUrls('KickAssAnime', [
-            "*://*." + url.hostname + "/*",
-        ])
+      formattedUrls.push("*://*." + url.hostname + "/*");
     }
+    addpageUrls('KickAssAnime', formattedUrls);
 }
 
 async function animekai() {
     const response = await fetch('https://animekai.me/');
-    const body = await response.text();
+  const body = await response.text();
 
-    const $ = cheerio.load(body);
+  const $ = cheerio.load(body);
 
-    const urls = $('.site-lists li > a')
-        .map((i, el) => new URL($(el).attr('href')))
-        .get();
+  const urls = $('.site-lists li > a')
+    .map((i, el) => new URL($(el).attr('href')))
+    .get();
 
-    for (const url of urls) {
-        addChibiUrls('AnimeKAI', ['*://' + url.hostname + '/*']);
-    }
+  let formattedUrls = [];
+  for (const url of urls) {
+    formattedUrls.push('*://' + url.hostname + '/*');
+  }
+  addChibiUrls('AnimeKAI', formattedUrls);
 }
 
 function addpageUrls(page, urls) {
-    let file = JSON.parse(fs.readFileSync(path.resolve(`./src/pages/${page}/meta.json`), 'utf8'));
+  logFoundUrls(page, urls, URL_TYPES.PAGE);
 
-    for (const url of urls) {
+  let file = JSON.parse(fs.readFileSync(path.resolve(`./src/pages/${page}/meta.json`), 'utf8'));
+
+  for (const url of urls) {
         if(!file.urls.match.includes(url)) {
-            file.urls.match.push(url);
-        }
+      file.urls.match.push(url);
     }
+  }
 
     fs.writeFileSync(path.resolve(`./src/pages/${page}/meta.json`), JSON.stringify(file, null, 2) + "\n");
 }
 
 function addChibiUrls(page, urls) {
-    let file = fs.readFileSync(
-        path.resolve(`src/pages-chibi/implementations/${page}/main.ts`),
-        'utf8',
-    );
+  logFoundUrls(page, urls, URL_TYPES.CHIBI);
 
-    const matchRegex = /match:\s*\[(.*?)\]/s;
-    const matchMatch = file.match(matchRegex);
-    if (!matchMatch) {
-        throw new Error(`No match found in ${page} main.ts`);
+  let file = fs.readFileSync(
+    path.resolve(`src/pages-chibi/implementations/${page}/main.ts`),
+    'utf8',
+  );
+
+  const matchRegex = /match:\s*\[(.*?)\]/s;
+  const matchMatch = file.match(matchRegex);
+  if (!matchMatch) {
+    throw new Error(`No match found in ${page} main.ts`);
+  }
+
+  const urlRegex = matchMatch[1].match(/'([^']+)'/g) || [];
+  const matchUrls = urlRegex.map(url => url.replace(/'/g, ''));
+
+  for (const url of urls) {
+    if (!matchUrls.includes(url)) {
+      matchUrls.push(url);
     }
+  }
 
-    const urlRegex = matchMatch[1].match(/'([^']+)'/g) || [];
-    const matchUrls = urlRegex.map(url => url.replace(/'/g, ''));
-
-    for (const url of urls) {
-        if (!matchUrls.includes(url)) {
-        matchUrls.push(url);
-        }
-    }
-
-    const updatedFile = file.replace(matchRegex, `match: [${matchUrls.map(url => `'${url}'`).join(', ')}]`);
-    fs.writeFileSync(path.resolve(`src/pages-chibi/implementations/${page}/main.ts`), updatedFile);
+  const updatedFile = file.replace(matchRegex, `match: [${matchUrls.map(url => `'${url}'`).join(', ')}]`);
+  fs.writeFileSync(path.resolve(`src/pages-chibi/implementations/${page}/main.ts`), updatedFile);
 }
 
 function addPlayerUrls(key, urls) {
-    let file = fs.readFileSync(path.resolve('./src/pages/playerUrls.js'), 'utf8');
+  logFoundUrls(key, urls, URL_TYPES.PLAYER);
 
-    const comment = `      // auto-${key}-replace-dont-remove`;
+  let file = fs.readFileSync(path.resolve('./src/pages/playerUrls.js'), 'utf8');
 
-    let data = "";
-    for (const url of urls) {
+  const comment = `      // auto-${key}-replace-dont-remove`;
+
+  let data = "";
+  for (const url of urls) {
         if(!file.includes(url)) {
             data += `      '*://${url}',\n`
         }
-    }
-    data += comment;
-    file = file.replace(comment, data);
+  }
+  data += comment;
+  file = file.replace(comment, data);
 
-    fs.writeFileSync(path.resolve('./src/pages/playerUrls.js'), file);
+  fs.writeFileSync(path.resolve('./src/pages/playerUrls.js'), file);
+}
+
+const URL_TYPES = {
+  PAGE: 'page',
+  CHIBI: 'chibi',
+  PLAYER: 'player',
+};
+/**
+ * Logs the url(s) found in page, chibi or player.
+ * @param {'page' | 'chibi' | 'player'} type - The type of url (sets log message)
+ */
+function logFoundUrls(key, urls, type = URL_TYPES.CHIBI) {
+  switch (type) {
+    case 'page':
+      if (urls.length <= 0) {
+        console.log(`\n[${key}] No Page found`);
+      } else if (urls.length === 1) {
+        console.log(`\n[${key}] Page found:\n`, urls[0]);
+      } else if (urls.length > 1) {
+        console.log(`\n[${key}] Pages found:\n`, urls.join(',\n '));
+      }
+      break;
+    case 'chibi':
+      if (urls.length <= 0) {
+        console.log(`\n[${key}] No Chibi found`);
+      } else if (urls.length === 1) {
+        console.log(`\n[${key}] Chibi found:\n`, urls[0]);
+      } else if (urls.length > 1) {
+        console.log(`\n[${key}] Chibis found:\n`, urls.join(',\n '));
+      }
+      break;
+    case 'player':
+      if (urls.length <= 0) {
+        console.log(`\n[${key}] No Player found`);
+      } else if (urls.length === 1) {
+        console.log(`\n[${key}] Player found:\n`, urls[0]);
+      } else if (urls.length > 1) {
+        console.log(`\n[${key}] Players found:\n`, urls.join(',\n'));
+      }
+      break;
+    default:
+      throw new Error('Invalid type provided.');
+  }
 }
 
 async function start() {
-    let lastError = null;
-    const tasks = {
-        voe,
-        // vidmoly,
-        mixdrop,
-        zoro,
-        // kickassanime,
-        animekai
-    }
+  const failedTasks = [];
+  const succeededTasks = [];
+  const tasks = {
+    voe,
+    // vidmoly,
+    mixdrop,
+    zoro,
+    // kickassanime,
+    animekai
+  }
 
-    for(const key of Object.keys(tasks)) {
-        await tasks[key]().catch(e => {
-            console.error(`[${key}]:`, e);
-            lastError = e;
-        });
-    }
+  for(const key of Object.keys(tasks)) {
+        await tasks[key]()
+          .then(() => succeededTasks.push(key))
+          .catch(e => {
+            console.error(`\n[${key}]:`, e);
+            failedTasks.push(key);
+          });
+  }
 
-    if (lastError) {
-        throw new Error('Some tasks failed');
-    }
+  if (succeededTasks.length) {
+    console.log('\n\n\x1b[32mTasks succeeded:\x1b[0m', succeededTasks.join(', '));
+  }
+  if (failedTasks.length) {
+    console.log('\x1b[31mTasks failed:\x1b[0m', failedTasks.join(', '));
+  }
+
+  console.log('\nAutoUrls — Done.');
 }
 
 start();
