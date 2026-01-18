b834ed60d152aa0b1adaae3ef0b3c16dd9355630
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2026-01-18 13:06:10 +0100

    [TASK] Fix Kagane

--- test/headless/test.js
+++ test/headless/test.js
@@ -177,6 +177,7 @@ async function PreparePage(block, page, url, testPage) {
     log(block, 'Cached', 2);
 
     await page.setRequestInterception(true);
+    await page.setBypassServiceWorker(true);
 
     page.on('request', (request) => {
       if (!request.isInterceptResolutionHandled()) {
