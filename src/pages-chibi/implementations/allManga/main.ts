1c709e18b3dbe32d23ddc8c92a015035b7cdf198
Gickass <faheemmahidin@gmail.com>
2026-02-13 11:06:44 +0800

    add support to allManga to

--- src/pages-chibi/implementations/allManga/main.ts
+++ src/pages-chibi/implementations/allManga/main.ts
@@ -5,41 +5,47 @@ export const allManga: PageInterface = {
   name: 'allManga',
   domain: 'https://allmanga.to',
   languages: ['English'],
-  type: 'manga',
+  type: 'anime',
   urls: {
-    // Url here is mess and cannot be use in coding
     match: ['*://allmanga.to/*', '*://youtu-chan.com/*'],
   },
   search: 'https://allmanga.to/search-manga?cty=ALL&query={searchtermRaw}',
   computedType: $c => {
-    return $c.querySelector('breadcrumb-item a').text().toLowerCase().run();
+    return $c.querySelector('.breadcrumb-item a[href]').text().trim().toLowerCase().run();
   },
+  // Url link is mess and cannot be use in as reference
   sync: {
     isSyncPage($c) {
-      return getJsonData($c).length().equals(3).run();
+      return getUrl($c).urlPart(5).matches('-\\d+-').run();
     },
     getTitle($c) {
-      return $c.querySelector('.h4 a').text().trim().run();
+      return $c
+        .querySelectorAll('.breadcrumb-item a')
+        .arrayFind($item =>
+          $item.getAttribute('href').urlPart(4).matches($c.this('sync.getIdentifier').run()).run(),
+        )
+        .text()
+        .trim()
+        .run();
     },
     getIdentifier($c) {
-      return $c.this('overview.getIdentifier').run();
+      return getUrl($c).urlPart(4).run();
     },
     getOverviewUrl($c) {
-      return $c.querySelector('.h4 a').getAttribute('href').ifNotReturn().urlAbsolute().run();
+      return getUrl($c).replaceRegex('/([^/]+)$', '').urlAbsolute().run();
     },
     getImage($c) {
-      return $c.querySelector('.card-body img').getAttribute('src').ifNotReturn().run();
+      return $c
+        .coalesce(
+          $c.querySelector('.card-body img').run(),
+          $c.querySelector('.card-header img').run(),
+        )
+        .getAttribute('src')
+        .ifNotReturn()
+        .run();
     },
     getEpisode($c) {
-      return getJsonData($c).get('2').get('name').string().regex('(\\d+)', 1).number().run();
-    },
-    nextEpUrl($c) {
-      return $c
-        .querySelector('.container .btn[aria-label*="Next"]:not(.disabled)')
-        .ifNotReturn()
-        .url()
-        .urlAbsolute()
-        .run();
+      return getUrl($c).urlPart(5).regex('-(\\d+)-', 1).number().run();
     },
     readerConfig: [
       {
@@ -50,13 +56,15 @@ export const allManga: PageInterface = {
   },
   overview: {
     isOverviewPage($c) {
-      return getJsonData($c).length().equals(2).run();
+      return $c
+        .and(getUrl($c).urlPart(5).boolean().not().run(), getUrl($c).urlPart(4).boolean().run())
+        .run();
     },
     getTitle($c) {
       return $c.querySelector('.breadcrumb-item span').text().trim().run();
     },
     getIdentifier($c) {
-      return getJsonData($c).get('1').get('item').string().urlPart(4).run();
+      return $c.this('sync.getIdentifier').run();
     },
     getImage($c) {
       return $c.querySelector('[property="og:image"]').getAttribute('content').ifNotReturn().run();
@@ -78,23 +86,27 @@ export const allManga: PageInterface = {
       return $c.addStyle(require('./style.less?raw').toString()).run();
     },
     ready($c) {
-      return $c.detectChanges(getJsonData($c).run(), $c.trigger().run()).domReady().trigger().run();
+      return $c
+        .detectChanges(getUrl($c).ifNotReturn().run(), $c.trigger().run())
+        .domReady()
+        .trigger()
+        .run();
     },
     syncIsReady($c) {
-      return $c.waitUntilTrue($c.querySelector('.h4 a').boolean().run()).trigger().run();
+      return $c.waitUntilTrue($c.querySelector('.container').boolean().run()).trigger().run();
     },
   },
 };
 
-// I don't know if it is a good idea to use json or query selector.
-function getJsonData($c: ChibiGenerator<unknown>) {
+function getUrl($c: ChibiGenerator<unknown>) {
   return $c
     .querySelector('[type="application/ld+json"]')
     .ifNotReturn()
     .text()
     .jsonParse()
     .get('@graph')
-    .get('2')
-    .get('itemListElement')
-    .type<HTMLElement[]>();
+    .get('0')
+    .get('url')
+    .string()
+    .trim();
 }
