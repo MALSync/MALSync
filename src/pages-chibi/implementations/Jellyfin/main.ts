e8e25b8e090dae3710096466e31ebb55300bfed8
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2026-03-04 00:52:38 +0100

    [TASK] Improve typing for Jellyfin implementation

--- src/pages-chibi/implementations/Jellyfin/main.ts
+++ src/pages-chibi/implementations/Jellyfin/main.ts
@@ -1,6 +1,41 @@
 import type { ChibiGenerator } from '../../../chibiScript/ChibiGenerator';
 import type { PageInterface } from '../../pageInterface';
 
+type CommonMetadata = {
+  Id: string;
+  Path: string;
+  GenreItems: { Name: string }[];
+  Tags: string[];
+};
+
+type EpisodeMetadata = {
+  Type: 'Episode';
+  SeasonId: string;
+  SeasonName: string;
+  SeriesId: string;
+  SeriesName: string;
+  IndexNumber: number;
+  ParentIndexNumber: number;
+} & CommonMetadata;
+
+type SeasonMetadata = {
+  Type: 'Season';
+  SeriesId: string;
+  SeriesName: string;
+  IndexNumber: number;
+} & CommonMetadata;
+
+type MovieMetadata = {
+  Type: 'Movie';
+  Name: string;
+} & CommonMetadata;
+
+type Metadata = EpisodeMetadata | SeasonMetadata | MovieMetadata;
+
+type OverviewMeta = SeasonMetadata;
+
+type SyncMeta = EpisodeMetadata | MovieMetadata;
+
 export const Jellyfin: PageInterface = {
   name: 'Jellyfin',
   domain: 'https://jellyfin.org/',
@@ -21,24 +56,24 @@ export const Jellyfin: PageInterface = {
           $c.querySelector('video').boolean().run(),
           $c
             .or(
-              meta($c).get('Type').equals('Episode').run(),
-              meta($c).get('Type').equals('Movie').run(),
+              meta<SyncMeta>($c).get('Type').equals('Episode').run(),
+              meta<SyncMeta>($c).get('Type').equals('Movie').run(),
             )
             .run(),
         )
         .run();
     },
     getTitle($c) {
-      const title = meta($c).get('SeriesName') as ChibiGenerator<string>;
-      const seasonIndex = meta($c).get('ParentIndexNumber');
+      const title = meta<EpisodeMetadata>($c).get('SeriesName');
+      const seasonIndex = meta<EpisodeMetadata>($c).get('ParentIndexNumber');
 
-      return meta($c)
+      return meta<SyncMeta>($c)
         .get('Type')
         .equals('Movie')
-        .ifThen($c => meta($c).get('Name').return().run())
+        .ifThen($c => meta<MovieMetadata>($c).get('Name').return().run())
         .if(
           seasonIndex.number().greaterThan(1).run(),
-          title.concat(' ').concat('Season ').concat(seasonIndex.run()).run(),
+          title.concat(' ').concat('Season ').concat(seasonIndex.string().run()).run(),
           title.run(),
         )
         .run();
@@ -46,9 +81,9 @@ export const Jellyfin: PageInterface = {
     getIdentifier($c) {
       return $c
         .coalesceFn(
-          meta($c).get('SeasonId').run(),
-          meta($c).get('SeriesId').run(),
-          meta($c).get('Id').run(),
+          meta<EpisodeMetadata>($c).get('SeasonId').run(),
+          meta<EpisodeMetadata>($c).get('SeriesId').run(),
+          meta<SyncMeta>($c).get('Id').run(),
         )
         .run();
     },
@@ -62,21 +97,28 @@ export const Jellyfin: PageInterface = {
         .run();
     },
     getEpisode($c) {
-      return meta($c).get('IndexNumber').run();
+      return meta<EpisodeMetadata>(
+        meta<SyncMeta>($c)
+          .get('Type')
+          .equals('Movie')
+          .ifThen($c => $c.number(1).return().run()),
+      )
+        .get('IndexNumber')
+        .run();
     },
   },
   overview: {
     isOverviewPage($c) {
-      return meta($c).get('Type').equals('Season').run();
+      return meta<OverviewMeta>($c).get('Type').equals('Season').run();
     },
     getTitle($c) {
-      const title = meta($c).get('SeriesName') as ChibiGenerator<string>;
-      const seasonIndex = meta($c).get('IndexNumber');
+      const title = meta<OverviewMeta>($c).get('SeriesName');
+      const seasonIndex = meta<OverviewMeta>($c).get('IndexNumber');
 
       return $c
         .if(
           seasonIndex.number().greaterThan(1).run(),
-          title.concat(' ').concat('Season ').concat(seasonIndex.run()).run(),
+          title.concat(' ').concat('Season ').concat(seasonIndex.string().run()).run(),
           title.run(),
         )
         .run();
@@ -85,7 +127,7 @@ export const Jellyfin: PageInterface = {
       return $c.string('').run();
     },
     getIdentifier($c) {
-      return meta($c).get('Id').run();
+      return meta<OverviewMeta>($c).get('Id').run();
     },
     uiInjection($c) {
       return $c.querySelector('.page:not(.hide) .detailSectionContent').uiPrepend().run();
@@ -144,17 +186,18 @@ function checkMetadataRequest($c: ChibiGenerator<unknown>) {
 function isAnime($c: ChibiGenerator<unknown>) {
   return $c
     .or(
-      ($c.getVariable('metadata').get('Path') as ChibiGenerator<string>)
-        .toLowerCase()
-        .includes('anime')
-        .run(),
-      ($c.getVariable('metadata').get('GenreItems') as ChibiGenerator<{ Name: string }[]>)
+      $c.getVariable<Metadata>('metadata').get('Path').toLowerCase().includes('anime').run(),
+      $c
+        .getVariable<Metadata>('metadata')
+        .get('GenreItems')
         .arrayFind(genre =>
           (genre.get('Name') as ChibiGenerator<string>).toLowerCase().equals('anime').run(),
         )
         .boolean()
         .run(),
-      ($c.getVariable('metadata').get('Tags') as ChibiGenerator<string[]>)
+      $c
+        .getVariable<Metadata>('metadata')
+        .get('Tags')
         .arrayFind(tag => tag.toLowerCase().equals('anime').run())
         .boolean()
         .run(),
@@ -180,6 +223,6 @@ function handleMovie($c: ChibiGenerator<unknown>) {
   );
 }
 
-function meta($c: ChibiGenerator<unknown>) {
-  return $c.getGlobalVariable('metadataGlobal');
+function meta<P>($c: ChibiGenerator<unknown>) {
+  return $c.getGlobalVariable<P>('metadataGlobal');
 }
