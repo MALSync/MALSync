b8acfab462368bf5677a4ff04e4336581b2c01d4
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2026-03-18 22:53:42 +0100

    [TASK] Add new player

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -84,7 +84,7 @@ module.exports = {
     match: ['*://*.mega.nz/*'],
   },
   animeflv: {
-    match: ['*://*.animeflv.net/*', '*://*.jwplayerhls.com/*'],
+    match: ['*://*.animeflv.net/*', '*://*.jwplayerhls.com/*', '*://playnixes.com/*'],
   },
   netu: {
     match: ['*://*.hqq.tv/*', '*://waaw.tv/*'],
