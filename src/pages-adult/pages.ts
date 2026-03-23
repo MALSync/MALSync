6ba69037e47003d79a34045f2f0dc371364eac80
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2026-03-23 10:57:24 +0100

    [TASK] Add mangadistrict

--- src/pages-adult/pages.ts
+++ src/pages-adult/pages.ts
@@ -1,7 +1,9 @@
 import { PageInterface } from '../pages-chibi/pageInterface';
 
 import { HentaiOcean } from './implementations/HentaiOcean/main';
+import { Mangadistrict } from './implementations/Mangadistrict/main';
 
 export const pages: { [key: string]: PageInterface } = {
   HentaiOcean,
+  Mangadistrict,
 };
