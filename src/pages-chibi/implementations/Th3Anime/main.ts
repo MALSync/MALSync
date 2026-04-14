74aa1ed3f157e77b3011310d766b354530bd8b33
BiscuitDev <86505354+Boxofbiscuits97@users.noreply.github.com>
2026-04-14 05:27:56 -0400

    Fix getTitle() and run lint:fix

--- src/pages-chibi/implementations/Th3Anime/main.ts
+++ src/pages-chibi/implementations/Th3Anime/main.ts
@@ -1,12 +1,12 @@
 import { PageInterface } from '../../pageInterface';
 
 export const Th3Anime: PageInterface = {
-  name: "Th3Anime",
-  domain: "https://th3anime.me/",
-  languages: ["English"],
-  type: "anime",
+  name: 'Th3Anime',
+  domain: 'https://th3anime.me/',
+  languages: ['English'],
+  type: 'anime',
   urls: {
-    match: ["*://th3anime.me/*"],
+    match: ['*://th3anime.me/*'],
   },
   search: 'https://th3anime.me/search?keyword={searchtermPlus}',
   sync: {
@@ -16,48 +16,49 @@ export const Th3Anime: PageInterface = {
         .run();
     },
     getTitle($c) {
-      return $c.querySelector(".text-white dynamic-name").getAttribute("data-title").trim().run();
+      return $c.querySelector('.text-white.dynamic-name').getAttribute('data-title').trim().run();
     },
     getIdentifier($c) {
       return $c.url().urlPart(4).run();
     },
     getOverviewUrl($c) {
-      return $c.url().urlStrip().replace("watch", "details").run();
+      return $c.url().urlStrip().replace('watch', 'details').run();
     },
     getEpisode($c) {
-      return $c
-        .url()
-        .urlPart(4)
-        .regex("ep[=](\d+)", 1)
-        .number()
-        .run();
+      return $c.url().urlPart(4).regex('ep[=](\d+)', 1).number().run();
     },
     uiInjection($c) {
       return $c.querySelector('.player-control').uiAfter().run();
     },
     nextEpUrl($c) {
       return $c
-        .url().split('/').slice(0, 4).join(
-        $c.querySelector('.ssl-item .ep-item .active')
-        .parent()
-        .next()
-        .ifNotReturn()
-        .find('a')
-        .getAttribute('data-id')
-        .ifNotReturn()
-        .urlAbsolute()
-        .run()).run();
+        .url()
+        .split('/')
+        .slice(0, 4)
+        .join(
+          $c
+            .querySelector('.ssl-item .ep-item .active')
+            .parent()
+            .next()
+            .ifNotReturn()
+            .find('a')
+            .getAttribute('data-id')
+            .ifNotReturn()
+            .urlAbsolute()
+            .run(),
+        )
+        .run();
     },
   },
   lifecycle: {
     setup($c) {
-      return $c.addStyle(require("./style.less?raw").toString()).run();
+      return $c.addStyle(require('./style.less?raw').toString()).run();
     },
     ready($c) {
       return $c
         .title()
-        .contains("Error 404")
-        .ifThen(($c) => $c.string("404").log().return().run())
+        .contains('Error 404')
+        .ifThen($c => $c.string('404').log().return().run())
         .domReady()
         .trigger()
         .run();
