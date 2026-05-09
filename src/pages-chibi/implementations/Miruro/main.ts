547a84a2154c8c25bc896bd748e894690ef11f9d
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2026-05-09 07:02:39 +0000

    Auto Urls: miruro

--- src/pages-chibi/implementations/Miruro/main.ts
+++ src/pages-chibi/implementations/Miruro/main.ts
@@ -6,7 +6,7 @@ export const Miruro: PageInterface = {
   languages: ['English'],
   type: 'anime',
   urls: {
-    match: ['*://*.miruro.to/*', '*://*.miruro.tv/*', '*://*.miruro.online/*', '*://*.miruro.bz/*'],
+    match: ['*://*.miruro.to/*', '*://*.miruro.tv/*', '*://*.miruro.online/*', '*://*.miruro.bz/*', '*://*.miruro.ru/*'],
   },
   search: 'https://www.miruro.to/search?query={searchtermPlus}',
   sync: {
