9e8c4797f6542caf9d1cb1c290ce4f87002f7469
hamza-wolf <hamzanazeer96@hotmail.com>
2026-06-20 23:45:24 +0500

    feat: add Zhanime.se chibi page integration

--- src/pages-chibi/pages.ts
+++ src/pages-chibi/pages.ts
@@ -109,6 +109,7 @@ import { TopManhua } from './implementations/TopManhua/main';
 import { Tsukuyomi } from './implementations/Tsukuyomi/main';
 import { HijalaScans } from './implementations/HijalaScans/main';
 import { Jellyfin } from './implementations/Jellyfin/main';
+import { Zhanime } from './implementations/Zhanime/main';
 
 export const pages: { [key: string]: PageInterface } = {
   animeav1,
@@ -220,4 +221,5 @@ export const pages: { [key: string]: PageInterface } = {
   Tsukuyomi,
   HijalaScans,
   Jellyfin,
+  Zhanime,
 };
