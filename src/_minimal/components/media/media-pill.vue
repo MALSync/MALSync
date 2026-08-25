6373e56a7175f59fb876a677cf92e2096242749f
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2026-08-25 15:35:53 +0200

    [TASK] Split pill when not enough space

--- src/_minimal/components/media/media-pill.vue
+++ src/_minimal/components/media/media-pill.vue
@@ -1,23 +1,27 @@
 <template>
-  <PillSplit :right="Boolean(showEp || streamUrl)" :left="Boolean(score)" :img="img">
-    <template #left>
-      <TextIcon icon="star">{{ score }}</TextIcon>
-    </template>
-    <template #right>
-      <div class="right-section">
-        <MediaLink v-if="Boolean(streamUrl)" :href="streamUrl">
-          <img class="streamIcon" :src="streamIcon" />
-        </MediaLink>
-        <div v-if="showEp">{{ watchedEp }}/<MediaTotalEpisode :episode="totalEp" /></div>
-        <MediaProgressPill
-          :episode="progressEp"
-          :text="progressText"
-          mode="large"
-          :watched-ep="watchedEp"
-        />
-      </div>
-    </template>
-  </PillSplit>
+  <div class="media-pill">
+    <PillSplit v-if="score" :right="false" :img="img">
+      <template #left>
+        <TextIcon icon="star">{{ score }}</TextIcon>
+      </template>
+    </PillSplit>
+    <PillSplit :left="false" :right="Boolean(showEp || streamUrl)" :img="img">
+      <template #right>
+        <div class="right-section">
+          <MediaLink v-if="Boolean(streamUrl)" :href="streamUrl">
+            <img class="streamIcon" :src="streamIcon" />
+          </MediaLink>
+          <div v-if="showEp">{{ watchedEp }}/<MediaTotalEpisode :episode="totalEp" /></div>
+          <MediaProgressPill
+            :episode="progressEp"
+            :text="progressText"
+            mode="large"
+            :watched-ep="watchedEp"
+          />
+        </div>
+      </template>
+    </PillSplit>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -82,6 +86,24 @@ const showEp = computed(() =>
 
 <style lang="less" scoped>
 @import '../../less/_globals.less';
+
+.media-pill {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  gap: 5px 0;
+  max-width: 100%;
+}
+
+.pill {
+  max-width: 100%;
+
+  & + .pill {
+    z-index: 0;
+    margin-inline-start: -18px;
+    padding-inline-start: 18px;
+  }
+}
 
 .right-section {
   display: flex;
